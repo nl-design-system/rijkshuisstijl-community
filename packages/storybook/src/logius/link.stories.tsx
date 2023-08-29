420dd3fd9870d5b870f0524dcb9804b0409abf66
Robbert Broersma <git@robbertbroersma.nl>
2023-08-29 22:14:54 +0200

    feat: add link story

---
+++ packages/storybook/src/logius/link.stories.tsx
@@ -0,0 +1,44 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { UtrechtLink } from '@utrecht/web-component-library-react';
+import readme from './link.md?raw';
+
+const meta = {
+  title: 'Logius/Link',
+  id: 'logius-link',
+  component: UtrechtLink,
+  argTypes: {
+    children: {
+      description: 'Link text',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof UtrechtLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    href: 'https://example.com/',
+    children: 'Aliquam non enim auctor',
+  },
+  name: 'Link',
+};
