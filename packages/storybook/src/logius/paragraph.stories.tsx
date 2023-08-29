0a05b4a5da4ef17f508a8fc2ffeadfc5f45249bb
Robbert Broersma <git@robbertbroersma.nl>
2023-08-29 22:14:42 +0200

    feat: add paragraph story

---
+++ packages/storybook/src/logius/paragraph.stories.tsx
@@ -0,0 +1,51 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { UtrechtParagraph } from '@utrecht/web-component-library-react';
+import readme from './paragraph.md?raw';
+
+const meta = {
+  title: 'Logius/Paragraph',
+  id: 'logius-paragraph',
+  component: UtrechtParagraph,
+  argTypes: {
+    children: {
+      description: 'Paragraph text',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof UtrechtParagraph>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Paragraph',
+};
+
+export const Lead: Story = {
+  args: {
+    lead: true,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Lead paragraph',
+};
