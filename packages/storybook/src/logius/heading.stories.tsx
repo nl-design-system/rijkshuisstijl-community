b825e0b8088b5f387c3602d3939c27b0cd41b5f2
Robbert Broersma <git@robbertbroersma.nl>
2023-08-29 22:15:19 +0200

    feat: add heading story

---
+++ packages/storybook/src/logius/heading.stories.tsx
@@ -0,0 +1,89 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { UtrechtHeading } from '@utrecht/web-component-library-react';
+import readme from './heading.md?raw';
+
+const meta = {
+  title: 'Logius/Heading',
+  id: 'logius-heading',
+  component: UtrechtHeading,
+  argTypes: {
+    level: {
+      description: 'Heading level',
+      control: { type: 'select' },
+      options: [1, 2, 3, 4, 5, 6],
+      defaultValue: 1,
+    },
+    children: {
+      description: 'Heading text',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof UtrechtHeading>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    level: 1,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading1: StoryObj<typeof meta> = {
+  args: {
+    level: 1,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading2: StoryObj<typeof meta> = {
+  args: {
+    level: 2,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading3: StoryObj<typeof meta> = {
+  args: {
+    level: 3,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading4: StoryObj<typeof meta> = {
+  args: {
+    level: 4,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading5: StoryObj<typeof meta> = {
+  args: {
+    level: 5,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
+
+export const Heading6: StoryObj<typeof meta> = {
+  args: {
+    level: 6,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+};
