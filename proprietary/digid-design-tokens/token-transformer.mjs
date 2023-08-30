a21c5eed769d6016495f5d3c3bd2706d170b7092
Robbert Broersma <git@robbertbroersma.nl>
2023-08-30 08:20:45 +0200

    build: theme support with custom token-transformer

---
+++ proprietary/digid-design-tokens/token-transformer.mjs
@@ -0,0 +1,11 @@
+import { tokenTransformer } from "../logius-design-tokens/src/token-transformer.mjs";
+import { resolve } from "node:path";
+
+tokenTransformer({
+	input: new URL(
+		"../logius-design-tokens/src/figma.tokens.json",
+		import.meta.url,
+	).pathname,
+	output: new URL("./tmp/figma.tokens.json", import.meta.url).pathname,
+	themes: ["DigiD"],
+});
