269ef4dc6c9bc05a3513c259851a0eb188f3f677
Robbert Broersma <git@robbertbroersma.nl>
2023-09-07 14:36:37 +0200

    docs: add RIVM paragraph story

---
+++ packages/storybook/src/rivm/paragraph.stories.tsx
@@ -0,0 +1,63 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { UtrechtParagraph } from '@utrecht/web-component-library-react';
+import readme from './paragraph.md?raw';
+
+const meta = {
+  title: 'RIVM/Paragraph',
+  id: 'rivm-paragraph',
+  component: UtrechtParagraph,
+  argTypes: {
+    lead: {
+      description: 'Lead paragraph',
+      type: {
+        name: 'boolean',
+      },
+      table: {
+        category: 'Property',
+      },
+      defaultValue: false,
+    },
+    children: {
+      description: 'Paragraph text - default webcomponent slot',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      table: {
+        category: 'Webcomponent Slot',
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof UtrechtParagraph>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Paragraph',
+};
+
+export const Lead: Story = {
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    lead: true,
+  },
+};
