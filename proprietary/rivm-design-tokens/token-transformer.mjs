f73359e364b225d3ff65296adc1f539f1c440d26
Robbert Broersma <git@robbertbroersma.nl>
2023-09-07 13:39:51 +0200

    feat: add RIVM design tokens

---
+++ proprietary/rivm-design-tokens/token-transformer.mjs
@@ -0,0 +1,8 @@
+import { tokenTransformer } from "../logius-design-tokens/src/token-transformer.mjs";
+import { resolve } from "node:path";
+
+tokenTransformer({
+  input: new URL("./src/figma.tokens.json", import.meta.url).pathname,
+  output: new URL("./tmp/figma.tokens.json", import.meta.url).pathname,
+  themes: ["Robijnrood"],
+});
