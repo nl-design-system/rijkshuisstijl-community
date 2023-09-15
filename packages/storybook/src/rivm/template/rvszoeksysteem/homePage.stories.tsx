575899088e4443eb1645f7ed0fbb7ea3bb97b39e
Robbert Broersma <git@robbertbroersma.nl>
2023-09-15 18:40:14 +0200

    docs: add logo

--- packages/storybook/src/rivm/template/rvszoeksysteem/homePage.stories.tsx
+++ packages/storybook/src/rivm/template/rvszoeksysteem/homePage.stories.tsx
@@ -1,12 +1,26 @@
 /* @license CC0-1.0 */
-import { FooterMenu } from '@nl-rvo/component-library-react/dist/css-module';
+import { FooterMenu, Logo } from '@nl-rvo/component-library-react/dist/css-module';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Document, Heading1, Heading2, Page, PageFooter } from '@utrecht/component-library-react/dist/css-module';
+import {
+  Document,
+  Heading1,
+  Heading2,
+  Page,
+  PageFooter,
+  PageHeader,
+} from '@utrecht/component-library-react/dist/css-module';
 import readme from './homePage.md?raw';
+import {} from '@utrecht/component-library-react';
 
 const PageTemplate = () => (
   <Document>
     <Page>
+      <PageHeader>
+        <Logo
+          title="Rijksinstituut voor Volksgezondheid en Milieu"
+          subtitle="Ministerie van Volksgezondheid, Welzijn en Sport"
+        ></Logo>
+      </PageHeader>
       <Heading1>Heading1</Heading1>
       <Heading2>Heading2</Heading2>
       <PageFooter>
