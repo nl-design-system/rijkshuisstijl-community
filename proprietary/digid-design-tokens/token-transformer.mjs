936d6a5691ebcbd33dca8f859237d78dfadfa246
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-10-03 15:55:48 +0200

    fix: linting fix

--- proprietary/digid-design-tokens/token-transformer.mjs
+++ proprietary/digid-design-tokens/token-transformer.mjs
@@ -2,10 +2,10 @@ import { tokenTransformer } from "../logius-design-tokens/src/token-transformer.
 import { resolve } from "node:path";
 
 tokenTransformer({
-	input: new URL(
-		"../logius-design-tokens/src/figma.tokens.json",
-		import.meta.url,
-	).pathname,
-	output: new URL("./tmp/figma.tokens.json", import.meta.url).pathname,
-	themes: ["DigiD"],
+  input: new URL(
+    "../logius-design-tokens/src/figma.tokens.json",
+    import.meta.url,
+  ).pathname,
+  output: new URL("./tmp/figma.tokens.json", import.meta.url).pathname,
+  themes: ["DigiD"],
 });
