712086a294275b06709d9f8a4b489eb8274ecf63
Robbert Broersma <git@robbertbroersma.nl>
2023-11-23 11:27:45 +0100

    feat: add next.js demo app

---
+++ apps/rvs.rivm.nl/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { Document } from '@utrecht/component-library-react';
+import type { Metadata } from 'next';
+import { PropsWithChildren } from 'react';
+import '@utrecht/component-library-css';
+import '@rijkshuisstijl-community/components-css/index.scss';
+import '@rijkshuisstijl-community/rivm-design-tokens/dist/theme.css';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: "Risico's van stoffen - RIVM",
+  description: 'Demo met NL Design System.',
+};
+const RivmTheme = ({ children }: PropsWithChildren<{}>) => <div className="rivm-theme">{children}</div>;
+
+export default function RootLayout({ children }: PropsWithChildren<{}>) {
+  return (
+    <html lang="nl">
+      <body>
+        <RivmTheme>
+          <Document>{children}</Document>
+        </RivmTheme>
+      </body>
+    </html>
+  );
+}
