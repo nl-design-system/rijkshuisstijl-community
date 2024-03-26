63ed2644b54e1875cdd0af7ddcae220314575a2f
matijs <matijs@gmail.com>
2024-03-26 15:34:46 +0100

    chore: add .mjs files
    Make sure Prettier, ESLint and lint-staged know about .mjs and .cjs
    files
    
    Set ESLint's ecmaVersion to 'latest' so the `.mjs` files in the repo can
    be linted.

--- proprietary/digid-design-tokens/token-transformer.mjs
+++ proprietary/digid-design-tokens/token-transformer.mjs
@@ -1,11 +1,8 @@
-import { tokenTransformer } from "../logius-design-tokens/src/token-transformer.mjs";
-import { resolve } from "node:path";
+import { tokenTransformer } from '../logius-design-tokens/src/token-transformer.mjs';
+import { resolve } from 'node:path';
 
 tokenTransformer({
-  input: new URL(
-    "../logius-design-tokens/src/figma.tokens.json",
-    import.meta.url,
-  ).pathname,
-  output: new URL("./tmp/figma.tokens.json", import.meta.url).pathname,
-  themes: ["DigiD"],
+  input: new URL('../logius-design-tokens/src/figma.tokens.json', import.meta.url).pathname,
+  output: new URL('./tmp/figma.tokens.json', import.meta.url).pathname,
+  themes: ['DigiD'],
 });
