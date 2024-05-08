469264ff40df3ddeaa90d223564c2873ed94e0dc
matijs <matijs@gmail.com>
2024-05-08 16:25:46 +0200

    build: bump storybook to v8
    - Bump Storybook to v8
    - Remove unsupported addons
    - Replace custom Prettier source solution with
      `@whitespace/storybook-addon-html` that now supports Storybook v8

--- packages/storybook/config/preview.tsx
+++ packages/storybook/config/preview.tsx
@@ -1,11 +1,6 @@
-import { withThemeByClassName } from '@storybook/addon-styling';
+import { withThemeByClassName } from '@storybook/addon-themes';
 import { Preview } from '@storybook/react';
-import type { StoryContext } from '@storybook/types';
 import { UtrechtDocument } from '@utrecht/web-component-library-react';
-import prettierBabel from 'prettier/parser-babel';
-import prettier from 'prettier/standalone';
-import React from 'react';
-import * as ReactDOMServer from 'react-dom/server';
 import '@utrecht/component-library-css/dist/index.css';
 import '@utrecht/component-library-css/dist/html.css';
 import '@rijkshuisstijl-community/design-tokens/dist/index.css';
@@ -15,44 +10,6 @@ import '@rijkshuisstijl-community/mijnoverheid-design-tokens/dist/theme.css';
 import '@rijkshuisstijl-community/rivm-design-tokens/dist/theme.css';
 import '@rijkshuisstijl-community/font/src/index.scss';
 import '@nl-rvo/assets/fonts/index.css';
-
-// Configure @etchteam/storybook-addon-status
-const addonStatus = {
-  status: {
-    statuses: {
-      PRODUCTION: {
-        background: '#088008',
-        color: '#ffffff',
-        description:
-          'Used in production in a variety of situations, well tested, stable APIs, mostly patches and minor releases.',
-      },
-      BETA: {
-        background: '#3065ee',
-        color: '#ffffff',
-        description:
-          'Used in production in a specific situation, evolving APIs based on feedback, breaking changes are still likely.',
-      },
-      ALPHA: {
-        background: '#e0bc2e',
-        color: '#000000',
-        description:
-          'Used in prototypes and in projects that are still in development, breaking changes occur frequently and are not communicated.',
-      },
-      'WORK IN PROGRESS': {
-        background: '#cc0000',
-        color: '#ffffff',
-        description:
-          'Do not use in production. Does not follow semantic versioning and any published packages are for internal use only.',
-      },
-    },
-  },
-};
-
-const previewTabs = {
-  'storybookjs/notes/panel': { title: 'Documentation' },
-  'storybook/docs/panel': { title: 'API' },
-  canvas: { title: 'Design Tokens' },
-};
 
 const preview: Preview = {
   decorators: [
@@ -70,40 +27,12 @@ const preview: Preview = {
   ],
   parameters: {
     previewTabs: {
-      ...previewTabs,
+      'storybookjs/notes/panel': { title: 'Documentation' },
+      'storybook/docs/panel': { title: 'API' },
+      canvas: { title: 'Design Tokens' },
     },
-    ...addonStatus,
     controls: { expanded: false },
-    options: {
-      panelPosition: 'right',
-    },
-    docs: {
-      // Show code by default.
-      // Stories without concise code snippets can hide the code at Story level.
-      source: {
-        state: 'open',
-        transform: (src: string, storyContext: StoryContext<any>): string => {
-          // Ensure valid HTML in the Preview source
-          const render =
-            typeof storyContext.component === 'function'
-              ? storyContext.component
-              : typeof storyContext.component?.render === 'function'
-                ? storyContext.component?.render
-                : null;
-
-          if (render) {
-            const renderOutput = render(storyContext.args);
-            const markup = ReactDOMServer.renderToStaticMarkup(renderOutput);
-            const prettierMarkup = prettier.format(markup, {
-              parser: 'babel',
-              plugins: [prettierBabel],
-            });
-            return prettierMarkup;
-          }
-          return src;
-        },
-      },
-    },
+    options: { panelPosition: 'right' },
   },
 };
 
