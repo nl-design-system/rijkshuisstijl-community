30a4eb8f8e6543d17fc99122ff8755106e126c68
matijs <matijs@gmail.com>
2024-05-14 17:13:27 +0200

    fixup! build: bump storybook to v8

--- packages/storybook/config/preview.tsx
+++ packages/storybook/config/preview.tsx
@@ -1,15 +1,17 @@
-import { withThemeByClassName } from '@storybook/addon-themes';
-import { Preview } from '@storybook/react';
-import { UtrechtDocument } from '@utrecht/web-component-library-react';
-import '@utrecht/component-library-css/dist/index.css';
-import '@utrecht/component-library-css/dist/html.css';
+import '@nl-rvo/assets/fonts/index.css';
 import '@rijkshuisstijl-community/design-tokens/dist/index.css';
 import '@rijkshuisstijl-community/digid-design-tokens/dist/theme.css';
+import '@rijkshuisstijl-community/font/src/index.scss';
 import '@rijkshuisstijl-community/logius-design-tokens/dist/theme.css';
 import '@rijkshuisstijl-community/mijnoverheid-design-tokens/dist/theme.css';
 import '@rijkshuisstijl-community/rivm-design-tokens/dist/theme.css';
-import '@rijkshuisstijl-community/font/src/index.scss';
-import '@nl-rvo/assets/fonts/index.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { Preview, StoryContext } from '@storybook/react';
+import '@utrecht/component-library-css/dist/html.css';
+import '@utrecht/component-library-css/dist/index.css';
+import { UtrechtDocument } from '@utrecht/web-component-library-react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { Prettify } from './Prettify';
 
 const preview: Preview = {
   decorators: [
@@ -33,6 +35,25 @@ const preview: Preview = {
     },
     controls: { expanded: false },
     options: { panelPosition: 'right' },
+    docs: {
+      source: {
+        transform(src: string, storyContext: StoryContext) {
+          const render =
+            typeof storyContext.component === 'function'
+              ? storyContext.component
+              : typeof storyContext.component?.render === 'function'
+                ? storyContext.component?.render
+                : null;
+
+          if (render) {
+            const srcString = renderToStaticMarkup(render(storyContext.args));
+
+            return Prettify({ ugly: srcString });
+          }
+          return src;
+        },
+      },
+    },
   },
 };
 
