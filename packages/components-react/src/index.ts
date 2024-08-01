35597f5051c078215399ce0673b86af71bb270cd
Ruben Smit <rsmit@quintor.local>
2024-08-01 14:08:58 +0200

    chore: fixed build errors with nextjs

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -1,3 +1,6 @@
-export { Button } from './Button';
+// Export all Utrecht components
+export * from '@utrecht/component-library-react/dist/css-module';
+
+// Export overwrites and new componentsexport { Button } from './Button';
 export { Logo } from './Logo';
 export type { LogoProps } from './Logo';
