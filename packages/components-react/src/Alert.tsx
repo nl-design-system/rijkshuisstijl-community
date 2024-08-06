9aaedf0e603c58e2def62c390a0a168a6be60f22
Adham AboHasson <aahasson@quintor.nl>
2024-08-06 15:47:45 +0200

    make a custom alert component

---
+++ packages/components-react/src/Alert.tsx
@@ -0,0 +1,63 @@
+import {
+  RhcIconError,
+  RhcIconInfo,
+  RhcIconSuccess,
+  RhcIconWarning,
+} from '@rijkshuisstijl-community/web-components-react';
+import { Heading, Icon, Paragraph, Alert as UAlert } from '@utrecht/component-library-react/dist/css-module';
+import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+const RhcIcon = ({ icon }: { icon: string }) =>
+  icon === 'info' ? (
+    <RhcIconInfo></RhcIconInfo>
+  ) : icon === 'ok' ? (
+    <RhcIconSuccess></RhcIconSuccess>
+  ) : icon === 'warning' ? (
+    <RhcIconWarning></RhcIconWarning>
+  ) : icon === 'error' ? (
+    <RhcIconError></RhcIconError>
+  ) : (
+    <></>
+  );
+export const Alert = forwardRef(
+  (
+    { type, icon, heading, headingLevel, textContent, ...restProps }: PropsWithChildren<any>,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    return (
+      <UAlert ref={ref} {...restProps} type={type}>
+        <div
+          style={{
+            display: 'flex',
+          }}
+        >
+          <div>
+            {icon && (
+              <Icon
+                style={{
+                  color:
+                    type === 'ok'
+                      ? 'var(--rhc-color-feedback-success-default)'
+                      : type === 'error'
+                        ? 'var(--rhc-color-feedback-error-default)'
+                        : type === 'warning'
+                          ? 'var(--rhc-color-feedback-warning-default)'
+                          : 'var(--rhc-color-feedback-info-default)',
+                  inlineSize: 'var(--rhc-space-200)',
+                  paddingInlineEnd: 'var(--rhc-space-100)',
+                }}
+              >
+                <RhcIcon icon={icon} />
+              </Icon>
+            )}
+          </div>
+          <div>
+            <Heading level={headingLevel}>{heading}</Heading>
+            <Paragraph>{textContent}</Paragraph>
+          </div>
+        </div>
+      </UAlert>
+    );
+  },
+);
+
+Alert.displayName = 'Alert';
--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -3,4 +3,5 @@ export * from '@utrecht/component-library-react/dist/css-module';
 
 export { Button } from './Button';
 export { Logo } from './Logo';
+export { Alert } from './Alert';
 export type { LogoProps } from './Logo';
