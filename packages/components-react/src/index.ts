25d7a3ee098946ba35998cc247754f78db017683
Adham AboHasson <aahasson@quintor.nl>
2024-08-09 14:02:27 +0200

    merge and resolve conflicts

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -2,7 +2,6 @@
 export * from '@utrecht/component-library-react/dist/css-module';
 
 // Export overwrites and new components
-export { Button } from './Button';
 export { Logo } from './Logo';
 export { Alert } from './Alert';
 export type { LogoProps } from './Logo';
