8e5821209cedcf23bded5cf3000e6a45254af368
Mees Work <Work@Meess-Laptop.local>
2024-08-12 14:28:52 +0200

    Added unorderedlist to Storybook

---
+++ packages/storybook/src/community/unorderedList.stories.tsx
@@ -0,0 +1,49 @@
+import { UnorderedList, UnorderedListItem } from '@rijkshuisstijl-community/components-react';
+import { Meta, StoryObj } from '@storybook/react';
+import readme from './unorderedList.md?raw';
+
+const meta = {
+  title: 'Rijkshuisstijl/Unordered List',
+  id: 'rijkshuisstijl-unorderedList',
+  component: UnorderedList,
+  args: {
+    children: '',
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UnorderedList>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultUnorderedList: Story = {
+  args: {
+    children: [
+      <UnorderedListItem>List item 1</UnorderedListItem>,
+      <UnorderedListItem>List item 2</UnorderedListItem>,
+      <UnorderedListItem>List item 3</UnorderedListItem>,
+    ],
+  },
+};
+
+export const NestedUnorderedList: Story = {
+  args: {
+    children: [
+      <UnorderedListItem>List item 1</UnorderedListItem>,
+      <UnorderedListItem>
+        List item 2
+        <UnorderedList>
+          <UnorderedListItem>Nested List item 1</UnorderedListItem>
+          <UnorderedListItem>Nested List item 2</UnorderedListItem>
+        </UnorderedList>
+      </UnorderedListItem>,
+      <UnorderedListItem>List item 3</UnorderedListItem>,
+    ],
+  },
+};
