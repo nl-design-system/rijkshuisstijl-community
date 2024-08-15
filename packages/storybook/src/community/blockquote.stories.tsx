4c1aec4586ae01b35047bcbfc0b0096a7c90e2aa
Ruben Smit <rsmit@quintor.local>
2024-08-15 16:29:49 +0200

    feat(blockquote): implemented component, styling, storybook and tests

--- packages/storybook/src/community/blockquote.stories.tsx
+++ packages/storybook/src/community/blockquote.stories.tsx
@@ -1,11 +1,11 @@
 /* @license CC0-1.0 */
 
+import { Blockquote } from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react';
-import { Blockquote, Paragraph } from '@utrecht/component-library-react/dist/css-module';
 import readme from './blockquote.md?raw';
 
 const meta = {
-  title: 'React Component/Blockquote',
+  title: 'Rijkshuisstijl/Blockquote',
   id: 'rijkshuisstijl-blockquote',
   component: Blockquote,
   argTypes: {
@@ -15,15 +15,7 @@ const meta = {
     },
   },
   args: {
-    children: (
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum
-      </Paragraph>
-    ),
+    children: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *',
   },
   tags: ['autodocs'],
   parameters: {
@@ -44,7 +36,7 @@ export const Default: Story = {};
 export const Attribution: Story = {
   name: 'Attribution',
   args: {
-    children: 'hello, world',
-    attribution: <cite>The C Programming Language</cite>,
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
   },
 };
--- proprietary/design-tokens/token-transformer.mjs
+++ proprietary/design-tokens/token-transformer.mjs
@@ -14,7 +14,6 @@ const init = async ({ input, output }) => {
     'components/alert',
     'components/avatar',
     'components/backdrop',
-    'components/blockquote',
     'components/breadcrumb',
     'components/button-group',
     'components/checkbox',
