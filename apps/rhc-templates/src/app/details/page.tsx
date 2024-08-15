de59cde95d292374ed45aa53604462205ac266c4
Ruben Smit <rsmit@quintor.local>
2024-08-15 14:11:49 +0200

    chore: removed unecessary fragments

--- apps/rhc-templates/src/app/details/page.tsx
+++ apps/rhc-templates/src/app/details/page.tsx
@@ -19,129 +19,127 @@ import {
 
 export default function Details() {
   return (
-    <>
-      <main>
-        <div className="unfinished">
-          <PageHeader>
-            <Logo organisation="Voorbeeld organisatie" subtitle="Voorbeeld sub-title"></Logo>
-            <BreadcrumbNav>
-              <BreadcrumbNavLink href="/" rel="home" index={0}>
-                Home
-              </BreadcrumbNavLink>
-              <BreadcrumbNavLink href="/a/" index={1}>
-                Onderwerpen
-              </BreadcrumbNavLink>
-              <BreadcrumbNavLink href="/a/b/" rel="up" index={2}>
-                Werktijden
-              </BreadcrumbNavLink>
-            </BreadcrumbNav>
-            <div className="header-image">
-              <div className="header-image-textbox">
-                <Heading level={1}>Veel bekeken</Heading>
-                <LinkList>
-                  <LinkListLink>Wat zijn de wettelijke regels voor mijn werktijden en rusttijden?</LinkListLink>
-                  <LinkListLink>Wat zijn de wettelijke regels voor pauzes tijdens mijn werk?</LinkListLink>
-                </LinkList>
-              </div>
-            </div>
-            <div className="sub-navigation">
+    <main>
+      <div className="unfinished">
+        <PageHeader>
+          <Logo organisation="Voorbeeld organisatie" subtitle="Voorbeeld sub-title"></Logo>
+          <BreadcrumbNav>
+            <BreadcrumbNavLink href="/" rel="home" index={0}>
+              Home
+            </BreadcrumbNavLink>
+            <BreadcrumbNavLink href="/a/" index={1}>
+              Onderwerpen
+            </BreadcrumbNavLink>
+            <BreadcrumbNavLink href="/a/b/" rel="up" index={2}>
+              Werktijden
+            </BreadcrumbNavLink>
+          </BreadcrumbNav>
+          <div className="header-image">
+            <div className="header-image-textbox">
+              <Heading level={1}>Veel bekeken</Heading>
               <LinkList>
-                <LinkListLink>Regels voor rijtijden en rusttijden bij wegvervoer</LinkListLink>
-                <LinkListLink>Vraag en antwoord</LinkListLink>
-                <LinkListLink>Documenten</LinkListLink>
+                <LinkListLink>Wat zijn de wettelijke regels voor mijn werktijden en rusttijden?</LinkListLink>
+                <LinkListLink>Wat zijn de wettelijke regels voor pauzes tijdens mijn werk?</LinkListLink>
               </LinkList>
             </div>
-          </PageHeader>
-        </div>
-
-        <PageContent className="container page-content">
-          <div className="unstarted">
-            <Paragraph>
-              De Arbeidstijdenwet geeft regels voor werktijden, pauzes en rusttijden van werknemers. Met deze regels wil
-              de Rijksoverheid werknemers beschermen tegen te lange werkdagen. Maar ook de combinatie van werk, privé en
-              zorgtaken gemakkelijker maken.
-            </Paragraph>
           </div>
-          <div className="unstarted">
-            <Separator></Separator>
-          </div>
-          <div className="unstarted">
-            <Heading level={1}>Vraag en antwoord</Heading>
-          </div>
-          <div className="unstarted">
+          <div className="sub-navigation">
             <LinkList>
-              <LinkListLink>Wat zijn de regels bij consignatie (oproepdienst)?</LinkListLink>
-              <LinkListLink>Hoe vaak mag ik nachtdienst hebben?</LinkListLink>
-              <LinkListLink>Welke brochures zijn er over Arbeidstijdenwet en Arbeidstijdenbesluit?</LinkListLink>
-              <LinkListLink>Wanneer geldt de Arbeidstijdenwet niet (of gedeeltelijk) voor mij?</LinkListLink>
-              <LinkListLink>Meer vragen en antwoorden</LinkListLink>
+              <LinkListLink>Regels voor rijtijden en rusttijden bij wegvervoer</LinkListLink>
+              <LinkListLink>Vraag en antwoord</LinkListLink>
+              <LinkListLink>Documenten</LinkListLink>
             </LinkList>
           </div>
-          <div className="unstarted">
-            <Heading level={1}>Uitgelicht</Heading>
-            <div className="image-group">
-              <div className="image-group-box">
-                <Figure className="unstarted">
-                  <Image
-                    alt="Multicolored tulip field"
-                    width="640"
-                    height="763"
-                    src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
-                  ></Image>
-                  <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
-                </Figure>
-              </div>
-              <div className="image-group-box">
-                <Figure className="unstarted">
-                  <Image
-                    alt="Multicolored tulip field"
-                    width="640"
-                    height="763"
-                    src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
-                  ></Image>
-                  <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
-                </Figure>
-              </div>
+        </PageHeader>
+      </div>
+
+      <PageContent className="container page-content">
+        <div className="unstarted">
+          <Paragraph>
+            De Arbeidstijdenwet geeft regels voor werktijden, pauzes en rusttijden van werknemers. Met deze regels wil
+            de Rijksoverheid werknemers beschermen tegen te lange werkdagen. Maar ook de combinatie van werk, privé en
+            zorgtaken gemakkelijker maken.
+          </Paragraph>
+        </div>
+        <div className="unstarted">
+          <Separator></Separator>
+        </div>
+        <div className="unstarted">
+          <Heading level={1}>Vraag en antwoord</Heading>
+        </div>
+        <div className="unstarted">
+          <LinkList>
+            <LinkListLink>Wat zijn de regels bij consignatie (oproepdienst)?</LinkListLink>
+            <LinkListLink>Hoe vaak mag ik nachtdienst hebben?</LinkListLink>
+            <LinkListLink>Welke brochures zijn er over Arbeidstijdenwet en Arbeidstijdenbesluit?</LinkListLink>
+            <LinkListLink>Wanneer geldt de Arbeidstijdenwet niet (of gedeeltelijk) voor mij?</LinkListLink>
+            <LinkListLink>Meer vragen en antwoorden</LinkListLink>
+          </LinkList>
+        </div>
+        <div className="unstarted">
+          <Heading level={1}>Uitgelicht</Heading>
+          <div className="image-group">
+            <div className="image-group-box">
+              <Figure className="unstarted">
+                <Image
+                  alt="Multicolored tulip field"
+                  width="640"
+                  height="763"
+                  src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
+                ></Image>
+                <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
+              </Figure>
+            </div>
+            <div className="image-group-box">
+              <Figure className="unstarted">
+                <Image
+                  alt="Multicolored tulip field"
+                  width="640"
+                  height="763"
+                  src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
+                ></Image>
+                <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
+              </Figure>
             </div>
           </div>
-        </PageContent>
-        <div className="unstarted">
-          <PageFooter>
-            <Heading level={1} appearance="utrecht-heading-2">
-              De Rijksoverheid. Voor Nederland
-            </Heading>
-            <div className="left-column">
-              <Heading level={2} appearance="utrecht-heading-4">
-                Service
-              </Heading>
-              <LinkList>
-                <LinkListLink href="#">Contact</LinkListLink>
-                <LinkListLink href="#">Abonneren</LinkListLink>
-                <LinkListLink href="#">RSS</LinkListLink>
-                <LinkListLink href="#">Vacatures</LinkListLink>
-                <LinkListLink href="#">Sitemap</LinkListLink>
-                <LinkListLink href="#">Help</LinkListLink>
-                <LinkListLink href="#">Archief</LinkListLink>
-              </LinkList>
-            </div>
-            <div className="right-column">
-              <Heading level={2} appearance="utrecht-heading-4">
-                Service
-              </Heading>
-              <LinkList>
-                <LinkListLink href="#">Over Rijksoverheid.nl</LinkListLink>
-                <LinkListLink href="#">Wetten en regelingen</LinkListLink>
-                <LinkListLink href="#">Copyright</LinkListLink>
-                <LinkListLink href="#">Privacy</LinkListLink>
-                <LinkListLink href="#">Cookies</LinkListLink>
-                <LinkListLink href="#">Toegankelijkheid</LinkListLink>
-                <LinkListLink href="#">Open data</LinkListLink>
-                <LinkListLink href="#">Kwetsbaarheid melden</LinkListLink>
-              </LinkList>
-            </div>
-          </PageFooter>
         </div>
-      </main>
-    </>
+      </PageContent>
+      <div className="unstarted">
+        <PageFooter>
+          <Heading level={1} appearance="utrecht-heading-2">
+            De Rijksoverheid. Voor Nederland
+          </Heading>
+          <div className="left-column">
+            <Heading level={2} appearance="utrecht-heading-4">
+              Service
+            </Heading>
+            <LinkList>
+              <LinkListLink href="#">Contact</LinkListLink>
+              <LinkListLink href="#">Abonneren</LinkListLink>
+              <LinkListLink href="#">RSS</LinkListLink>
+              <LinkListLink href="#">Vacatures</LinkListLink>
+              <LinkListLink href="#">Sitemap</LinkListLink>
+              <LinkListLink href="#">Help</LinkListLink>
+              <LinkListLink href="#">Archief</LinkListLink>
+            </LinkList>
+          </div>
+          <div className="right-column">
+            <Heading level={2} appearance="utrecht-heading-4">
+              Service
+            </Heading>
+            <LinkList>
+              <LinkListLink href="#">Over Rijksoverheid.nl</LinkListLink>
+              <LinkListLink href="#">Wetten en regelingen</LinkListLink>
+              <LinkListLink href="#">Copyright</LinkListLink>
+              <LinkListLink href="#">Privacy</LinkListLink>
+              <LinkListLink href="#">Cookies</LinkListLink>
+              <LinkListLink href="#">Toegankelijkheid</LinkListLink>
+              <LinkListLink href="#">Open data</LinkListLink>
+              <LinkListLink href="#">Kwetsbaarheid melden</LinkListLink>
+            </LinkList>
+          </div>
+        </PageFooter>
+      </div>
+    </main>
   );
 }
--- apps/rhc-templates/src/app/page/page.tsx
+++ apps/rhc-templates/src/app/page/page.tsx
@@ -37,204 +37,202 @@ import { HeadingGroup } from '@utrecht/component-library-react';
 
 export default function Page() {
   return (
-    <>
-      <main className="page">
-        <div className="unfinished header">
-          <PageHeader>
-            <Logo organisation="Voorbeeld organisatie" subtitle="Voorbeeld sub-title"></Logo>
-          </PageHeader>
+    <main className="page">
+      <div className="unfinished header">
+        <PageHeader>
+          <Logo organisation="Voorbeeld organisatie" subtitle="Voorbeeld sub-title"></Logo>
+        </PageHeader>
+      </div>
+      <div className="unstarted">
+        {/* wrong spacing, wrong background color, wrong text color, no way to add right aligning link */}
+        <NavBar>
+          <NavList>
+            <NavListLinkButton>
+              <a href="">Link 1</a>
+            </NavListLinkButton>
+            <NavListLinkButton>
+              <a href="">Link 2</a>
+            </NavListLinkButton>
+            <NavListLinkButton>
+              <a href="">Link 3</a>
+            </NavListLinkButton>
+          </NavList>
+        </NavBar>
+      </div>
+      <PageContent className="container page-content">
+        <div className="unstarted">
+          <HeadingGroup>
+            <Heading level={1}>Titel van de pagina (H1)</Heading>
+            <Paragraph>Dit is een sub-titel van de H1</Paragraph>
+          </HeadingGroup>
+        </div>
+        <Figure className="unstarted">
+          <Image
+            alt="Multicolored tulip field"
+            width="640"
+            height="763"
+            src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
+          ></Image>
+          <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
+        </Figure>
+        <div className="unstarted">
+          <Button type="submit" appearance="primary-action-button">
+            Primary action button
+          </Button>
         </div>
         <div className="unstarted">
-          {/* wrong spacing, wrong background color, wrong text color, no way to add right aligning link */}
-          <NavBar>
-            <NavList>
-              <NavListLinkButton>
-                <a href="">Link 1</a>
-              </NavListLinkButton>
-              <NavListLinkButton>
-                <a href="">Link 2</a>
-              </NavListLinkButton>
-              <NavListLinkButton>
-                <a href="">Link 3</a>
-              </NavListLinkButton>
-            </NavList>
-          </NavBar>
+          <Heading level={2}>Dit is een H2</Heading>
         </div>
-        <PageContent className="container page-content">
-          <div className="unstarted">
-            <HeadingGroup>
-              <Heading level={1}>Titel van de pagina (H1)</Heading>
-              <Paragraph>Dit is een sub-titel van de H1</Paragraph>
-            </HeadingGroup>
-          </div>
-          <Figure className="unstarted">
-            <Image
-              alt="Multicolored tulip field"
-              width="640"
-              height="763"
-              src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
-            ></Image>
-            <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
-          </Figure>
-          <div className="unstarted">
-            <Button type="submit" appearance="primary-action-button">
-              Primary action button
-            </Button>
-          </div>
-          <div className="unstarted">
-            <Heading level={2}>Dit is een H2</Heading>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Dit is een paragraaf.</Paragraph>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Opsomming ordered list</Paragraph>
-          </div>
-          <div className="unstarted">
-            <OrderedList>
-              <OrderedListItem>Ordered list item.</OrderedListItem>
-              <OrderedListItem>Ordered list item.</OrderedListItem>
-              <OrderedListItem>Ordered list item.</OrderedListItem>
-            </OrderedList>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Opsomming unordered list</Paragraph>
-          </div>
-          <div className="unstarted">
-            <UnorderedList>
-              <UnorderedListItem>Unordered list item.</UnorderedListItem>
-              <UnorderedListItem>Unordered list item.</UnorderedListItem>
-              <UnorderedListItem>Unordered list item.</UnorderedListItem>
-            </UnorderedList>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Opsomming link list</Paragraph>
-          </div>
-          <div className="unstarted">
-            <LinkList
-              icon={() => (
-                <Icon>
-                  <svg viewBox="0 0 1000 1000">
-                    <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
-                  </svg>
-                </Icon>
-              )}
-              links={[{ href: 'examle.com' }, { href: 'github.com' }]}
-            >
-              <LinkListLink href="/">Dit is een normale link</LinkListLink>
-              <LinkListLink href="/">Dit is een normale link</LinkListLink>
-              <LinkListLink href="example.com" external={true}>
-                Dit is een externe link
-              </LinkListLink>
-            </LinkList>
-          </div>
-          <div className="unstarted">
-            <Heading level={3}>Dit is een H3</Heading>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Dit is een paragraaf.</Paragraph>
-          </div>
-          <div className="unstarted">
-            <Link external={true} href="example.com">
+        <div className="unstarted">
+          <Paragraph>Dit is een paragraaf.</Paragraph>
+        </div>
+        <div className="unstarted">
+          <Paragraph>Opsomming ordered list</Paragraph>
+        </div>
+        <div className="unstarted">
+          <OrderedList>
+            <OrderedListItem>Ordered list item.</OrderedListItem>
+            <OrderedListItem>Ordered list item.</OrderedListItem>
+            <OrderedListItem>Ordered list item.</OrderedListItem>
+          </OrderedList>
+        </div>
+        <div className="unstarted">
+          <Paragraph>Opsomming unordered list</Paragraph>
+        </div>
+        <div className="unstarted">
+          <UnorderedList>
+            <UnorderedListItem>Unordered list item.</UnorderedListItem>
+            <UnorderedListItem>Unordered list item.</UnorderedListItem>
+            <UnorderedListItem>Unordered list item.</UnorderedListItem>
+          </UnorderedList>
+        </div>
+        <div className="unstarted">
+          <Paragraph>Opsomming link list</Paragraph>
+        </div>
+        <div className="unstarted">
+          <LinkList
+            icon={() => (
+              <Icon>
+                <svg viewBox="0 0 1000 1000">
+                  <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
+                </svg>
+              </Icon>
+            )}
+            links={[{ href: 'examle.com' }, { href: 'github.com' }]}
+          >
+            <LinkListLink href="/">Dit is een normale link</LinkListLink>
+            <LinkListLink href="/">Dit is een normale link</LinkListLink>
+            <LinkListLink href="example.com" external={true}>
               Dit is een externe link
-            </Link>
-          </div>
-          <div className="unstarted">
-            <Table>
-              <TableCaption>Caption van tabel</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHeaderCell>Label</TableHeaderCell>
-                  <TableHeaderCell>Label</TableHeaderCell>
-                  <TableHeaderCell>Label</TableHeaderCell>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Label</TableCell>
-                  <TableCell>Label</TableCell>
-                  <TableCell>Label</TableCell>
-                </TableRow>
-              </TableBody>
-              <TableFooter>
-                <TableRow>
-                  <TableCell>Label</TableCell>
-                  <TableCell>Label</TableCell>
-                  <TableCell>Label</TableCell>
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </div>
-          <div className="unstarted">
-            <Heading level={4}>Dit is een H4</Heading>
-          </div>
-          <div className="unstarted">
-            <Paragraph>
-              Dit is een wat langere paragraaf. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              <span className="unstarted">
-                <Link href="/">Dit is een normale link</Link>
-              </span>
-            </Paragraph>
-          </div>
-          <div className="unstarted">
-            <Blockquote attribution="Label">
-              Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *
-            </Blockquote>
-          </div>
-          <div className="unstarted">
-            <Heading level={5}>Dit is een H5</Heading>
-          </div>
-          <div className="unstarted">
-            <Paragraph>Dit is een paragraaf.</Paragraph>
-          </div>
-          <div className="unstarted">
-            <Separator></Separator>
-          </div>
-          <div className="unstarted">
-            <Heading level={5} appearance="utrecht-heading-6">
-              Dit is een H5 met H6 appearance
-            </Heading>
-          </div>
-          <ActionGroup>
-            <Button appearance="secondary-action-button">
-              Link kopiëren
-              <Icon>
-                <svg viewBox="0 0 1000 1000">
-                  <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
-                </svg>
-              </Icon>
-            </Button>
-            <ButtonLink appearance="secondary-action-button" external={true}>
-              E-mail
-              <Icon>
-                <svg viewBox="0 0 1000 1000">
-                  <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
-                </svg>
-              </Icon>
-            </ButtonLink>
-            <ButtonLink appearance="secondary-action-button" external={true}>
-              Twitter
-              <Icon>
-                <svg viewBox="0 0 1000 1000">
-                  <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
-                </svg>
-              </Icon>
-            </ButtonLink>
-            <ButtonLink appearance="secondary-action-button" external={true}>
-              LinkedIn
-              <Icon>
-                <svg viewBox="0 0 1000 1000">
-                  <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
-                </svg>
-              </Icon>
-            </ButtonLink>
-          </ActionGroup>
-        </PageContent>
-      </main>
-    </>
+            </LinkListLink>
+          </LinkList>
+        </div>
+        <div className="unstarted">
+          <Heading level={3}>Dit is een H3</Heading>
+        </div>
+        <div className="unstarted">
+          <Paragraph>Dit is een paragraaf.</Paragraph>
+        </div>
+        <div className="unstarted">
+          <Link external={true} href="example.com">
+            Dit is een externe link
+          </Link>
+        </div>
+        <div className="unstarted">
+          <Table>
+            <TableCaption>Caption van tabel</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderCell>Label</TableHeaderCell>
+                <TableHeaderCell>Label</TableHeaderCell>
+                <TableHeaderCell>Label</TableHeaderCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>Label</TableCell>
+                <TableCell>Label</TableCell>
+                <TableCell>Label</TableCell>
+              </TableRow>
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell>Label</TableCell>
+                <TableCell>Label</TableCell>
+                <TableCell>Label</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+        <div className="unstarted">
+          <Heading level={4}>Dit is een H4</Heading>
+        </div>
+        <div className="unstarted">
+          <Paragraph>
+            Dit is een wat langere paragraaf. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            <span className="unstarted">
+              <Link href="/">Dit is een normale link</Link>
+            </span>
+          </Paragraph>
+        </div>
+        <div className="unstarted">
+          <Blockquote attribution="Label">
+            Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *
+          </Blockquote>
+        </div>
+        <div className="unstarted">
+          <Heading level={5}>Dit is een H5</Heading>
+        </div>
+        <div className="unstarted">
+          <Paragraph>Dit is een paragraaf.</Paragraph>
+        </div>
+        <div className="unstarted">
+          <Separator></Separator>
+        </div>
+        <div className="unstarted">
+          <Heading level={5} appearance="utrecht-heading-6">
+            Dit is een H5 met H6 appearance
+          </Heading>
+        </div>
+        <ActionGroup>
+          <Button appearance="secondary-action-button">
+            Link kopiëren
+            <Icon>
+              <svg viewBox="0 0 1000 1000">
+                <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
+              </svg>
+            </Icon>
+          </Button>
+          <ButtonLink appearance="secondary-action-button" external={true}>
+            E-mail
+            <Icon>
+              <svg viewBox="0 0 1000 1000">
+                <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
+              </svg>
+            </Icon>
+          </ButtonLink>
+          <ButtonLink appearance="secondary-action-button" external={true}>
+            Twitter
+            <Icon>
+              <svg viewBox="0 0 1000 1000">
+                <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
+              </svg>
+            </Icon>
+          </ButtonLink>
+          <ButtonLink appearance="secondary-action-button" external={true}>
+            LinkedIn
+            <Icon>
+              <svg viewBox="0 0 1000 1000">
+                <rect fill="currentColor" x="0" y="0" width="1000" height="1000" />
+              </svg>
+            </Icon>
+          </ButtonLink>
+        </ActionGroup>
+      </PageContent>
+    </main>
   );
 }
