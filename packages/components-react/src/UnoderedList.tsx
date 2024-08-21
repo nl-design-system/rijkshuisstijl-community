f392265124a8633a5c5a38f155fadd1006c97237
Mees Work <Work@Meess-Laptop.local>
2024-08-21 16:03:17 +0200

    Created wrapper for UnorderedList component This wrapper adds a prop named 'Nested' which will add the class 'utrecht-unordered-list--nested' if present

--- packages/components-react/src/UnoderedList.tsx
+++ packages/components-react/src/UnoderedList.tsx
@@ -1,3 +1,30 @@
 import '@rijkshuisstijl-community/components-css/index.scss';
+import { UnorderedList as UtrechtUnorderedList } from '@utrecht/component-library-react';
+import { UnorderedListProps } from '@utrecht/component-library-react/dist/css-module';
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
 
-export { UnorderedList, UnorderedListItem } from '@utrecht/component-library-react/';
+interface RhcUnorderedListProps extends UnorderedListProps {
+  nested?: boolean;
+}
+
+export const UnorderedList = forwardRef(
+  (
+    { children, nested, ...restProps }: PropsWithChildren<RhcUnorderedListProps>,
+    ref: ForwardedRef<HTMLUListElement>,
+  ) => {
+    return (
+      <UtrechtUnorderedList
+        ref={ref}
+        {...restProps}
+        className={clsx({
+          'utrecht-unordered-list--nested': nested,
+        })}
+      >
+        {children}
+      </UtrechtUnorderedList>
+    );
+  },
+);
+
+UnorderedList.displayName = 'UnorderedList';
