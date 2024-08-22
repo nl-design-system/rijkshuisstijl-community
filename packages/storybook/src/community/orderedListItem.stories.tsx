9cb793aae380f3fe9111645da6cb4fab3aab50bb
Mees Work <Work@Meess-Laptop.local>
2024-08-22 10:50:48 +0200

    Feat: Added story for OrderedListItem

---
+++ packages/storybook/src/community/orderedListItem.stories.tsx
@@ -0,0 +1,26 @@
+import { OrderedList, OrderedListItem } from '@rijkshuisstijl-community/components-react';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Rijkshuisstijl/Ordered List/Ordered Item',
+  id: 'rijkshuisstijl-orderedListItem',
+  component: OrderedListItem,
+  decorators: [(Story) => <OrderedList>{Story()}</OrderedList>],
+  args: {
+    children: 'Ordered List item',
+  },
+  argTypes: {
+    value: {
+      type: { name: 'number', required: false },
+      table: {
+        category: 'HTML attribute',
+      },
+    },
+  },
+} satisfies Meta<typeof OrderedListItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
