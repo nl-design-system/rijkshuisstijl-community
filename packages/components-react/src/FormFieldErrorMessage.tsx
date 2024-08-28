17ecfe7f6f3e883f634b42385e2eb3f5d8536203
Adham AboHasson <aahasson@quintor.nl>
2024-08-28 15:30:35 +0200

    fix: change the name of `Icon to UtrechtIcon`

--- packages/components-react/src/FormFieldErrorMessage.tsx
+++ packages/components-react/src/FormFieldErrorMessage.tsx
@@ -2,7 +2,7 @@ import {
   FormFieldErrorMessage as UtrechtFormFieldErrorMessage,
   FormFieldErrorMessageProps as UtrechtFormFieldErrorMessageProps,
 } from '@utrecht/component-library-react';
-import { Icon } from '@utrecht/component-library-react/dist/css-module';
+import { Icon as UtrechtIcon } from '@utrecht/component-library-react/dist/css-module';
 import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
 import { ErrorIcon } from './icons';
 
@@ -12,9 +12,9 @@ export const FormFieldErrorMessage = forwardRef(
   ({ children, ...restProps }: PropsWithChildren<FormFieldErrorMessageProps>, ref: ForwardedRef<HTMLDivElement>) => (
     <UtrechtFormFieldErrorMessage ref={ref} {...restProps}>
       <span className={'utrecht-form-field-error-message--icon-container'}>
-        <Icon className={'utrecht-form-field-error-message--icon-container-icon'}>
+        <UtrechtIcon className={'utrecht-form-field-error-message--icon-container-icon'}>
           <ErrorIcon />
-        </Icon>{' '}
+        </UtrechtIcon>{' '}
         {children}
       </span>
     </UtrechtFormFieldErrorMessage>
