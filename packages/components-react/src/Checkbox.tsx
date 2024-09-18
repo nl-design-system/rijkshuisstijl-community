b67b3af104a6fa309162383cbaeb8dea1fb6ceef
Adham AboHasson <aahasson@quintor.nl>
2024-09-18 09:42:48 +0200

    fix: trying github commits, never again

--- packages/components-react/src/Checkbox.tsx
+++ packages/components-react/src/Checkbox.tsx
@@ -1,13 +1,1 @@
-import { CheckboxProps, Checkbox as UtrechtCheckbox } from '@utrecht/component-library-react';
-import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
-
-export { type CheckboxProps };
-
-export const Checkbox = forwardRef(
-  ({ ...Props }: PropsWithChildren<CheckboxProps>, ref: ForwardedRef<HTMLInputElement>) => {
-    return <UtrechtCheckbox ref={ref} {...Props} />;
-  },
-);
-export { Checkbox, type CheckboxProps } from '@utrecht/component-library-react;
-
-Checkbox.displayName = 'Checkbox';
+export { Checkbox, type CheckboxProps } from '@utrecht/component-library-react';
