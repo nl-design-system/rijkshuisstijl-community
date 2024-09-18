37f7524f36d0f313380e21cea01f001c739f3190
Ruben Smit <rsmit@quintor.local>
2024-09-18 16:43:49 +0200

    feat(fieldset): cleaned up storybook

--- packages/storybook/src/community/fieldset.stories.tsx
+++ packages/storybook/src/community/fieldset.stories.tsx
@@ -1,6 +1,11 @@
 /* @license CC0-1.0 */
 
-import { Fieldset, FieldsetLegend, type FieldsetProps, Paragraph } from '@rijkshuisstijl-community/components-react';
+import {
+  Fieldset,
+  FieldsetLegend,
+  type FieldsetProps,
+  FormFieldTextbox,
+} from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react';
 import clsx from 'clsx';
 import { ReactNode } from 'react';
@@ -23,52 +28,21 @@ const FormFieldsetStory = ({ children, legend, legendId, section, ...restProps }
 };
 
 const meta = {
-  title: 'Rijkshuisstijl/Form fieldset',
-  id: 'rijkshuisstijl-fieldset',
-  component: Fieldset,
-  argTypes: {
-    disabled: {
-      description: 'Disabled',
-      control: 'boolean',
-    },
-    children: {
-      description: 'Set the content of the fieldset',
-      control: 'text',
-    },
-    element: {
-      description: 'HTML element',
-      control: { type: 'select' },
-      options: ['div', 'fieldset'],
-    },
-    invalid: {
-      description: 'Invalid',
-      control: 'boolean',
-    },
-    legend: {
-      description: 'Set the content of the legend',
-      control: 'text',
-    },
-    section: {
-      description: 'Section appearance',
-      type: 'boolean',
-    },
-  },
+  title: 'Rijkshuisstijl/Fieldset',
+  id: 'rhc-fieldset',
+  component: FormFieldsetStory,
   args: {
     disabled: false,
-    element: 'fieldset',
-    children: '',
-    invalid: false,
-    legend: '',
-    section: false,
+    children: (
+      <>
+        <FormFieldTextbox label="Field A"></FormFieldTextbox>
+        <FormFieldTextbox label="Field B"></FormFieldTextbox>
+        <FormFieldTextbox label="Field C"></FormFieldTextbox>
+      </>
+    ),
+    legend: 'Legend',
   },
-  parameters: {
-    bugs: 'https://github.com/nl-design-system/utrecht/issues?q=is%3Aissue+is%3Aopen+label%3Acomponent%2Fform-fieldset',
-    nldesignsystem: 'https://nldesignsystem.nl/fieldset',
-    tokensPrefix: 'utrecht-form-fieldset',
-    status: {
-      type: 'WORK IN PROGRESS',
-    },
-  },
+  render: FormFieldsetStory,
 } satisfies Meta<typeof FormFieldsetStory>;
 
 export default meta;
@@ -76,11 +50,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    disabled: false,
-    children: <Paragraph>Fieldset body</Paragraph>,
-    legend: 'Fieldset legend',
-  },
   parameters: {
     docs: {
       description: {
@@ -92,47 +61,8 @@ export const Default: Story = {
   },
 };
 
-export const WithoutCSS: Story = {
-  args: {
-    disabled: false,
-    children: <Paragraph>Fieldset body</Paragraph>,
-    legend: 'Fieldset legend',
-  },
-  decorators: [(Story) => <div className="no-style-decorator">{Story()}</div>],
-  name: 'Without CSS',
-  parameters: {
-    chromatic: { disableSnapshot: true },
-    docs: {
-      description: {
-        story: 'When the CSS can not be loaded, the visual representation is still clear to the user.',
-      },
-    },
-  },
-};
-
-export const Div: Story = {
-  args: {
-    ...Default.args,
-    element: 'div',
-    legendId: 'e7d3424c-8034-4494-9a52-110c2e5f6f4c',
-  },
-  name: '<div> as markup',
-  parameters: {
-    docs: {
-      description: {
-        story: `Markup using only \`<div>\` HTML elements. This is not the recommended markup, but the CSS implementation should support this flexibility anyway.
-
-Semantics built into the \`<fieldset>\` and \`<legend>\` elements must now be specified explicitly: \`role="group"\` for the fieldset (but \`role="radiogroup"\` is OK too), and the legend must explicitly be associated with the group using \`aria-labelledby\`.
-
-The \`disabled\` effect from \`<fieldset>\` will no longer work, so all form controls must be set to \`disabled\` individually.`,
-      },
-    },
-  },
-};
-
 export const Disabled: Story = {
   args: {
-    ...Default.args,
     disabled: true,
   },
   name: 'Disabled',
@@ -140,36 +70,6 @@ export const Disabled: Story = {
     docs: {
       description: {
         story: 'Styling via the `.utrecht-form-fieldset__legend--disabled` class names.',
-      },
-    },
-  },
-};
-
-export const Section: Story = {
-  args: {
-    ...Default.args,
-    section: true,
-  },
-  name: 'Section',
-  parameters: {
-    docs: {
-      description: {
-        story: 'Styling via the `.utrecht-form-fieldset` and `.utrecht-form-fieldset--section` class names.',
-      },
-    },
-  },
-};
-
-export const Invalid: Story = {
-  args: {
-    ...Default.args,
-    invalid: true,
-  },
-  name: 'Invalid',
-  parameters: {
-    docs: {
-      description: {
-        story: 'Styling via the `.utrecht-form-fieldset__legend--invalid` class names.',
       },
     },
   },
