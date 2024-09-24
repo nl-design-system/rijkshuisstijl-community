47851f0e4aa76ad74ae77233c80aa2ba7ed278a1
Ruben Smit <rsmit@quintor.local>
2024-09-24 15:20:14 +0200

    feat(blockquote): added and implemented extra tokens and added other variations to storybook

--- packages/components-react/src/Blockquote.tsx
+++ packages/components-react/src/Blockquote.tsx
@@ -2,17 +2,18 @@ import {
   Blockquote as UtrechtBlockquote,
   BlockquoteProps as UtrechtBlockquoteProps,
 } from '@utrecht/component-library-react';
+import clsx from 'clsx';
 import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
 
 export type { UtrechtBlockquoteProps as BlockquoteProps };
 
 export const Blockquote = forwardRef(
   (
-    { children, attribution, ...restProps }: PropsWithChildren<UtrechtBlockquoteProps>,
+    { className, children, attribution, ...restProps }: PropsWithChildren<UtrechtBlockquoteProps>,
     ref: ForwardedRef<HTMLQuoteElement>,
   ) => (
-    <UtrechtBlockquote {...restProps} attribution={attribution} ref={ref}>
-      <div className="utrecht-blockquote__content">{children}</div>
+    <UtrechtBlockquote {...restProps} attribution={attribution} className={clsx('rhc-blockquote', className)} ref={ref}>
+      <div className="rhc-blockquote__content utrecht-blockquote__content">{children}</div>
     </UtrechtBlockquote>
   ),
 );
--- packages/storybook/src/community/blockquote.stories.tsx
+++ packages/storybook/src/community/blockquote.stories.tsx
@@ -2,6 +2,7 @@
 
 import { Blockquote } from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react';
+import { CSSProperties } from 'react';
 import readme from './blockquote.md?raw';
 
 const meta = {
@@ -38,5 +39,59 @@ export const Attribution: Story = {
   args: {
     children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
     attribution: '— Pippi Langkous',
+  },
+};
+
+export const blueCornerBorder: Story = {
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    style: {
+      '--utrecht-blockquote-border-color': '#154273',
+      '--utrecht-blockquote-border-block-end-width': '0.125rem',
+      '--utrecht-blockquote-border-inline-end-width': '0.125rem',
+      '--utrecht-blockquote-border-end-end-radius': '3rem',
+    } as unknown as CSSProperties,
+  },
+};
+
+export const pinkBackground: Story = {
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    style: {
+      '--utrecht-blockquote-background-color': '#f6bde1',
+      '--utrecht-blockquote-border-end-start-radius': '3rem',
+      '--utrecht-blockquote-padding': '2.5rem',
+    } as unknown as CSSProperties,
+  },
+};
+
+export const pinkLeftBorder: Story = {
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    style: {
+      '--utrecht-blockquote-border-color': '#f6bde1',
+      '--utrecht-blockquote-border-inline-start-width': '0.75rem',
+      '--utrecht-blockquote-padding-inline-start': '2.5rem',
+      '--utrecht-blockquote-padding-inline-end': '2.5rem',
+      '--utrecht-blockquote-padding-block-start': '1.5rem',
+      '--utrecht-blockquote-padding-block-end': '1.5rem',
+    } as unknown as CSSProperties,
+  },
+};
+
+export const pinkCornerBorder: Story = {
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    style: {
+      '--utrecht-blockquote-border-color': '#f6bde1',
+      '--utrecht-blockquote-border-block-start-width': '0.25rem',
+      '--utrecht-blockquote-border-inline-start-width': '0.25rem',
+      '--utrecht-blockquote-border-start-start-radius': '3rem',
+      '--utrecht-blockquote-padding': '2.5rem',
+    } as unknown as CSSProperties,
   },
 };
