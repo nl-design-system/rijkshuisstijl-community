40c2cadd6730f9e44aed145c3413c272d3f7b4bc
Ruben Smit <rsmit@quintor.nl>
2024-09-25 09:51:00 +0200

    Feature/nav (#674)
    #555
    
    ---------
    
    Co-authored-by: Ruben Smit <rsmit@quintor.local>

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -49,6 +49,7 @@ export { Hero, type HeroProps } from './Hero';
 export { Image, type ImageProps } from './Image';
 export { Link, type LinkProps } from './Link';
 export { Logo, type LogoProps } from './Logo';
+export { NavBar, type NavBarItemProps, type NavBarProps } from './NavBar';
 export { Paragraph, type ParagraphProps } from './Paragraph';
 export { Radio, type RadioProps } from './Radio';
 export { Separator, type SeparatorProps } from './Separator';
