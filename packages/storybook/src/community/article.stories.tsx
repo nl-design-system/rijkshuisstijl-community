8c11a6463c884dd0d0f68d0201ff698d65042442
Adham Abo Hasson <aahasson@quintor.nl>
2024-09-26 09:20:41 +0200

    fix: no text in article.stories.tsx (#676)

--- packages/storybook/src/community/article.stories.tsx
+++ packages/storybook/src/community/article.stories.tsx
@@ -9,7 +9,19 @@ const meta = {
   id: 'rhc-article',
   component: Article,
   argTypes: {},
-  args: {},
+  args: {
+    children: [
+      <p>
+        Lorem ipsum dolor sit amet. Aut amet quibusdam et atque soluta id unde provident non sequi dolor rem iusto
+        expedita eum voluptates asperiores aut quae modi. 33 ipsa recusandae et repudiandae optio est ullam fugit sit
+        dolores praesentium sed pariatur aspernatur. Et voluptatibus expedita aut odit sint vel eius dolores et nobis
+        tempora et cupiditate rerum. Qui mollitia maiores non unde repellat a totam corrupti et tempore nihil est earum
+        facilis hic odio nisi est eligendi quidem? Aut similique quasi non repellat expedita qui magni tempore ab
+        aperiam voluptatum sit quia atque et enim alias ea voluptas internos. Qui quia eius vel alias dolores et
+        expedita ipsum.
+      </p>,
+    ],
+  },
   tags: ['autodocs'],
   parameters: {
     docs: {
