9723538ec3b3260459d6a986b80e0bf255e055da
Ruben Smit <rsmit@quintor.nl>
2024-09-26 12:48:33 +0200

    Feature/logo (inverted colors) (#662)
    #567
    
    ---------
    
    Co-authored-by: Ruben Smit <rsmit@quintor.local>

--- packages/storybook/src/community/logo.stories.tsx
+++ packages/storybook/src/community/logo.stories.tsx
@@ -1,11 +1,48 @@
-import { Logo } from '@rijkshuisstijl-community/components-react';
+import { Logo, type LogoProps } from '@rijkshuisstijl-community/components-react';
 import { DutchMapIcon } from '@rijkshuisstijl-community/components-react/src/icons';
 import { Meta, StoryObj } from '@storybook/react';
+import { CSSProperties } from 'react';
+
+interface LogoCSSProperties extends CSSProperties {
+  '--rhc-logo-color': string;
+  '--rhc-logo-img-background-color': string;
+  '--rhc-logo-img-color': string;
+}
+
+interface LogoStoryProps extends LogoProps {
+  imgBackgroundColor?: string;
+  imgIconColor?: string;
+  textColor?: string;
+}
+
+const LogoStory = ({ imgBackgroundColor, textColor, imgIconColor, ...args }: LogoStoryProps) => (
+  <>
+    <Logo
+      {...args}
+      style={
+        {
+          '--rhc-logo-color': textColor || '#154273',
+          '--rhc-logo-img-background-color': imgBackgroundColor || '#ffffff',
+          '--rhc-logo-img-color': imgIconColor || '#154273',
+        } as LogoCSSProperties
+      }
+    >
+      <DutchMapIcon />
+    </Logo>
+  </>
+);
 
 const meta = {
   title: 'Rijkshuisstijl/Logo',
   id: 'rhc-logo',
-  component: Logo,
+  component: LogoStory,
+  args: {
+    organisation: '',
+    subtitle: '',
+    imgBackgroundColor: '#ffffff',
+    imgIconColor: '#154273',
+    textColor: '#154273',
+  },
   argTypes: {
     organisation: {
       name: 'organisation',
@@ -15,8 +52,30 @@ const meta = {
       name: 'subtitle',
       type: { name: 'string', required: false },
     },
+    imgBackgroundColor: {
+      control: 'color',
+      description: 'Changes background color of the image',
+      table: {
+        category: 'Demo',
+      },
+    },
+    imgIconColor: {
+      control: 'color',
+      description: 'Changes color of the image icon',
+      table: {
+        category: 'Demo',
+      },
+    },
+    textColor: {
+      control: 'color',
+      description: 'Changes text color',
+      table: {
+        category: 'Demo',
+      },
+    },
   },
-} satisfies Meta<typeof Logo>;
+  render: LogoStory,
+} satisfies Meta<typeof LogoStory>;
 
 export default meta;
 
@@ -27,25 +86,9 @@ export const Default: Story = {
   },
 };
 
-export const Organisation: Story = {
-  args: {
-    organisation: 'Voorbeeld organisatie',
-  },
-};
-
-export const OrganisationSubtitle: Story = {
+export const WithSubtitle: Story = {
   args: {
     organisation: 'Voorbeeld organisatie',
     subtitle: 'Voorbeeld sub-title',
   },
-  name: 'Organisation + subtitle',
-};
-
-export const ExampleIcon: Story = {
-  args: {
-    organisation: 'Voorbeeld organisatie',
-    subtitle: 'Voorbeeld sub-title',
-    children: <DutchMapIcon />,
-  },
-  name: 'Organisation + subtitle + example icon',
 };
