4db97835f4a4ad4112a6c7b11b2f95d3765d9407
Ruben Smit <rsmit@quintor.local>
2024-10-09 13:20:04 +0200

    chore(toggletip): removed unnecessary properties

--- packages/components-react/src/Toggletip.tsx
+++ packages/components-react/src/Toggletip.tsx
@@ -1,20 +1,18 @@
 import clsx from 'clsx';
 import { DetailsHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren, ReactNode } from 'react';
-import { Alert, type AlertProps } from './Alert';
+import { Alert } from './Alert';
 import { Icon } from './icon/Icon';
-import { type RijkshuisstijlIconID } from './icon/IconTypes';
 
 export interface ToggletipProps extends DetailsHTMLAttributes<HTMLDetailsElement> {
   className?: string;
-  alertType?: AlertProps['type'];
-  icon?: RijkshuisstijlIconID | ReactNode;
 }
 
 export const ToggletipButton = forwardRef(
-  ({ icon }: { icon?: RijkshuisstijlIconID | ReactNode }, ref: ForwardedRef<HTMLButtonElement>) => {
+  ({ children }: { children?: ReactNode }, ref: ForwardedRef<HTMLButtonElement>) => {
     return (
       <button className="rhc-toggletip__button" ref={ref}>
-        <Icon className="rhc-toggletip__icon" icon={icon ?? 'info'} />
+        <Icon className="rhc-toggletip__icon" icon="info" />
+        {children}
       </button>
     );
   },
@@ -22,13 +20,10 @@ export const ToggletipButton = forwardRef(
 ToggletipButton.displayName = 'ToggletipButton';
 
 export const ToggletipContent = forwardRef(
-  (
-    { alertType, children }: { alertType?: AlertProps['type']; children: ReactNode },
-    ref: ForwardedRef<HTMLDivElement>,
-  ) => {
+  ({ children }: { children: ReactNode }, ref: ForwardedRef<HTMLDivElement>) => {
     return (
       <div className="rhc-toggletip__content" ref={ref}>
-        <Alert type={alertType ?? 'info'}>{children}</Alert>
+        <Alert type="info">{children}</Alert>
       </div>
     );
   },
@@ -36,16 +31,13 @@ export const ToggletipContent = forwardRef(
 ToggletipContent.displayName = 'ToggletipContent';
 
 export const Toggletip = forwardRef(
-  (
-    { children, className, icon, alertType, ...restProps }: PropsWithChildren<ToggletipProps>,
-    ref: ForwardedRef<HTMLDetailsElement>,
-  ) => {
+  ({ children, className, ...restProps }: PropsWithChildren<ToggletipProps>, ref: ForwardedRef<HTMLDetailsElement>) => {
     return (
       <details className={clsx('rhc-toggletip', className)} ref={ref} {...restProps}>
         <summary className="rhc-toggletip__summary">
-          <ToggletipButton icon={icon} />
+          <ToggletipButton />
         </summary>
-        <ToggletipContent alertType={alertType}>{children}</ToggletipContent>
+        <ToggletipContent>{children}</ToggletipContent>
       </details>
     );
   },
--- packages/storybook/src/community/toggletip.stories.tsx
+++ packages/storybook/src/community/toggletip.stories.tsx
@@ -1,34 +1,12 @@
-import { iconOptions, Toggletip } from '@rijkshuisstijl-community/components-react';
+import { Toggletip } from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
   title: 'Rijkshuisstijl/Toggletip',
   id: 'rhc-toggletip',
   component: Toggletip,
-  argTypes: {
-    alertType: {
-      description: 'Alert type',
-      control: { type: 'select' },
-      options: [undefined, 'info', 'warning', 'error', 'success'],
-      table: {
-        defaultValue: { summary: 'info' },
-        type: { summary: 'string' },
-      },
-    },
-    icon: {
-      description: 'Toggletip icon',
-      control: { type: 'select' },
-      options: iconOptions,
-      table: {
-        defaultValue: { summary: 'info' },
-        type: { summary: 'string' },
-      },
-    },
-  },
   args: {
     children: 'Lorem ipsum dolor sit amet, consecteur ad * isicing elit, sed do eiusmod *',
-    icon: 'info',
-    alertType: 'info',
   },
 } satisfies Meta<typeof Toggletip>;
 
