cfbc2ad3bc4510208adb56681e7dd34e54c9910e
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-10-22 07:16:33 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@d41c4ff22c1de778840211754132d62615de0aac 🚀

---
+++ assets/checkbox-group.stories-gdjRIC0n.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{C as r,d as s}from"./index.esm-B_plQxKZ.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";/* empty css              */import"./index.esm-ByJjFMhT.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./extends-CF3RwP-h.js";const c=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community CheckboxGroup component
+
+[NL design system](https://www.nldesignsystem.nl/checkbox-group/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=954-2226&node-type=canvas&t=HiNKOQhf1hQtLZrr-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/462)
+
+## Usage
+
+\`\`\`tsx
+import { CheckboxGroup, Checkbox } from '@rijkshuisstijl-community/components-react';
+
+<CheckboxGroup>
+  <Checkbox>Checkbox 1</Checkbox>
+  <Checkbox>Checkbox 2</Checkbox>
+</CheckboxGroup>;
+\`\`\`
+`,C={title:"Rijkshuisstijl/CheckboxGroup",id:"rhc-checkbox-group",component:r,parameters:{docs:{description:{component:c}}}},o={args:{children:[e.jsx(s,{label:"Checkbox 1"}),e.jsx(s,{label:"Checkbox 2"}),e.jsx(s,{label:"Checkbox 3"})]}};var t,i,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: [<FormFieldCheckboxOption label="Checkbox 1" />, <FormFieldCheckboxOption label="Checkbox 2" />, <FormFieldCheckboxOption label="Checkbox 3" />]
+  }
+}`,...(n=(i=o.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,C as default};
