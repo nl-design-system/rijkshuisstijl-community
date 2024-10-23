7dccaf7116d0811f1597ae0f8fb0e84add5581c3
Mees Work <Work@Meess-Laptop.local>
2024-10-23 10:55:05 +0200

    feedback: deleted onused props and classes

--- packages/components-react/src/Keep.tsx
+++ packages/components-react/src/Keep.tsx
@@ -3,30 +3,11 @@ import { forwardRef, PropsWithChildren } from 'react';
 
 interface keepProps {
   keep: 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left';
-  nested: boolean;
   borderRadius: 's' | 'm' | 'l';
-  firstChild?: string;
-  secondChild?: string;
 }
 
-export const Keep = forwardRef(
-  ({ keep, children, firstChild, secondChild, nested, borderRadius }: PropsWithChildren<keepProps>) => {
-    return (
-      <div
-        className={clsx(
-          {
-            'rhc-keep-nested': nested,
-            'rhc-keep': !nested,
-          },
-          `rhc-keep-${keep}-${borderRadius}`,
-          firstChild !== undefined ? `rhc-keep-child rhc-keep-first-child-${firstChild}` : firstChild,
-          secondChild !== undefined ? `rhc-keep-child rhc-keep-second-child-${secondChild}` : secondChild,
-        )}
-      >
-        {children}
-      </div>
-    );
-  },
-);
+export const Keep = forwardRef(({ keep, children, borderRadius }: PropsWithChildren<keepProps>) => {
+  return <div className={clsx('rhc-keep', `rhc-keep-${keep}-${borderRadius}`)}>{children}</div>;
+});
 
 Keep.displayName = 'Keep';
