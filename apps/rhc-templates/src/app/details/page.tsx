39cad50102eb913d6ab3bce8c3b552307457b8ef
Ruben Smit <rsmit@quintor.local>
2024-10-23 10:10:01 +0200

    fix(templates): changed template styling to fix backgrounds and alignment

--- apps/rhc-templates/src/app/details/page.tsx
+++ apps/rhc-templates/src/app/details/page.tsx
@@ -13,6 +13,7 @@ import {
   Separator,
   LinkList,
   LinkListLink,
+  PageContent,
   PageHeader,
 } from '@rijkshuisstijl-community/components-react';
 import { UtrechtIconChevronRight } from '@utrecht/web-component-library-react';
@@ -21,81 +22,83 @@ import { RichText } from '@utrecht/component-library-react/dist/css-module';
 export default function Details() {
   return (
     <>
-      <div className="rhc-background-color-white">
-        <PageHeader>
-          <BreadcrumbNav>
-            <BreadcrumbNavLink href="/" rel="home" index={0}>
-              Home
-            </BreadcrumbNavLink>
-            <BreadcrumbNavLink href="/a/" index={1}>
-              Onderwerpen
-            </BreadcrumbNavLink>
-            <BreadcrumbNavLink href="/a/b/" rel="up" index={2}>
-              Werktijden
-            </BreadcrumbNavLink>
-          </BreadcrumbNav>
-          <div className="header-image">
-            <div className="header-image-textbox">
-              <Heading level={1}>Veel bekeken</Heading>
+      <div className="rhc-main-content">
+        <PageContent className="container page-content rhc-page-content rhc-background-color-white">
+          <PageHeader>
+            <BreadcrumbNav>
+              <BreadcrumbNavLink href="/" rel="home" index={0}>
+                Home
+              </BreadcrumbNavLink>
+              <BreadcrumbNavLink href="/a/" index={1}>
+                Onderwerpen
+              </BreadcrumbNavLink>
+              <BreadcrumbNavLink href="/a/b/" rel="up" index={2}>
+                Werktijden
+              </BreadcrumbNavLink>
+            </BreadcrumbNav>
+            <div className="header-image">
+              <div className="header-image-textbox">
+                <Heading level={1}>Veel bekeken</Heading>
+                <LinkList>
+                  <LinkListLink>Wat zijn de wettelijke regels voor mijn werktijden en rusttijden?</LinkListLink>
+                  <LinkListLink>Wat zijn de wettelijke regels voor pauzes tijdens mijn werk?</LinkListLink>
+                </LinkList>
+              </div>
+            </div>
+            <div className="sub-navigation">
               <LinkList>
-                <LinkListLink>Wat zijn de wettelijke regels voor mijn werktijden en rusttijden?</LinkListLink>
-                <LinkListLink>Wat zijn de wettelijke regels voor pauzes tijdens mijn werk?</LinkListLink>
+                <LinkListLink>Regels voor rijtijden en rusttijden bij wegvervoer</LinkListLink>
+                <LinkListLink>Vraag en antwoord</LinkListLink>
+                <LinkListLink>Documenten</LinkListLink>
               </LinkList>
             </div>
-          </div>
-          <div className="sub-navigation">
-            <LinkList>
-              <LinkListLink>Regels voor rijtijden en rusttijden bij wegvervoer</LinkListLink>
-              <LinkListLink>Vraag en antwoord</LinkListLink>
-              <LinkListLink>Documenten</LinkListLink>
-            </LinkList>
-          </div>
-        </PageHeader>
-        <main className="rhc-main-content">
-          <Article>
-            <RichText>
-              <Paragraph>
-                De Arbeidstijdenwet geeft regels voor werktijden, pauzes en rusttijden van werknemers. Met deze regels
-                wil de Rijksoverheid werknemers beschermen tegen te lange werkdagen. Maar ook de combinatie van werk,
-                privé en zorgtaken gemakkelijker maken.
-              </Paragraph>
-              <Separator></Separator>
-              <Heading level={1}>Vraag en antwoord</Heading>
-              <LinkList>
-                <LinkListLink>Wat zijn de regels bij consignatie (oproepdienst)?</LinkListLink>
-                <LinkListLink>Hoe vaak mag ik nachtdienst hebben?</LinkListLink>
-                <LinkListLink>Welke brochures zijn er over Arbeidstijdenwet en Arbeidstijdenbesluit?</LinkListLink>
-                <LinkListLink>Wanneer geldt de Arbeidstijdenwet niet (of gedeeltelijk) voor mij?</LinkListLink>
-                <LinkListLink>Meer vragen en antwoorden</LinkListLink>
-              </LinkList>
-              <Heading level={1}>Uitgelicht</Heading>
-              <div className="image-group">
-                <div className="image-group-box">
-                  <Figure className="unstarted">
-                    <Image
-                      alt="Multicolored tulip field"
-                      width="640"
-                      height="763"
-                      src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
-                    ></Image>
-                    <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
-                  </Figure>
+          </PageHeader>
+          <main className="rhc-main-content">
+            <Article>
+              <RichText>
+                <Paragraph>
+                  De Arbeidstijdenwet geeft regels voor werktijden, pauzes en rusttijden van werknemers. Met deze regels
+                  wil de Rijksoverheid werknemers beschermen tegen te lange werkdagen. Maar ook de combinatie van werk,
+                  privé en zorgtaken gemakkelijker maken.
+                </Paragraph>
+                <Separator></Separator>
+                <Heading level={1}>Vraag en antwoord</Heading>
+                <LinkList>
+                  <LinkListLink>Wat zijn de regels bij consignatie (oproepdienst)?</LinkListLink>
+                  <LinkListLink>Hoe vaak mag ik nachtdienst hebben?</LinkListLink>
+                  <LinkListLink>Welke brochures zijn er over Arbeidstijdenwet en Arbeidstijdenbesluit?</LinkListLink>
+                  <LinkListLink>Wanneer geldt de Arbeidstijdenwet niet (of gedeeltelijk) voor mij?</LinkListLink>
+                  <LinkListLink>Meer vragen en antwoorden</LinkListLink>
+                </LinkList>
+                <Heading level={1}>Uitgelicht</Heading>
+                <div className="image-group">
+                  <div className="image-group-box">
+                    <Figure className="unstarted">
+                      <Image
+                        alt="Multicolored tulip field"
+                        width="640"
+                        height="763"
+                        src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
+                      ></Image>
+                      <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
+                    </Figure>
+                  </div>
+                  <div className="image-group-box">
+                    <Figure>
+                      <Image
+                        alt="Multicolored tulip field"
+                        width="640"
+                        height="763"
+                        src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
+                      ></Image>
+                      <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
+                    </Figure>
+                  </div>
                 </div>
-                <div className="image-group-box">
-                  <Figure>
-                    <Image
-                      alt="Multicolored tulip field"
-                      width="640"
-                      height="763"
-                      src="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/0bfd32af3f34ff7ce62f4769fbec8895720dde75/proprietary/assets/src/placeholder.jpg"
-                    ></Image>
-                    <FigureCaption>Bijschrift (figcaption) van een afbeelding)</FigureCaption>
-                  </Figure>
-                </div>
-              </div>
-            </RichText>
-          </Article>
-        </main>
+              </RichText>
+            </Article>
+          </main>
+        </PageContent>
       </div>
       <Footer
         heading="De Rijksoverheid. Voor Nederland"
