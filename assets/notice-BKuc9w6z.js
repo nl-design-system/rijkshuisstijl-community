8cbb53fc4fabff33df809904723990b2fdd379cb
MeesD94 <mdubbe@quintor.nl>
2024-10-29 13:27:04 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@659cb88f2aef9d3e56bc96a5f54029a2a79147df 🚀

---
+++ assets/notice-BKuc9w6z.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ag as i,ah as a}from"./chunk-HLWAVYOI-D1PJYess.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B4WFVnyY.js";import"../sb-preview/runtime.js";import"./react-18-CxOrbeQH.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./extends-CF3RwP-h.js";import"./setPrototypeOf-CqX0LrMY.js";import"./index-DSgSp0X8.js";const s=`# Auteursrecht {auteur}
+
+Copyright (c) {jaar} {auteur}
+
+## Logo en huisstijl
+
+Op het huisstijl en logo zijn auteursrechten van toepassing. Het gebruik van logo en huisstijl is alleen toegestaan voor gebruik door {auteur}.
+
+Wanneer je een bewerking van de software wilt gebruiken voor andere doeleinden, mag je niet het logo van {auteur} gebruiken en je ontwerpt een eigen huisstijl.
+
+## Lettertype
+
+Lettertypes die worden gebruikt voor de huisstijl zijn niet allemaal gratis en open source. Let op dat bij gebruik van die bijgeleverde lettertypes je een (betaalde) licentie regelt. Pas anders de configuratie aan om minder of andere lettertypes te gebruiken.
+
+## Toestemming
+
+Wanneer je het logo of de huisstijl wilt gebruiken kun je voor toestemming contact opnemen met {contactpersoon}.
+`;function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Example/Toestemming voor gebruik"}),`
+`,e.jsx(a,{children:s})]})}function x(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r()}export{x as default};
---
+++ assets/orderedList.stories-BLh4rqbT.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{b as O,O as r}from"./index.esm-DN4TXdWq.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./extends-CF3RwP-h.js";const L=`# Rijkshuisstijl Community ordered list component
+
+Opsomming waar de volgorde van items in de lijst niet van belang is.
+
+## Terminologie
+
+- **unordered list**: uitgeschreven versie van de HTML afkorting \`<ol>\`
+- **list item**: uitgeschreven van van [HTML \`<li>\`](https://html.spec.whatwg.org/multipage/grouping-content.html#the-li-element). Ook [in ARIA is het \`role="listitem"\`](https://www.w3.org/TR/wai-aria-1.2/#listitem)
+`,I={title:"Rijkshuisstijl/Ordered List",id:"rhc-ordered-list",component:O,args:{children:""},parameters:{docs:{description:{component:L}}}},t={args:{children:[e.jsx(r,{children:"Ordered List item 1"}),e.jsx(r,{children:"Ordered List item 2"}),e.jsx(r,{children:"Ordered List item 3"})]}},i={args:{children:[e.jsx(r,{children:"Ordered List item 1"}),e.jsxs(r,{children:["Ordered List item 2",e.jsxs(O,{children:[e.jsx(r,{children:"Order List item 2.1"}),e.jsx(r,{children:"Order List item 2.2"})]})]}),e.jsx(r,{children:"Ordered List item 3"})]}};var d,s,n;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedListItem>Ordered List item 1</OrderedListItem>, <OrderedListItem>Ordered List item 2</OrderedListItem>, <OrderedListItem>Ordered List item 3</OrderedListItem>]
+  }
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var m,o,a;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedListItem>Ordered List item 1</OrderedListItem>, <OrderedListItem>
+        Ordered List item 2
+        <OrderedList>
+          <OrderedListItem>Order List item 2.1</OrderedListItem>
+          <OrderedListItem>Order List item 2.2</OrderedListItem>
+        </OrderedList>
+      </OrderedListItem>, <OrderedListItem>Ordered List item 3</OrderedListItem>]
+  }
+}`,...(a=(o=i.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const j=["DefaultOrderedList","NestedOrderedList"];export{t as DefaultOrderedList,i as NestedOrderedList,j as __namedExportsOrder,I as default};
