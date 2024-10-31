96bdf58493947af9735a351f5c4e80ace358b63d
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2024-10-31 09:34:43 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@086f3f93575e461f5f220ca623f87d766532ceeb 🚀

---
+++ assets/icon.stories-BW863FIl.js
@@ -0,0 +1,14 @@
+import{j as m}from"./jsx-runtime-BlAj40OV.js";import{m as p,I as i,o as d}from"./index.esm-OSDnnu3r.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";/* empty css              */import"./index.esm-DN4TXdWq.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./extends-CF3RwP-h.js";/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var l=p("outline","brand-x","IconBrandX",[["path",{d:"M4 4l11.733 16h4.267l-11.733 -16z",key:"svg-0"}],["path",{d:"M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772",key:"svg-1"}]]);const B={title:"Rijkshuisstijl/Icon",id:"rhc-icon",component:i,argTypes:{icon:{control:{type:"select"},options:d}},parameters:{status:{type:"STABLE"}}},o={args:{icon:"home"}},r={args:{children:m.jsx(l,{})}};var e,s,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    icon: 'home'
+  }
+}`,...(t=(s=o.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,n,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: <IconBrandX />
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const k=["Default","CustomIcon"];export{r as CustomIcon,o as Default,k as __namedExportsOrder,B as default};
---
+++ assets/navigation-list.stories-DJx0mxsA.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{t as a,s as i}from"./index.esm-OSDnnu3r.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";/* empty css              */import"./index.esm-DN4TXdWq.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./extends-CF3RwP-h.js";const m=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Navigation List component
+
+NL design system - geen | [Figma](https://www.figma.com/design/Nv5EsCW9ioWBUSi9m9JqOa/Local---Rijkshuisstijl---Bibliotheek?node-id=4074-1580&node-type=canvas&t=HuDzyBW9wHdB2QVh-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/557)
+
+## Usage
+
+\`\`\`tsx
+import { NavigationList, NavigationListItem, NavigationListLink } from '@rijkshuisstijl-community/components-react';
+
+<NavigationList>
+  <NavigationListItem description="Uw gegevens, familie en identiteitsbewijs" href="#" icon="user" label="Identiteit" />
+  <NavigationListItem
+    description="Uw inkomen, toeslagen, bijdragen en belastingen"
+    href="#"
+    icon="currency-euro"
+    label="Financiën"
+  />
+</NavigationList>;
+\`\`\`
+`,w={title:"Rijkshuisstijl/NavigationList",component:a,parameters:{status:{type:"STABLE"},docs:{description:{component:m}}},argTypes:{}},n={render:s=>e.jsxs(a,{...s,children:[e.jsx(i,{description:"Uw gegevens, familie en identiteitsbewijs",href:"#",icon:"user",label:"Identiteit"}),e.jsx(i,{description:"Uw inkomen, toeslagen, bijdragen en belastingen",href:"#",icon:"currency-euro",label:"Financiën"}),e.jsx(i,{description:"Uw pensioen, arbeidsgegevens en uitkeringen",href:"#",icon:"briefcase",label:"Werk"})]})},t={render:s=>e.jsx("div",{style:{width:"380px"},children:e.jsxs(a,{...s,children:[e.jsx(i,{description:"Uw gegevens, familie en identiteitsbewijs",href:"#",icon:"user",label:"Identiteit"}),e.jsx(i,{description:"Uw inkomen, toeslagen, bijdragen en belastingen",href:"#",icon:"currency-euro",label:"Financiën"}),e.jsx(i,{description:"Uw pensioen, arbeidsgegevens en uitkeringen",href:"#",icon:"briefcase",label:"Werk"})]})})};var r,o,c;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: (args: NavigationListProps) => <NavigationList {...args}>
+      <NavigationListItem description="Uw gegevens, familie en identiteitsbewijs" href={'#'} icon="user" label="Identiteit" />
+      <NavigationListItem description="Uw inkomen, toeslagen, bijdragen en belastingen" href={'#'} icon="currency-euro" label="Financiën" />
+      <NavigationListItem description="Uw pensioen, arbeidsgegevens en uitkeringen" href={'#'} icon="briefcase" label="Werk" />
+    </NavigationList>
+}`,...(c=(o=n.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var g,l,d;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: (args: NavigationListProps) => <div style={{
+    width: '380px'
+  }}>
+      <NavigationList {...args}>
+        <NavigationListItem description="Uw gegevens, familie en identiteitsbewijs" href={'#'} icon="user" label="Identiteit" />
+        <NavigationListItem description="Uw inkomen, toeslagen, bijdragen en belastingen" href={'#'} icon="currency-euro" label="Financiën" />
+        <NavigationListItem description="Uw pensioen, arbeidsgegevens en uitkeringen" href={'#'} icon="briefcase" label="Werk" />
+      </NavigationList>
+    </div>
+}`,...(d=(l=t.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const N=["Default","SmallContainer"];export{n as Default,t as SmallContainer,N as __namedExportsOrder,w as default};
