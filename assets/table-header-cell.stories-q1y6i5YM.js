df86c58f5eec4381c93c662f5d89d65ce55d0ba0
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2024-11-04 10:52:16 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@d0563b2b564c14d24fdb7b537e1b0fb222225655 🚀

---
+++ assets/table-header-cell.stories-q1y6i5YM.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{G as m}from"./index.esm-Aq17w2Zn.js";import{T as p,s as g,r as h}from"./index.esm-BilPrETv.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";/* empty css              */import"./objectWithoutProperties-BJfzkg-c.js";import"./extends-CF3RwP-h.js";const u=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community TableHeaderCell component
+
+[NL design system](https://www.nldesignsystem.nl/table/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1561-6448&t=texUKkpCqzgFVuch-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/465)
+
+## Usage
+
+\`\`\`jsx
+import { TableHeaderCell } from '@rijkshuisstijl/community-components';
+
+<TableHeaderCell align={'center'} aria-sort={'descending'} scope={'col'} withSorting>
+  Header
+</TableHeaderCell>;
+\`\`\`
+`,n=({children:s,withSorting:a,alignCell:i,scope:c,...d})=>e.jsx(p,{children:e.jsx(g,{children:e.jsx(h,{children:e.jsx(m,{...d,alignCell:i,scope:c,withSorting:a,children:s})})})}),H={title:"Rijkshuisstijl/Table/Table Header Cell",id:"rhc-table-header-cell",component:n,tags:["autodocs"],argTypes:{withSorting:{type:"boolean"},"aria-sort":{options:["ascending","descending","none"],control:{type:"select"}},scope:{options:["row","col"],control:{type:"select"}},alignCell:{options:["start","end","center"],control:{type:"select"}}},render:s=>e.jsx(n,{...s}),parameters:{status:{type:"STABLE"},docs:{description:{component:u}}}},t={args:{children:"Label",scope:"col",withSorting:!1,alignCell:"start"}};var o,r,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Label',
+    scope: 'col',
+    withSorting: false,
+    alignCell: 'start'
+  }
+}`,...(l=(r=t.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const k=["Default"];export{t as Default,k as __namedExportsOrder,H as default};
