790dbbf7d95ecb66afe489cb735b63267e2620a0
aakminasi <arefcoding@gmail.com>
2024-11-05 11:21:15 +0100

    modified formfield unit test

--- packages/components-react/src/FormField.test.tsx
+++ packages/components-react/src/FormField.test.tsx
@@ -4,8 +4,20 @@ import { render } from '@testing-library/react';
 import { FormField } from '.';
 
 describe('FormField', () => {
+  const defaultProps = {
+    label: 'Test Label',
+    description: 'Test Description',
+    errorMessage: 'Test Error Message',
+    input: <input data-testid="form-input" />,
+    status: 'Test Status',
+    invalid: true,
+    statusId: 'status-id',
+    errorMessageId: 'error-message-id',
+    descriptionId: 'description-id',
+    id: 'test-id',
+  };
   it('renders a visible element', () => {
-    const { container } = render(<FormField />);
+    const { container } = render(<FormField {...defaultProps} />);
 
     const formField = container.querySelector(':only-child');
 
