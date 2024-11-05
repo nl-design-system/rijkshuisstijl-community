d90026e30c114d674c14d35eb1a1365c4ad38bab
Rerbun <ruben.smit4@gmail.com>
2024-11-05 11:43:11 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@29fdaa510238e01973ff1f9baa21faa798082bd6 🚀

---
+++ assets/message-list.stories-kE5OyGZv.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-BoFrXSaA.js";import{r as n,M as s}from"./index.esm-DfiX5HAn.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";/* empty css              */import"./index.esm-BhO2p2eB.js";import"./objectWithoutProperties-BJfzkg-c.js";const g=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Message List component
+
+NL design system - geen | [Figma](https://www.figma.com/design/Nv5EsCW9ioWBUSi9m9JqOa/Local---Rijkshuisstijl---Bibliotheek?node-id=4070-5888&node-type=frame&t=xhXU5ugIkPqvOZwt-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/556)
+
+## Usage
+
+\`\`\`tsx
+import { MessageList, MessageListItem } from '@rijkshuisstijl-community/components-react';
+
+<MessageList>
+  <MessageListItem description={'Uw pensioenoverzicht'} href={'#'} label={'Pensioenfonds'} metaData={'Meta Data'} />
+  <MessageListItem description={'Herinnering APK'} href={'#'} label={'RDW'} metaData={'Meta Data'} />
+  <MessageListItem
+    description={'Aanslag OZB'}
+    href={'#'}
+    label={'Samenwerkings-verband Haaglanden'}
+    metaData={'Meta Data'}
+  />
+</MessageList>;
+\`\`\`
+`,D={title:"Rijkshuisstijl/Message List",id:"rhc-message-list",component:n,args:{},render:()=>e.jsxs(n,{children:[e.jsx(s,{description:"Uw pensioenoverzicht",href:"#",label:"Pensioenfonds",metaData:"01-05-2024",withBadge:{ariaLabel:"Nieuw",role:"status"}}),e.jsx(s,{description:"Herinnering APK",href:"#",label:"RDW",metaData:"04-04-2024"}),e.jsx(s,{description:"Aanslag OZB",href:"#",label:"Samenwerkings-verband Haaglanden",metaData:"04-04-2024"})]}),parameters:{status:{type:"STABLE"},docs:{description:{component:g}}}},a={},t={render:c=>e.jsx("div",{style:{width:"380px"},children:e.jsxs(n,{...c,children:[e.jsx(s,{description:"Uw pensioenoverzicht",href:"#",label:"Pensioenfonds",metaData:"01-05-2024",withBadge:{ariaLabel:"Nieuw",role:"status"}}),e.jsx(s,{description:"Herinnering APK",href:"#",label:"RDW",metaData:"04-04-2024"}),e.jsx(s,{description:"Aanslag OZB",href:"#",label:"Samenwerkings-verband Haaglanden",metaData:"04-04-2024"})]})})};var i,r,o;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(o=(r=a.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};var m,l,d;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => <div style={{
+    width: '380px'
+  }}>
+      <MessageList {...args}>
+        <MessageListItem description={'Uw pensioenoverzicht'} href={'#'} label={'Pensioenfonds'} metaData={'01-05-2024'} withBadge={{
+        ariaLabel: 'Nieuw',
+        role: 'status'
+      }} />
+        <MessageListItem description={'Herinnering APK'} href={'#'} label={'RDW'} metaData={'04-04-2024'} />
+        <MessageListItem description={'Aanslag OZB'} href={'#'} label={'Samenwerkings-verband Haaglanden'} metaData={'04-04-2024'} />
+      </MessageList>
+    </div>
+}`,...(d=(l=t.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const w=["Default","SmallContainer"];export{a as Default,t as SmallContainer,w as __namedExportsOrder,D as default};
