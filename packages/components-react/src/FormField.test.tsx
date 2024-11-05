72d4b1ba68867eb615d363ee9d042257453c1b4e
aakminasi <arefcoding@gmail.com>
2024-11-05 11:28:24 +0100

    modified formfield and formfieldselect unit tests

--- packages/components-react/src/FormField.test.tsx
+++ packages/components-react/src/FormField.test.tsx
@@ -6,15 +6,10 @@ import { FormField } from '.';
 describe('FormField', () => {
   const defaultProps = {
     label: 'Test Label',
-    description: 'Test Description',
-    errorMessage: 'Test Error Message',
     input: <input data-testid="form-input" />,
-    status: 'Test Status',
-    invalid: true,
     statusId: 'status-id',
     errorMessageId: 'error-message-id',
     descriptionId: 'description-id',
-    id: 'test-id',
   };
   it('renders a visible element', () => {
     const { container } = render(<FormField {...defaultProps} />);
