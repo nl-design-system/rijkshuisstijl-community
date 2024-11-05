39160b65d0a6086683c1a6ad48d73dd141ccb4f3
JoeriRoijenga <jroijenga@quintor.nl>
2024-11-05 13:39:53 +0100

    FEAT: added first test setup for PreHeading

--- packages/components-react/src/PreHeading.test.tsx
+++ packages/components-react/src/PreHeading.test.tsx
@@ -1,15 +1,31 @@
 import '@testing-library/jest-dom';
 
-import { render } from '@testing-library/react';
-import { Heading } from '.';
+import { render, screen } from '@testing-library/react';
+import { Heading, PreHeading } from '.';
 
-describe('Heading', () => {
+describe('PreHeading', () => {
   it('renders a visible element', () => {
-    const { container } = render(<Heading level={0} />);
+    const { container } = render(<PreHeading heading={<Heading level={1}>heading</Heading>}>pre-heading</PreHeading>);
 
-    const heading = container.querySelector(':only-child');
+    const preHeading = container.querySelector(':only-child');
 
-    expect(heading).toBeInTheDocument();
-    expect(heading).toBeVisible();
+    expect(preHeading).toBeInTheDocument();
+    expect(preHeading).toBeVisible();
+  });
+
+  it('elements have correct grid area placement', () => {
+    render(<PreHeading heading={<Heading level={1}>heading</Heading>}>pre-heading</PreHeading>);
+
+    const preHeading = screen.getByText('pre-Heading');
+    const heading = screen.getByText('heading');
+
+    const preHeadingStyle = window.getComputedStyle(preHeading);
+    const headingStyle = window.getComputedStyle(heading);
+
+    expect(preHeadingStyle.gridArea).toBe('pre-heading');
+    expect(headingStyle.gridArea).toBe('heading');
+
+    expect(preHeading).toBeInTheDocument();
+    expect(preHeading).toBeVisible();
   });
 });
