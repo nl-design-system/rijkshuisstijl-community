760d9550bd8b9817c991c6c982a3dad9bfcea770
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-11-05 12:53:32 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@f2b1fa4f13380c9a6792f12e17463773362e9f94 🚀

---
+++ assets/paragraph.stories-Cul6AEbp.js
@@ -0,0 +1,12 @@
+import{b as n}from"./index.esm-MEOwvUWN.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BeNflsVP.js";import"./iframe-BVwbWY7U.js";import"../sb-preview/runtime.js";const c=`# Paragraph component
+`;/* @license CC0-1.0 */const L={title:"RIVM/Paragraph",id:"rivm-paragraph",component:n,argTypes:{lead:{description:"Lead paragraph",type:{name:"boolean"},table:{category:"Property"},defaultValue:!1},children:{description:"Paragraph text - default webcomponent slot",type:{name:"string",required:!0},table:{category:"Webcomponent Slot"},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{docs:{description:{component:c}}}},e={args:{children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Paragraph"},r={args:{children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",lead:!0}};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Paragraph'
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,i,d;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    lead: true
+  }
+}`,...(d=(i=r.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};const P=["Default","Lead"];export{e as Default,r as Lead,P as __namedExportsOrder,L as default};
