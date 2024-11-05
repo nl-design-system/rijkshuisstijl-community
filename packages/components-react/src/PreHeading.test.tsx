4a8f24c0a1498c3d91c90d37198e3375548b58b4
JoeriRoijenga <jroijenga@quintor.nl>
2024-11-05 14:15:00 +0100

    FEAT: finished tests

--- packages/components-react/src/PreHeading.test.tsx
+++ packages/components-react/src/PreHeading.test.tsx
@@ -4,28 +4,16 @@ import { render, screen } from '@testing-library/react';
 import { Heading, PreHeading } from '.';
 
 describe('PreHeading', () => {
-  it('renders a visible element', () => {
-    const { container } = render(<PreHeading heading={<Heading level={1}>heading</Heading>}>pre-heading</PreHeading>);
-
-    const preHeading = container.querySelector(':only-child');
-
-    expect(preHeading).toBeInTheDocument();
-    expect(preHeading).toBeVisible();
-  });
-
-  it('elements have correct grid area placement', () => {
+  it('renders both the heading and pre-heading elements', () => {
     render(<PreHeading heading={<Heading level={1}>heading</Heading>}>pre-heading</PreHeading>);
 
-    const preHeading = screen.getByText('pre-Heading');
-    const heading = screen.getByText('heading');
-
-    const preHeadingStyle = window.getComputedStyle(preHeading);
-    const headingStyle = window.getComputedStyle(heading);
-
-    expect(preHeadingStyle.gridArea).toBe('pre-heading');
-    expect(headingStyle.gridArea).toBe('heading');
+    const preHeading = screen.getByText(/pre-Heading/i);
+    const heading = screen.getByText(/^heading$/i);
 
     expect(preHeading).toBeInTheDocument();
     expect(preHeading).toBeVisible();
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toBeVisible();
   });
 });
