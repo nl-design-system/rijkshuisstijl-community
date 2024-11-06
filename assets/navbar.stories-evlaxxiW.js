aa0d8a81f7a8358336e9279c3d40ef11287631e8
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2024-11-06 08:54:31 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@6c6d24894331308e89eb207fb728146f432d472e 🚀

---
+++ assets/navbar.stories-evlaxxiW.js
@@ -0,0 +1,18 @@
+import{N as g}from"./index.esm-BoJrDoda.js";/* empty css              */import"./index.esm-C4p8kHu0.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./jsx-runtime-BoFrXSaA.js";const H={title:"Rijkshuisstijl/NavBar",id:"rhc-nav-bar",component:g,parameters:{status:{type:"STABLE"}}},s=[{label:"Link",href:"/"},{label:"Link",href:"/"},{label:"Link",href:"/"}],h=[{label:"Link",href:"/"},{label:"Link",href:"/"}],e={args:{items:s}},r={args:{items:s,headingItem:{label:"Heading",href:"/"}}},a={args:{items:s,endItems:h}};var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    items
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var i,m,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    items,
+    headingItem: {
+      label: 'Heading',
+      href: '/'
+    }
+  }
+}`,...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var p,d,l;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    items,
+    endItems
+  }
+}`,...(l=(d=a.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const S=["Default","WithHeading","WithEndItems"];export{e as Default,a as WithEndItems,r as WithHeading,S as __namedExportsOrder,H as default};
