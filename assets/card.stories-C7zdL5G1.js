697a2be8b072e61d4e945e28436561870986d794
AdhamAH <development@adhamabohasson.com>
2024-11-07 15:48:02 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@d92ada9df096f54b13f6f6d0459716eba4587d4b 🚀

---
+++ assets/card.stories-C7zdL5G1.js
@@ -0,0 +1,12 @@
+import{C as u}from"./index.esm-Qrv_h3js.js";/* empty css              */import"./index.esm-DNTZgetO.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./jsx-runtime-BoFrXSaA.js";const x={title:"Rijkshuisstijl/Card",id:"rhc-card",component:u,argTypes:{heading:{description:"Card heading",control:{type:"text"},defaultValue:"Card Heading"},appearance:{description:"Card appearance",control:{type:"select"},options:["default","full-bleed","horizontal"],defaultValue:"default"},imageSrc:{description:"Image source URL",control:{type:"text"},defaultValue:""},imageAlt:{description:"Image alt text",control:{type:"text"},defaultValue:""},description:{description:"Card content",control:{type:"text"},defaultValue:""}},args:{imageSrc:"./placeholder.jpg",imageAlt:"Placeholder Image",href:"#",title:"Card Title",heading:"Default Card",description:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *",metadata:"Metadata",linkLabel:"Label"},tags:["autodocs"],parameters:{status:{type:"UNSTABLE"},docs:{description:{component:"Card component for displaying content in various styles."}}}},e={},a={args:{heading:"Full Bleed Card",appearance:"full-bleed",description:"This card has a background image."}},r={args:{heading:"Heading",appearance:"horizontal"}};var t,o,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var i,d,n;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    heading: 'Full Bleed Card',
+    appearance: 'full-bleed',
+    description: 'This card has a background image.'
+  }
+}`,...(n=(d=a.parameters)==null?void 0:d.docs)==null?void 0:n.source}}};var l,c,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    heading: 'Heading',
+    appearance: 'horizontal'
+  }
+}`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const S=["Default","FullBleed","Horizontal"];export{e as Default,a as FullBleed,r as Horizontal,S as __namedExportsOrder,x as default};
