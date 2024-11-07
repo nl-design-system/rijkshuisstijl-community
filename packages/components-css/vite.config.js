26fdef0cca2d13d218122553905aae2e9b8128de
Adham AboHasson <aahasson@quintor.nl>
2024-11-07 16:37:15 +0100

    fix: disable  eslint for vite config

--- packages/components-css/vite.config.js
+++ packages/components-css/vite.config.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-undef */
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    // eslint-disable-next-line no-undef
     outDir: path.resolve(__dirname, 'dist'), // Output folder for compiled CSS
     rollupOptions: {
-      // eslint-disable-next-line no-undef
       input: path.resolve(__dirname, 'index.scss'), // Entry point for SCSS
       output: {
         // Disable JS output and ensure only CSS is generated
