fa3e1a2e00f4ac0566f6feba6b518d81b39f8375
veslav3 <royhonders@gmail.com>
2024-11-07 21:54:13 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@484f79e23251f68569c31acbb220e2cd0eb35110 🚀

---
+++ assets/message-list-item.stories-BRYznlOJ.js
@@ -0,0 +1,35 @@
+import{j as a}from"./jsx-runtime-BoFrXSaA.js";import{M as r,s as h}from"./index.esm-DdWnWX0u.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";/* empty css              */import"./index.esm-6Yj_janZ.js";import"./objectWithoutProperties-BJfzkg-c.js";const L=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Message List component
+
+NL design system - geen | [Figma](https://www.figma.com/design/Nv5EsCW9ioWBUSi9m9JqOa/Local---Rijkshuisstijl---Bibliotheek?node-id=4070-5888&node-type=frame&t=xhXU5ugIkPqvOZwt-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/556)
+
+## Usage
+
+\`\`\`tsx
+import { MessageList, MessageListItem } from '@rijkshuisstijl-community/components-react';
+
+<MessageList>
+  <MessageListItem description={'Uw pensioenoverzicht'} href={'#'} label={'Pensioenfonds'} metaData={'Meta Data'} />
+</MessageList>;
+\`\`\`
+`,I={title:"Rijkshuisstijl/Message List/Message List Item",id:"rhc-message-list-item",component:r,args:{description:"Description",href:"#",label:"Label",metaData:"Meta Data"},argTypes:{description:{description:"Description",control:"text",table:{category:"API"}},href:{description:"Target URL",control:"text",table:{category:"API"}},label:{description:"Label",control:"text",table:{category:"API"}},metaData:{description:"Meta data",control:"text",table:{category:"API"}}},parameters:{status:{type:"STABLE"},docs:{description:{component:L}}},render:g=>a.jsx(h,{children:a.jsx(r,{...g})}),tags:["autodocs"]},e={args:{description:"Description",href:"#",label:"Label",metaData:"Meta Data"}},t={parameters:{pseudo:{hover:!0}}},s={parameters:{pseudo:{focus:!0}}};var o,i,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    description: 'Description',
+    href: '#',
+    label: 'Label',
+    metaData: 'Meta Data'
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var c,m,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,l,u;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  }
+}`,...(u=(l=s.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const k=["Default","Hover","Focus"];export{e as Default,s as Focus,t as Hover,k as __namedExportsOrder,I as default};
---
+++ assets/pre-heading.stories-BlwhArFw.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-BoFrXSaA.js";import{P as t}from"./index.esm-DdWnWX0u.js";import{o}from"./index.esm-6Yj_janZ.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";/* empty css              */import"./objectWithoutProperties-BJfzkg-c.js";const d=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community pre-heading component
+
+## Gebruik en toegankelijkheid van pre-headings
+
+- Gebruik altijd maar één heading in combinatie met een pre-heading.
+- Gebruik alleen een pre-heading als er content onder staat.
+- Gebruikers van screenreaders gebruiken headings en pre-headings vaak om een indruk te krijgen van de structuur van een pagina.
+- Gebruik pre-headings niet voor de opmaak maar alleen voor de tekstuele hiërarchie van de content.
+- Beperk het aantal pre-headings op een pagina.
+
+### De pre-heading component beschikt over property \`heading\`
+
+**heading (verplicht)**
+Dit verwijst naar het Heading component. Dit component is nodig om vervolgens de pre-heading visueel boven te tonen. In de HTML zal deze nog steeds onder de heading getoond zijn.
+
+**children (verplicht)**
+Dit verwijst naar het text component dat als child kan worden meegegeven aan de PreHeading. Dit is de text die visueel getoond gaat worden boven de heading.
+
+## Usage
+
+\`\`\`tsx
+import { PreHeading } from '@rijkshuisstijl-community/components-react';
+
+<PreHeading heading={<Heading level={1}>Introductie</Heading>}>Stap 1 van 8</PreHeading>;
+\`\`\`
+
+---
+`,u={title:"Rijkshuisstijl/PreHeading",id:"rhc-pre-heading",component:t,argTypes:{},parameters:{status:{type:"UNSTABLE"},docs:{description:{component:d}}}},e={name:"Default",args:{children:"Stap 1 van 8",heading:r.jsx(o,{level:2,children:"Introductie"})}};var n,a,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: 'Stap 1 van 8',
+    heading: <Heading level={2}>Introductie</Heading>
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,u as default};
