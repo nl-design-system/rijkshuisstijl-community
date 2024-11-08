777a6c7d3d8e83a0a6228973c40c66d683fb35b9
Adham AboHasson <aahasson@quintor.nl>
2024-11-08 08:57:25 +0100

    merge and fix any errors

--- apps/rhc-templates/src/app/layout.tsx
+++ apps/rhc-templates/src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { PropsWithChildren } from 'react';
 import '@nl-rvo/assets/fonts/index.css';
-import '@rijkshuisstijl-community/design-tokens/dist/index.css';
+import '@rijkshuisstijl-community/design-tokens/dist/wetgevend/index.css';
 import '@rijkshuisstijl-community/components-css/dist/index.css';
 import '@rijkshuisstijl-community/font/src/index.mjs';
 import '@rijkshuisstijl-community/storybook/src/templates/globals.css';
