4b48b3d50bc5895cba10635af6357a81812aa982
AdhamAH <development@adhamabohasson.com>
2024-11-18 10:44:53 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@5d55143af4a0d9f63e8a10d5450903a903b4ee93 🚀

---
+++ assets/counter-badge.stories-V3sfEjKY.js
@@ -0,0 +1,15 @@
+import{g as i}from"./index.esm-Cb36RVwl.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./jsx-runtime-BoFrXSaA.js";const d=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Counter Badge component
+
+Een counter badge is een badge die een getal toont.
+`,h={title:"Rijkshuisstijl/Counter Badge",id:"rhc-counter-badge",component:i,tags:["autodocs"],argTypes:{children:{description:"Text content",control:"text"},value:{description:"Value",control:"number"}},parameters:{status:{type:"UNSTABLE"},docs:{description:{component:d}}}},e={args:{children:"2"}},r={args:{children:"2,002",value:"2002"}};var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: '2'
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var a,s,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: '2,002',
+    value: '2002'
+  }
+}`,...(c=(s=r.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const C=["Default","WithValue"];export{e as Default,r as WithValue,C as __namedExportsOrder,h as default};
---
+++ assets/dot-badge.stories-CWw0pzET.js
@@ -0,0 +1,14 @@
+import{D as o}from"./index.esm-B6oMNm63.js";/* empty css              */import"./index.esm-Cb36RVwl.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./jsx-runtime-BoFrXSaA.js";const a=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Dot Badge component
+
+Nl design system - geen | [Figma](https://www.figma.com/design/Nv5EsCW9ioWBUSi9m9JqOa/Local---Rijkshuisstijl---Bibliotheek?node-id=4069-2838&node-type=canvas&t=HuDzyBW9wHdB2QVh-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/744)
+
+## Usage
+
+\`\`\`tsx
+import { DotBadge } from '@rijkshuisstijl-community/components-react';
+
+<DotBadge aria-label="Nieuwe Bericht" />;
+\`\`\`
+`,u={title:"Rijkshuisstijl/Dot Badge",id:"rhc-dot-badge",component:o,argTypes:{"aria-label":{control:"text"}},args:{"aria-label":"Nieuw Bericht"},parameters:{status:{type:"UNSTABLE"},docs:{description:{component:a}}}},t={};var e,s,i;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(i=(s=t.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,u as default};
