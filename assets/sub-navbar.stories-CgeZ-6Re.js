685e1723180783edbeed10f3046a615ee7a1bd06
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-11-20 09:52:49 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@90eb6981f44640d83c84a599bf62f0691506bc5c 🚀

---
+++ assets/sub-navbar.stories-CgeZ-6Re.js
@@ -0,0 +1,5 @@
+import{S as t}from"./index.esm-T5s4OxiK.js";/* empty css              */import"./index.esm-C1RX3ACj.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./jsx-runtime-BoFrXSaA.js";const u={title:"Rijkshuisstijl/NavBar/SubNavBar",id:"rhc-sub-nav-bar",component:t},s=[[{label:"Label",href:"#"},{label:"Label",href:"#"},{label:"Label",href:"#"}],[{label:"Label",href:"#"}]],r={args:{columns:s}};var a,e,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    columns
+  }
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
