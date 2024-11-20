532c4fd13536e7c0eb2afdd8e37bf3913ef517c5
Gerwin Terpstra <gterpstra@quintor.nl>
2024-11-20 14:45:26 +0100

    Fix: removed Paragraph wrapper of LinkList

--- packages/storybook/src/templates/collage/index.tsx
+++ packages/storybook/src/templates/collage/index.tsx
@@ -159,13 +159,11 @@ export default function Collage() {
                       <FormFieldCheckboxOption label="Label"></FormFieldCheckboxOption>
                     </FormFieldCheckboxGroup>
                     <Alert heading="Heading" type="error">
-                      <Paragraph>
-                        <LinkList>
-                          <LinkListLink href="#">Label</LinkListLink>
-                          <LinkListLink href="#">Label</LinkListLink>
-                          <LinkListLink href="#">Label</LinkListLink>
-                        </LinkList>
-                      </Paragraph>
+                      <LinkList>
+                        <LinkListLink href="#">Label</LinkListLink>
+                        <LinkListLink href="#">Label</LinkListLink>
+                        <LinkListLink href="#">Label</LinkListLink>
+                      </LinkList>
                     </Alert>
                     <FormField description="Description" label="Label">
                       <FormFieldErrorMessage>Error message</FormFieldErrorMessage>
