e01264906f8793a1e7788f69fe0e97c00ea14070
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-11-20 15:26:07 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@2d782611f3201b7170510b267a15169577321a4a 🚀

---
+++ assets/article.stories-DSVdrbCs.js
@@ -0,0 +1,27 @@
+import{j as s}from"./jsx-runtime-BoFrXSaA.js";import{a as o}from"./index.esm-B0fqiDPs.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";/* empty css              */import"./index.esm-C1RX3ACj.js";import"./objectWithoutProperties-BJfzkg-c.js";const r=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community article component
+
+[NL design system](https://www.nldesignsystem.nl/article/) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/566)
+
+De article component is er om de breedte van de inhoud ervan te beperken tot de ideale lees-lengte.
+
+## Usage
+
+\`\`\`tsx
+import { Article } from '@rijkshuisstijl-community/components-react';
+
+<Article>
+  <p>
+    Lorem ipsum dolor sit amet. Aut amet quibusdam et atque soluta id unde provident non sequi dolor rem iusto expedita
+    eum voluptates asperiores aut quae modi. 33 ipsa recusandae et repudiandae optio est ullam fugit sit dolores
+    praesentium sed pariatur aspernatur. Et voluptatibus expedita aut odit sint vel eius dolores et nobis tempora et
+    cupiditate rerum. Qui mollitia maiores non unde repellat a totam corrupti et tempore nihil est earum facilis hic
+    odio nisi est eligendi quidem? Aut similique quasi non repellat expedita qui magni tempore ab aperiam voluptatum sit
+    quia atque et enim alias ea voluptas internos. Qui quia eius vel alias dolores et expedita ipsum.
+  </p>
+</Article>;
+\`\`\`
+`,q={title:"Rijkshuisstijl/Article",id:"rhc-article",component:o,argTypes:{},args:{children:[s.jsx("p",{children:"Lorem ipsum dolor sit amet. Aut amet quibusdam et atque soluta id unde provident non sequi dolor rem iusto expedita eum voluptates asperiores aut quae modi. 33 ipsa recusandae et repudiandae optio est ullam fugit sit dolores praesentium sed pariatur aspernatur. Et voluptatibus expedita aut odit sint vel eius dolores et nobis tempora et cupiditate rerum. Qui mollitia maiores non unde repellat a totam corrupti et tempore nihil est earum facilis hic odio nisi est eligendi quidem? Aut similique quasi non repellat expedita qui magni tempore ab aperiam voluptatum sit quia atque et enim alias ea voluptas internos. Qui quia eius vel alias dolores et expedita ipsum."})]},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:r}}}},e={args:{}};var t,i,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,q as default};
