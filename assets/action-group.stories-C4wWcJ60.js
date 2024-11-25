f6afa36f2b1e2fc11077277be83f5c5cccd0b358
nl-design-system-ci <79643794+nl-design-system-ci@users.noreply.github.com>
2024-11-25 11:07:43 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@859207af652b3cfb9616092ee25a9e413e3dcb32 🚀

---
+++ assets/action-group.stories-C4wWcJ60.js
@@ -0,0 +1,26 @@
+import{j as a}from"./jsx-runtime-BoFrXSaA.js";import{B as p,d as t}from"./index.esm-CVUy8KX1.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./objectWithoutProperties-BJfzkg-c.js";const m=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community action group component
+
+De action group is een container voor een aantal buttons, of voor een losstaande button.
+
+## Terminologie
+
+- **button**: van het \`<button>\` HTML element, \`role="button"\` in ARIA.
+- **group**: van \`role=group\` in ARIA.
+
+## Verticale variant
+
+Voor smalle schermen bij plaatsing in smalle panelen (zoals een sidebar) is er een variant waar buttons onder elkaar staan, in plaats van naast elkaar. Gebruik deze variant niet wanneer er voldoende ruimte is voor de standaard variant. Je kunt bijvoorbeeld CSS media queries gebruiken om deze variant toe te passen voor kleine schermen.
+`,B={title:"Rijkshuisstijl/ActionGroup",id:"rhc-action-group",component:p,argTypes:{direction:{description:"Layout of the action group",control:"select",options:["column","row"]}},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:m}}}},n={args:{children:[a.jsx(t,{appearance:"primary-action-button",children:"Save and continue"}),a.jsx(t,{appearance:"secondary-action-button",children:"Back"})]},name:"Default"},e={args:{direction:"column",children:[a.jsx(t,{appearance:"primary-action-button",children:"Save and continue"}),a.jsx(t,{appearance:"secondary-action-button",children:"Back"})]},name:"Column"};var o,r,i;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: [<Button appearance="primary-action-button">Save and continue</Button>, <Button appearance="secondary-action-button">Back</Button>]
+  },
+  name: 'Default'
+}`,...(i=(r=n.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var s,c,u;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    direction: 'column',
+    children: [<Button appearance="primary-action-button">Save and continue</Button>, <Button appearance="secondary-action-button">Back</Button>]
+  },
+  name: 'Column'
+}`,...(u=(c=e.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const h=["Default","Column"];export{e as Column,n as Default,h as __namedExportsOrder,B as default};
---
+++ assets/table-cell.stories-Cw5qSGKr.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-BoFrXSaA.js";import{G as m}from"./index.esm-C2Nw_-gQ.js";import{T as c,j as p,k as d}from"./index.esm-CVUy8KX1.js";import"./index-WI1YhYyJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";/* empty css              */import"./objectWithoutProperties-BJfzkg-c.js";const u=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community TableCell component
+
+[NL design system](https://www.nldesignsystem.nl/table/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1561-6448&t=texUKkpCqzgFVuch-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/465)
+
+## Usage
+
+\`\`\`jsx
+import { TableCell } from '@rijkshuisstijl/community-components';
+
+<TableCell align={'center'}>Label</TableCell>;
+\`\`\`
+`,l=({children:t,alignCell:r,...o})=>e.jsx(c,{children:e.jsx(p,{children:e.jsx(d,{children:e.jsx(m,{alignCell:r,...o,children:t})})})}),y={title:"Rijkshuisstijl/Table/Table Cell",id:"rhc-table-cell",component:l,tags:["autodocs"],argTypes:{alignCell:{options:["start","end","center"],control:{type:"select"}}},render:t=>e.jsx(l,{...t}),parameters:{status:{type:"STABLE"},docs:{description:{component:u}}}},s={args:{children:"Label",alignCell:"start"}};var n,i,a;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Label',
+    alignCell: 'start'
+  }
+}`,...(a=(i=s.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const f=["Default"];export{s as Default,f as __namedExportsOrder,y as default};
