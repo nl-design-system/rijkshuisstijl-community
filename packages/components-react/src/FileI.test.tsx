fa35ae2276b8c2e90cb60270187c4b4b6096ae67
Adham AboHasson <aahasson@quintor.nl>
2024-11-29 14:16:24 +0100

    fix: failing build
    Signed-off-by: Adham AboHasson <aahasson@quintor.nl>

--- packages/components-react/src/FileI.test.tsx
+++ packages/components-react/src/FileI.test.tsx
@@ -1,13 +1,24 @@
 import '@testing-library/jest-dom';
 
 import { render } from '@testing-library/react';
-import { FileInputItem } from '.';
+import { File as FileComponent } from './File';
+beforeAll(() => {
+  Object.defineProperty(window, 'URL', {
+    value: {
+      createObjectURL: jest.fn().mockImplementation((blob: Blob | MediaSource) => `mocked-url/${(blob as File).name}`),
+    },
+  });
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
 
 describe('FileInputItem', () => {
   it('renders a visible element', () => {
     const file = new File([], 'example.png', { type: 'image/png' });
     const { container } = render(
-      <FileInputItem
+      <FileComponent
         allowedFileTypes={''}
         file={file}
         fileSizeErrorMessage={''}
