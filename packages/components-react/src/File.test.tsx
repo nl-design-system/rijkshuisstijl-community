1c6160e998593042f3e6e407927f0486c2a88273
Gerwin Terpstra <gterpstra@quintor.nl>
2024-12-02 12:23:35 +0100

    Added file tests

--- packages/components-react/src/File.test.tsx
+++ packages/components-react/src/File.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { File as FileComponent } from './File';
 beforeAll(() => {
   Object.defineProperty(window, 'URL', {
@@ -14,7 +14,7 @@ afterAll(() => {
   jest.restoreAllMocks();
 });
 
-describe('FileInputItem', () => {
+describe('FileComponent', () => {
   it('renders a visible element', () => {
     const file = new File([], 'example.png', { type: 'image/png' });
     const { container } = render(
@@ -28,9 +28,65 @@ describe('FileInputItem', () => {
       />,
     );
 
-    const fileInputItem = container.querySelector(':only-child');
+    const fileComponent = container.querySelector(':only-child');
 
-    expect(fileInputItem).toBeInTheDocument();
-    expect(fileInputItem).toBeVisible();
+    expect(fileComponent).toBeInTheDocument();
+    expect(fileComponent).toBeVisible();
+  });
+
+  it('shows the correct error message when a wrong file type is uploaded', () => {
+    const file = new File([], 'example.txt', { type: 'text/plain' });
+    const fileTypeErrorMessage = 'Dit bestandstype wordt niet toegestaan.';
+    render(
+      <FileComponent
+        allowedFileTypes=".png,.jpg"
+        file={file}
+        fileSizeErrorMessage=""
+        fileTypeErrorMessage={fileTypeErrorMessage}
+        maxFileSizeInBytes={10485760}
+        onDelete={jest.fn()}
+      />,
+    );
+
+    const alertElement = screen.getByText(fileTypeErrorMessage);
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toBeVisible();
+  });
+
+  it('shows the correct error message when the file is too big', () => {
+    const largeFile = new File([], 'largefile.png', { type: 'image/png' });
+    Object.defineProperty(largeFile, 'size', { value: 11 * 1024 * 1024 });
+    const fileSizeErrorMessage = 'Dit bestand is groter dan 10 MB.';
+    render(
+      <FileComponent
+        allowedFileTypes=".png,.jpg"
+        file={largeFile}
+        fileSizeErrorMessage={fileSizeErrorMessage}
+        fileTypeErrorMessage=""
+        maxFileSizeInBytes={10485760}
+        onDelete={jest.fn()}
+      />,
+    );
+
+    const alertElement = screen.getByText(fileSizeErrorMessage);
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toBeVisible();
+  });
+
+  it('renders the correct link for the file for preview', () => {
+    const file = new File([], 'example.png', { type: 'image/png' });
+    render(
+      <FileComponent
+        allowedFileTypes=".png,.jpg"
+        file={file}
+        fileSizeErrorMessage=""
+        fileTypeErrorMessage=""
+        maxFileSizeInBytes={10485760}
+        onDelete={jest.fn()}
+      />,
+    );
+
+    const linkElement = screen.getByRole('link', { name: /example.png/i });
+    expect(linkElement).toHaveAttribute('href', 'mocked-url/example.png');
   });
 });
