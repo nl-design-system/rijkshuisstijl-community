95bbaaec3f52c58cccc6ea8a8e390297d0dd55a7
AdhamAH <development@adhamabohasson.com>
2024-12-02 14:34:05 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@1b1b1692cf84da2af671a514be8a8a252c4931f9 🚀

---
+++ assets/logo.stories-CAFw8YLc.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-8Jttx2DA.js";import{s as C,I as f}from"./index.esm-BATW6mMo.js";import"./index-BzhKaBtb.js";import"./_commonjsHelpers-CqkleIqs.js";/* empty css              */import"./index-CgFCBr0q.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./clsx-B-dksMZM.js";import"./index-BhpIufNd.js";import"./index-DYgQ_QFa.js";const g="#fff",m="#154273",d="#154273",t=({imageBackgroundColor:p,textColor:u,imageIconColor:b,...h})=>o.jsx(o.Fragment,{children:o.jsx(C,{...h,style:{"--rhc-logo-color":u||d,"--rhc-logo-image-background-color":p||g,"--rhc-logo-image-color":b||m},children:o.jsx(f,{icon:"nederland-map"})})}),E={title:"Rijkshuisstijl/Logo",id:"rhc-logo",component:t,args:{organisation:"",subtitle:"",imageBackgroundColor:g,imageIconColor:m,textColor:d},argTypes:{organisation:{name:"organisation",type:{name:"string",required:!0}},subtitle:{name:"subtitle",type:{name:"string",required:!1}},imageBackgroundColor:{control:"color",description:"Changes background color of the image",table:{category:"Demo"}},imageIconColor:{control:"color",description:"Changes color of the image icon",table:{category:"Demo"}},textColor:{control:"color",description:"Changes text color",table:{category:"Demo"}}},parameters:{status:{type:"STABLE"}},render:t},r={args:{organisation:"Voorbeeld organisatie"}},e={args:{organisation:"Voorbeeld organisatie",subtitle:"Voorbeeld sub-title"}};var a,s,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    organisation: 'Voorbeeld organisatie'
+  }
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var n,c,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    organisation: 'Voorbeeld organisatie',
+    subtitle: 'Voorbeeld sub-title'
+  }
+}`,...(l=(c=e.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const q=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,q as __namedExportsOrder,E as default};
