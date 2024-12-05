e394f29f91db9dd05885fc8ac46d4e5c82d1c740
Roy Honders <rhonders@quintor.nl>
2024-12-05 13:02:39 +0100

    fix: test

--- packages/components-react/src/FormSummary.test.tsx
+++ packages/components-react/src/FormSummary.test.tsx
@@ -4,12 +4,12 @@ import { FormSummary } from './FormSummary';
 
 describe('FormSummary', () => {
   it('renders without crashing', () => {
-    render(<FormSummary aria-label={'test'} />);
+    render(<FormSummary aria-label={'test'} list-item-key="What he said" list-item-value="I am a potato" />);
     const formSummary = screen.getByTestId('paragraph');
     expect(formSummary).toBeInTheDocument();
   });
   it('applies correct aria-label', () => {
-    render(<FormSummary aria-label={'test'} />);
+    render(<FormSummary aria-label={'test'} list-item-key="What he said" list-item-value="I am a potato" />);
     const formSummary = screen.getByTestId('paragraph');
     expect(formSummary).toHaveAttribute('aria-label', 'test');
   });
