ccd5a235ba98718991b245da3041fd7da5740f0b
Roy Honders <rhonders@quintor.nl>
2024-12-05 11:45:31 +0100

    feat: add h5.

--- packages/components-react/src/FormSummary.tsx
+++ packages/components-react/src/FormSummary.tsx
@@ -1,14 +1,21 @@
 import { forwardRef, HTMLAttributes } from 'react';
+import { Heading } from './Heading';
 
 export interface FormSummaryProps extends HTMLAttributes<HTMLDivElement> {
   'aria-label': string;
+  'list-item-key': string;
+  'list-item-value': string;
+  'list-item-action'?: string;
 }
 
 export const FormSummary = forwardRef<HTMLDivElement, FormSummaryProps>((props, ref) => {
   return (
-    <p data-testid="paragraph" ref={ref} {...props}>
-      Hello world!
-    </p>
+    <>
+      <Heading level={5}>Hello world</Heading>
+      <p data-testid="paragraph" ref={ref} {...props}>
+        Hello world!
+      </p>
+    </>
   );
 });
 
