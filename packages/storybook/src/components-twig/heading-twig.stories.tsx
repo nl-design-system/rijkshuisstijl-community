57c122124ff00fa77ff7eede8a84b14736612643
Robbert Broersma <git@robbertbroersma.nl>
2024-12-10 21:12:20 +0100

    feat: add TwigRenderer component

---
+++ packages/storybook/src/components-twig/heading-twig.stories.tsx
@@ -0,0 +1,98 @@
+/* @license CC0-1.0 */
+
+import twigTemplate from '@rijkshuisstijl-community/components-twig/src/Heading.twig?raw';
+import type { Meta, StoryObj } from '@storybook/react';
+import { createTwigComponent } from './TwigRenderer';
+
+interface TwigHeadingProps {
+  appearance?: string;
+  level: number;
+  children: string;
+}
+
+const TwigHeading = createTwigComponent<TwigHeadingProps>(twigTemplate);
+
+const meta = {
+  title: 'Rijkshuisstijl/Twig/Heading',
+  id: 'rhc-twig-heading',
+  component: TwigHeading,
+  argTypes: {
+    level: {
+      description: 'Heading level',
+      control: { type: 'select' },
+      options: [1, 2, 3, 4, 5],
+    },
+    appearance: {
+      description: 'Appearance',
+      control: { type: 'select' },
+      options: [
+        undefined,
+        'utrecht-heading-1',
+        'utrecht-heading-2',
+        'utrecht-heading-3',
+        'utrecht-heading-4',
+        'utrecht-heading-5',
+      ],
+    },
+  },
+  parameters: {
+    status: {
+      type: 'STABLE',
+    },
+    docs: {
+      description: {
+        // component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof TwigHeading>;
+
+export default meta;
+
+export const TwigHeading1: StoryObj<typeof meta> = {
+  args: {
+    level: 1,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 1',
+};
+
+export const TwigHeading2: StoryObj<typeof meta> = {
+  args: {
+    level: 2,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 2',
+};
+
+export const TwigHeading3: StoryObj<typeof meta> = {
+  args: {
+    level: 3,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 3',
+};
+
+export const TwigHeading4: StoryObj<typeof meta> = {
+  args: {
+    level: 4,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 4',
+};
+
+export const TwigHeading5: StoryObj<typeof meta> = {
+  args: {
+    level: 5,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 5',
+};
+
+export const TwigHeading6: StoryObj<typeof meta> = {
+  args: {
+    level: 6,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+  },
+  name: 'Heading 6',
+};
