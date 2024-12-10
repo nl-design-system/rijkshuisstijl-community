08af788824dd763b3d59de80db91814850286bee
MeesD94 <mdubbe@quintor.nl>
2024-12-10 09:17:32 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@fcddc1febb4389f620fa3c1c3c05b63fef18eb00 🚀

---
+++ assets/separator.stories-6oSG8UAq.js
@@ -0,0 +1,16 @@
+import{x as d}from"./index.esm-BSchPxkL.js";/* empty css              */import"./index-B9EZEqMY.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-BzhKaBtb.js";import"./_commonjsHelpers-CqkleIqs.js";import"./jsx-runtime-8Jttx2DA.js";import"./clsx-B-dksMZM.js";import"./index-BhpIufNd.js";import"./index-DYgQ_QFa.js";const p=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community separator component
+
+Scheidingslijnen worden gebruikt om de scanbaarheid van een lange contentpagina te vergroten.
+
+## Terminologie
+
+- **separator**, van de ARIA \`role="separator"\`. "hr" van \`<hr>\` uit HTML is te onduidelijk buiten de context van code, "horizontal rule" te specifieke beschrijving van de originele rendering in HTML. De "separator" zou er ook heel anders uit kunnen zien dan een "rule" (lijn), en bovendien kan er voor andere tekstrichtingen wel eens een verticale variant komen.
+
+"Divider" is ook een vaak gehoorde term hiervoor, misschien moet deze voor vindbaarheid ook genoemd worden in de documentatie.
+`,S={title:"Rijkshuisstijl/Separator",id:"rhc-separator",component:d,parameters:{status:{type:"STABLE"},docs:{description:{component:p}}},argTypes:{invisible:{control:{type:"boolean"}}}},e={},r={args:{invisible:!0}};var n,o,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var t,a,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    invisible: true
+  }
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const f=["Default","Invisible"];export{e as Default,r as Invisible,f as __namedExportsOrder,S as default};
---
+++ assets/side-nav.stories-DcEaU4m8.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-8Jttx2DA.js";import{z as c,E as t,G as i,y as n,x as a}from"./index.esm-BSchPxkL.js";import{f as v}from"./index-B9EZEqMY.js";import"./index-BzhKaBtb.js";import"./_commonjsHelpers-CqkleIqs.js";/* empty css              */import"./clsx-B-dksMZM.js";import"./index-BhpIufNd.js";import"./index-DYgQ_QFa.js";import"./objectWithoutProperties-BJfzkg-c.js";const m=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community SideNav component
+`,f={title:"Rijkshuisstijl/SideNav",id:"rhc-sideNav",component:c,tags:["autodocs"],argTypes:{},parameters:{status:{type:"UNSTABLE"},docs:{description:{component:m}}}},r={args:{children:[e.jsx(t,{children:e.jsx(i,{children:e.jsx(n,{current:!0,href:"/#",icon:"tegelweergave",children:"Overzicht"})})}),e.jsx(a,{invisible:!0}),e.jsxs(t,{children:[e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"activiteit",children:"Mijn taken"})}),e.jsx(i,{children:e.jsxs(n,{href:"/#",icon:"mail",children:["Mijn berichten",e.jsx(v,{children:"2"})]})}),e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"bewerken",children:"Mijn lopende zaken"})})]}),e.jsx(a,{invisible:!0}),e.jsx(t,{children:e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"user",children:"Mijn gegevens"})})})]}};var s,d,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: [<SideNavList>
+        <SideNavItem>
+          <SideNavLink current href="/#" icon="tegelweergave">
+            Overzicht
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>, <Separator invisible />, <SideNavList>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="activiteit">
+            Mijn taken
+          </SideNavLink>
+        </SideNavItem>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="mail">
+            Mijn berichten
+            <BadgeCounter>2</BadgeCounter>
+          </SideNavLink>
+        </SideNavItem>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="bewerken">
+            Mijn lopende zaken
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>, <Separator invisible />, <SideNavList>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="user">
+            Mijn gegevens
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>]
+  }
+}`,...(o=(d=r.parameters)==null?void 0:d.docs)==null?void 0:o.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,f as default};
