3e94d992eb305ddab56168eac8f4f45e700de33d
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-12-13 12:12:33 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@3c5533ca8afcb80813f5fea7f5372e50f6e269e2 🚀

--- assets/sub-navbar.stories-C19kkEcE.js
+++
@@ -1,5 +0,0 @@
-import{S as t}from"./index.esm-Dmnp414R.js";/* empty css              */import"./index-BclNPbce.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./jsx-runtime-D_zvdyIk.js";import"./clsx-B-dksMZM.js";import"./index-j0aIpYJF.js";import"./index-BlU3qzUC.js";const h={title:"Rijkshuisstijl/NavBar/SubNavBar",id:"rhc-sub-nav-bar",component:t},i=[[{label:"Label",href:"#",id:"li-1"},{label:"Label",href:"#",id:"li-2"},{label:"Label",href:"#",id:"li-3"}],[{label:"Label",href:"#",id:"li-4"}]],r={args:{columns:i}};var a,e,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    columns
-  }
-}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const v=["Default"];export{r as Default,v as __namedExportsOrder,h as default};
