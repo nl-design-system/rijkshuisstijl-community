3c5533ca8afcb80813f5fea7f5372e50f6e269e2
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-12-13 13:08:48 +0100

    edited hero props (#989)
    [#925](https://github.com/nl-design-system/rijkshuisstijl-community/issues/925)

--- packages/components-react/src/Hero.tsx
+++ packages/components-react/src/Hero.tsx
@@ -5,17 +5,26 @@ import { HeadingGroup } from './HeadingGroup';
 import { Image } from './Image';
 import { Paragraph } from './Paragraph';
 
-export interface HeroProps extends HTMLAttributes<HTMLDivElement> {
+export interface HeroBaseProps extends HTMLAttributes<HTMLDivElement> {
   textAlign?: 'start' | 'end';
   imageSrc: string;
   imageAlt: string;
-  heading: ReactNode;
   headingLevel?: 1 | 2 | 3 | 4 | 5;
-  subHeading: ReactNode;
-  heroMessage?: boolean;
   aspectRatio?: '16 / 9' | '1 / 1' | '4 / 3';
   borderRadiusCorner?: 'start-start' | 'start-end' | 'end-start' | 'end-end';
 }
+
+export interface HeroWithHeading extends HeroBaseProps {
+  heading: ReactNode;
+  subHeading?: ReactNode;
+}
+
+export interface HeroWithoutSubHeading extends HeroBaseProps {
+  heading?: ReactNode;
+  subHeading?: never;
+}
+
+export type HeroProps = HeroWithHeading | HeroWithoutSubHeading;
 
 export const Hero = forwardRef(
   (
@@ -27,7 +36,6 @@ export const Hero = forwardRef(
       imageAlt,
       heading,
       subHeading,
-      heroMessage,
       headingLevel = 3,
       borderRadiusCorner,
       aspectRatio = '16 / 9',
@@ -36,12 +44,12 @@ export const Hero = forwardRef(
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
     return (
-      <div
+      <section
         ref={ref}
         className={clsx(
           'rhc-hero',
-          heroMessage && `rhc-hero--text-align-${textAlign}`,
-          heroMessage &&
+          heading && `rhc-hero--text-align-${textAlign}`,
+          heading &&
             borderRadiusCorner &&
             `rhc-hero--custom-border-radius-corner rhc-hero--border-radius-corner-${borderRadiusCorner}`,
           `rhc-hero--aspect-ratio-${aspectRatio.replace(' / ', '-')}`,
@@ -50,18 +58,18 @@ export const Hero = forwardRef(
         {...restProps}
       >
         <Image alt={imageAlt} className="rhc-hero__image" src={imageSrc} />
-        {heroMessage && (
+        {heading && (
           <div className={clsx('rhc-hero__message')}>
             <HeadingGroup>
               <Heading appearance="utrecht-heading-3" className="rhc-hero__heading" level={headingLevel}>
                 {heading}
               </Heading>
-              <Paragraph className="rhc-hero__sub-heading">{subHeading}</Paragraph>
+              {subHeading && <Paragraph className="rhc-hero__sub-heading">{subHeading}</Paragraph>}
             </HeadingGroup>
           </div>
         )}
         {children && <div className="rhc-hero__children">{children}</div>}
-      </div>
+      </section>
     );
   },
 );
--- packages/storybook/src/community/hero.md
+++ packages/storybook/src/community/hero.md
@@ -1,3 +1,20 @@
 <!-- @license CC0-1.0 -->
 
 # Rijkshuisstijl Community hero component
+
+[NL design system](https://www.nldesignsystem.nl/hero/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=15708-524&node-type=canvas&t=fXG4KjJRXbo2PG2J-0)
+
+Let op: Zowel `heading` als `subHeading` zijn optioneel, maar als je een `subHeading` toevoegt, moet je ook een `heading` toevoegen. Een Hero-component met alleen een `subHeading` is niet toegestaan.
+
+## Usage
+
+```tsx
+import { Hero } from '@rijkshuisstijl-community/components-react';
+
+<Hero
+  heading="Heading"
+  imageAlt="Tulip field"
+  imageSrc="https://raw.githubusercontent.com/nl-design-system/rijkshuisstijl-community/main/proprietary/assets/src/placeholder.jpg"
+  subHeading="Subtext"
+/>;
+```
