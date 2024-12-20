760aab5ca8827894d1716b773fab5f975a3797e6
GerwinTerpstra <150233640+GerwinTerpstra@users.noreply.github.com>
2024-12-20 13:45:37 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@3d2ab459b15152403fa0341c8ef8ffdb2db957cb 🚀

---
+++ assets/sub-navbar.stories-C7VrXVDM.js
@@ -0,0 +1,5 @@
+import{S as t}from"./index.esm-DAvEtrr1.js";/* empty css              */import"./index-vsdXSVOh.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-D_zvdyIk.js";import"./clsx-B-dksMZM.js";import"./index-BhQyoJpt.js";import"./index-CgpWq_u8.js";const h={title:"Rijkshuisstijl/NavBar/SubNavBar",id:"rhc-sub-nav-bar",component:t},i=[[{label:"Label",href:"#",id:"li-1"},{label:"Label",href:"#",id:"li-2"},{label:"Label",href:"#",id:"li-3"}],[{label:"Label",href:"#",id:"li-4"}]],r={args:{columns:i}};var a,e,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    columns
+  }
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const v=["Default"];export{r as Default,v as __namedExportsOrder,h as default};
---
+++ assets/table-header-cell.stories-CB__XVSv.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{O as p}from"./index.esm-DAvEtrr1.js";import{T as d,j as g,i as h}from"./index-vsdXSVOh.js";/* empty css              */import"./clsx-B-dksMZM.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-BhQyoJpt.js";import"./index-CgpWq_u8.js";import"./objectWithoutProperties-BJfzkg-c.js";const u=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community TableHeaderCell component
+
+[NL design system](https://www.nldesignsystem.nl/table/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1561-6448&t=texUKkpCqzgFVuch-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/465)
+
+## Usage
+
+\`\`\`jsx
+import { TableHeaderCell } from '@rijkshuisstijl/community-components';
+
+<TableHeaderCell align={'center'} aria-sort={'descending'} scope={'col'} withSorting>
+  Header
+</TableHeaderCell>;
+\`\`\`
+`,n=({children:s,withSorting:l,alignCell:a,scope:c,...m})=>e.jsx(d,{children:e.jsx(g,{children:e.jsx(h,{children:e.jsx(p,{...m,alignCell:a,scope:c,withSorting:l,children:s})})})}),S={title:"Rijkshuisstijl/Table/Table Header Cell",id:"rhc-table-header-cell",component:n,tags:["autodocs"],argTypes:{withSorting:{type:"boolean"},"aria-sort":{options:["ascending","descending","none"],control:{type:"select"}},scope:{options:["row","col"],control:{type:"select"}},alignCell:{options:["start","end","center"],control:{type:"select"}}},render:s=>e.jsx(n,{...s}),parameters:{status:{type:"STABLE"},docs:{description:{component:u}}}},t={args:{children:"Label",scope:"col",withSorting:!1,alignCell:"start"}};var o,r,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Label',
+    scope: 'col',
+    withSorting: false,
+    alignCell: 'start'
+  }
+}`,...(i=(r=t.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const R=["Default"];export{t as Default,R as __namedExportsOrder,S as default};
