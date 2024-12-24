723653be5d3a6a0c92f92ee6c5a173d2a67d4b23
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2024-12-24 09:14:07 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@b73e728727f1e231411be0b001bc06b2bff9e3f8 🚀

---
+++ assets/side-nav-link.stories-BbbpDgpx.js
@@ -0,0 +1,78 @@
+import{j as K}from"./jsx-runtime-D_zvdyIk.js";import{y as m,r as P}from"./index.esm-U9XyXukO.js";/* empty css              */import"./index-B0wZklxY.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./clsx-B-dksMZM.js";import"./index-CgpWq_u8.js";const Q=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community SideNavLink component
+`,oe={title:"Rijkshuisstijl/SideNav/SideNavLink",id:"rhc-sideNavLink",component:m,tags:["autodocs"],argTypes:{icon:{control:{type:"select"},options:P,description:"The icon to use",table:{type:{summary:"string"}}},current:{control:{type:"boolean"},description:"The current state of the link",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}}},args:{children:"Label",icon:"activiteit"},render:J=>K.jsx(m,{...J}),parameters:{status:{type:"STABLE"},docs:{description:{component:Q}}}},e={args:{children:"Mijn gegevens",icon:"user"}},r={parameters:{pseudo:{hover:!0}}},s={parameters:{pseudo:{focus:!0}}},t={parameters:{pseudo:{focusVisible:!0}}},o={parameters:{pseudo:{active:!0}}},a={args:{current:!0}},n={args:{current:!0},parameters:{pseudo:{hover:!0}}},c={args:{current:!0},parameters:{pseudo:{active:!0}}},u={args:{current:!0},parameters:{pseudo:{focus:!0}}},i={args:{current:!0},parameters:{pseudo:{focusVisible:!0,focus:!0}}},p={args:{icon:void 0}};var d,l,g;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: 'Mijn gegevens',
+    icon: 'user'
+  }
+}`,...(g=(l=e.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var v,f,S;r.parameters={...r.parameters,docs:{...(v=r.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(S=(f=r.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var h,b,A;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  }
+}`,...(A=(b=s.parameters)==null?void 0:b.docs)==null?void 0:A.source}}};var C,y,j;t.parameters={...t.parameters,docs:{...(C=t.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focusVisible: true
+    }
+  }
+}`,...(j=(y=t.parameters)==null?void 0:y.docs)==null?void 0:j.source}}};var V,F,k;o.parameters={...o.parameters,docs:{...(V=o.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      active: true
+    }
+  }
+}`,...(k=(F=o.parameters)==null?void 0:F.docs)==null?void 0:k.source}}};var L,x,N;a.parameters={...a.parameters,docs:{...(L=a.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    current: true
+  }
+}`,...(N=(x=a.parameters)==null?void 0:x.docs)==null?void 0:N.source}}};var H,T,E;n.parameters={...n.parameters,docs:{...(H=n.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    current: true
+  },
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(E=(T=n.parameters)==null?void 0:T.docs)==null?void 0:E.source}}};var R,D,I;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    current: true
+  },
+  parameters: {
+    pseudo: {
+      active: true
+    }
+  }
+}`,...(I=(D=c.parameters)==null?void 0:D.docs)==null?void 0:I.source}}};var M,O,W;u.parameters={...u.parameters,docs:{...(M=u.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    current: true
+  },
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  }
+}`,...(W=(O=u.parameters)==null?void 0:O.docs)==null?void 0:W.source}}};var _,B,q;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    current: true
+  },
+  parameters: {
+    pseudo: {
+      focusVisible: true,
+      focus: true
+    }
+  }
+}`,...(q=(B=i.parameters)==null?void 0:B.docs)==null?void 0:q.source}}};var w,z,G;p.parameters={...p.parameters,docs:{...(w=p.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    icon: undefined
+  }
+}`,...(G=(z=p.parameters)==null?void 0:z.docs)==null?void 0:G.source}}};const ae=["Default","Hover","Focus","FocusVisible","Active","Current","CurrentAndHover","CurrentAndActive","CurrentAndFocus","CurrentAndFocusVisible","WithoutIcon"];export{o as Active,a as Current,c as CurrentAndActive,u as CurrentAndFocus,i as CurrentAndFocusVisible,n as CurrentAndHover,e as Default,s as Focus,t as FocusVisible,r as Hover,p as WithoutIcon,ae as __namedExportsOrder,oe as default};
