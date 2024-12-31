fd1c495d5bfb60f80bc4499bc1e0c1c55b3324a2
GerwinTerpstra <150233640+GerwinTerpstra@users.noreply.github.com>
2024-12-31 09:03:16 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@68195acde1ae409b5fee1bbefce01a5f2e25a8ce 🚀

---
+++ assets/table-cell.stories-CITR6-NL.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{K as m}from"./index.esm-CX03gi65.js";import{T as c,i as p,j as d}from"./index-QEKeRpAx.js";/* empty css              */import"./clsx-B-dksMZM.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-CgpWq_u8.js";const u=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community TableCell component
+
+[NL design system](https://www.nldesignsystem.nl/table/) | [Figma](https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1561-6448&t=texUKkpCqzgFVuch-0) | [GitHub](https://github.com/nl-design-system/rijkshuisstijl-community/issues/465)
+
+## Usage
+
+\`\`\`jsx
+import { TableCell } from '@rijkshuisstijl/community-components';
+
+<TableCell align={'center'}>Label</TableCell>;
+\`\`\`
+`,l=({children:t,alignCell:a,...o})=>e.jsx(c,{children:e.jsx(p,{children:e.jsx(d,{children:e.jsx(m,{alignCell:a,...o,children:t})})})}),f={title:"Rijkshuisstijl/Table/Table Cell",id:"rhc-table-cell",component:l,tags:["autodocs"],argTypes:{alignCell:{options:["start","end","center"],control:{type:"select"}}},render:t=>e.jsx(l,{...t}),parameters:{status:{type:"STABLE"},docs:{description:{component:u}}}},s={args:{children:"Label",alignCell:"start"}};var n,i,r;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Label',
+    alignCell: 'start'
+  }
+}`,...(r=(i=s.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const k=["Default"];export{s as Default,k as __namedExportsOrder,f as default};
