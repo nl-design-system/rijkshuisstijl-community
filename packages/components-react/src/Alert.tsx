da352b697cefec03760491d2f183a9e10e06bd67
Roy Honders <rhonders@quintor.nl>
2025-01-02 11:43:59 +0100

    feat: Use React 19 refs
          This commit updates the code to use the simplified ref handling introduced in React 19.
    
          - Removed unnecessary `forwardRef` wrappers.
          - Passed refs directly as props to function components.
    
          This change simplifies the code and improves readability while taking advantage of the latest React features.

--- packages/components-react/src/Alert.tsx
+++ packages/components-react/src/Alert.tsx
@@ -1,51 +1,55 @@
 import { Heading, Paragraph, Alert as UtrechtAlert } from '@utrecht/component-library-react';
 import clsx from 'clsx';
-import { ForwardedRef, forwardRef, PropsWithChildren, ReactNode } from 'react';
+import { PropsWithChildren, ReactNode, Ref } from 'react';
 import { Icon } from './icon/Icon';
 
 export interface AlertProps {
+  ref?: Ref<HTMLDivElement>;
   type: 'info' | 'ok' | 'warning' | 'error';
   heading?: ReactNode;
   headingLevel?: 1 | 2 | 3 | 4 | 5;
   textContent?: ReactNode;
 }
-export const Alert = forwardRef(
-  (
-    { type, heading, headingLevel, textContent, children, ...restProps }: PropsWithChildren<AlertProps>,
-    ref: ForwardedRef<HTMLDivElement>,
-  ) => {
-    return (
-      <UtrechtAlert className="rhc-alert" ref={ref} type={type} {...restProps}>
-        <div
-          className={clsx('rhc-alert__icon-container', {
-            'rhc-alert__icon-container--ok': type === 'ok',
-            'rhc-alert__icon-container--error': type === 'error',
-            'rhc-alert__icon-container--warning': type === 'warning',
-            'rhc-alert__icon-container--info': type === 'info',
-          })}
-        >
-          <Icon
-            icon={
-              type === 'info'
-                ? 'info-circle'
-                : type === 'ok'
-                  ? 'circle-check'
-                  : type === 'warning'
-                    ? 'let-op'
-                    : 'alert-circle'
-            }
-          />
-        </div>
-        <div>
-          <Heading appearance="utrecht-heading-5" level={headingLevel || 3}>
-            {heading}
-          </Heading>
-          <Paragraph>{textContent}</Paragraph>
-          {children}
-        </div>
-      </UtrechtAlert>
-    );
-  },
-);
+export const Alert = ({
+  ref,
+  type,
+  heading,
+  headingLevel,
+  textContent,
+  children,
+  ...restProps
+}: PropsWithChildren<AlertProps>) => {
+  return (
+    <UtrechtAlert className="rhc-alert" ref={ref} type={type} {...restProps}>
+      <div
+        className={clsx('rhc-alert__icon-container', {
+          'rhc-alert__icon-container--ok': type === 'ok',
+          'rhc-alert__icon-container--error': type === 'error',
+          'rhc-alert__icon-container--warning': type === 'warning',
+          'rhc-alert__icon-container--info': type === 'info',
+        })}
+      >
+        <Icon
+          icon={
+            type === 'info'
+              ? 'info-circle'
+              : type === 'ok'
+                ? 'circle-check'
+                : type === 'warning'
+                  ? 'let-op'
+                  : 'alert-circle'
+          }
+        />
+      </div>
+      <div>
+        <Heading appearance="utrecht-heading-5" level={headingLevel || 3}>
+          {heading}
+        </Heading>
+        <Paragraph>{textContent}</Paragraph>
+        {children}
+      </div>
+    </UtrechtAlert>
+  );
+};
 
 Alert.displayName = 'Alert';
