9a5c4c6fc6a7ae05ef482c4025604f32a9528988
aakminasi <arefcoding@gmail.com>
2025-01-13 16:49:55 +0100

    updated terug naar boven button

--- packages/components-react/src/Footer.tsx
+++ packages/components-react/src/Footer.tsx
@@ -1,6 +1,7 @@
 import { ColumnLayout, PageFooterProps, PageFooter as UtrechtPageFooter } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 import { ForwardedRef, forwardRef, PropsWithChildren, ReactNode } from 'react';
+import { Button } from './Button';
 import { Heading } from './Heading';
 
 interface FooterProps extends PageFooterProps {
@@ -89,8 +90,12 @@ export const Footer = forwardRef(
           )}
         >
           <div className="rhc-page-subfooter__content rhc-page-footer__wrapper">
+            {backtotop && (
+              <Button appearance="subtle-button" className="rhc-page-subfooter__backtotop" onClick={scrollBackToTop}>
+                Terug naar boven
+              </Button>
+            )}
             {subFooter}
-            {backtotop && <span onClick={scrollBackToTop}>Back To Top</span>}
           </div>
         </div>
       )}
