3747029bd1bd0680a4e11e0eaa822a2ee9d3dbcd
JoeriRoijenga <jroijenga@quintor.nl>
2025-01-13 15:56:48 +0100

    FEAT: added extra attributes to accordion WC

--- packages/web-components/src/components/accordion/index.tsx
+++ packages/web-components/src/components/accordion/index.tsx
@@ -6,7 +6,7 @@ export type AccordionWebComponentAttributes = AccordionProviderProps;
 
 export class AccordionWebComponent extends BaseWebComponent {
   static override tagName: string = 'rhc-accordion';
-  static override observedAttributes: string[] = ['appearance', 'icon', 'sections'];
+  static override observedAttributes: string[] = ['appearance', 'icon', 'sections', 'heading', 'headingLevel'];
 
   constructor() {
     super(stylesheet);
@@ -16,7 +16,13 @@ export class AccordionWebComponent extends BaseWebComponent {
     this.root.render(
       <AccordionProvider
         appearance={this.getAttribute('appearance') ?? undefined}
+        heading={this.getAttribute('heading') ?? undefined}
         icon={this.getAttribute('icon')}
+        headingLevel={
+          this.getAttribute('headingLevel')
+            ? (Number(this.getAttribute('headingLevel')) as AccordionProviderProps['headingLevel'])
+            : undefined
+        }
         sections={
           (this.getAttribute('sections') &&
             JSON.parse(this.getAttribute('sections') as string)) as AccordionProviderProps['sections']
