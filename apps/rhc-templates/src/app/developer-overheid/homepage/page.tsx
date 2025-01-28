0982852efe8067c7dfb6a0b955ffd521de2bf0ce
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-01-28 13:01:16 +0100

    feat: storybook template homepage + import in app

---
+++ apps/rhc-templates/src/app/developer-overheid/homepage/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import PageContent from '@rijkshuisstijl-community/storybook/src/templates/developer-overheid/homepage'; //TODO: importeer Storybook homepage template
+
+export default function Page() {
+  return <PageContent />;
+}
