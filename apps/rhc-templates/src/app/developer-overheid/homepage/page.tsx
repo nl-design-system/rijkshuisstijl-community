534f8291fefc01ec9527c109afa57d376022f2b9
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-01-28 13:01:16 +0100

    feat: storybook template homepage + import in app
    refactor: storybook homepage template + some css
    
    refactor: homepage.stories & template, add some css to globals,
    
    refactor: removing unused code, comments etc.
    
    refactor: homepage layout
    
    style: add grid container
    
    refactor: update PageHeader add Logo, TextInput and add items to NavBar
    
    refactor: remove unused cards
    
    refactor: remove unused imports and unnecessary comments
    
    feat: Header replace logo and add title
    
    style: card and logo styling
    
    feat: header logo en nav button added
    
    style: logo styling
    
    build: install dependencies
    
    feat: add classname card
    
    refactor: footer and hero correcte text implementeren
    
    feat: add classnames to components
    
    style: layout inline styling
    
    feat: add SkipLink + ScrollLink to homepage template
    
    refactor: change classname on skiplink (now visible on focus)
    
    feat: add StoryRootDecorator + implemantation
    
    refactor: remove comments and unused code, move some files

---
+++ apps/rhc-templates/src/app/developer-overheid/homepage/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import PageContent from '@rijkshuisstijl-community/storybook/src/templates/developer-overheid/homepage';
+
+export default function Page() {
+  return <PageContent />;
+}
