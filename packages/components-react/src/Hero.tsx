1b62d9252f51608323c979d19529e9b84cca66c7
Adham Abo Hasson <aahasson@quintor.nl>
2025-01-30 13:54:40 +0100

    Refactor/heading component to use nl component candidate (#1150)
    Deel van #1090 
    Signed-off-by: Adham AboHasson <aahasson@quintor.nl>

--- packages/components-react/src/Hero.tsx
+++ packages/components-react/src/Hero.tsx
@@ -61,7 +61,7 @@ export const Hero = forwardRef(
         {heading && (
           <div className={clsx('rhc-hero__message')}>
             <HeadingGroup>
-              <Heading appearance="utrecht-heading-3" className="rhc-hero__heading" level={headingLevel}>
+              <Heading appearance="level-3" className="rhc-hero__heading" level={headingLevel}>
                 {heading}
               </Heading>
               {subHeading && <Paragraph className="rhc-hero__sub-heading">{subHeading}</Paragraph>}
--- packages/storybook/src/community/heading.stories.tsx
+++ packages/storybook/src/community/heading.stories.tsx
@@ -18,14 +18,7 @@ const meta = {
     appearance: {
       description: 'Appearance',
       control: { type: 'select' },
-      options: [
-        undefined,
-        'utrecht-heading-1',
-        'utrecht-heading-2',
-        'utrecht-heading-3',
-        'utrecht-heading-4',
-        'utrecht-heading-5',
-      ],
+      options: [undefined, 'level-1', 'level-2', 'level-3', 'level-4', 'level-5'],
     },
   },
   parameters: {
@@ -38,9 +31,11 @@ const meta = {
         component: mergeMarkdown([readme]),
       },
     },
-    // TODO: add Figma, GitHub and NL DesignSystem links
-    componentOrigin:
-      'Dit component is overgenomen van de Gemeente Utrecht, met alleen overgeschreven design tokens van de Rijkshuisstijl Community.',
+    nldesignsystem: 'https://www.nldesignsystem.nl/heading/',
+    figma:
+      'https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=153-1039&p=f&t=AHS0qWRed9fEffkN-0',
+    github:
+      'https://github.com/orgs/nl-design-system/projects/59/views/1?filterQuery=heading&pane=issue&itemId=72572695&issue=nl-design-system%7Crijkshuisstijl-community%7C469',
   },
 } satisfies Meta<typeof Heading>;
 
@@ -50,6 +45,7 @@ export const Heading1: StoryObj<typeof meta> = {
   args: {
     level: 1,
     children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearance: 'level-1',
   },
   name: 'Heading 1',
 };
@@ -58,6 +54,7 @@ export const Heading2: StoryObj<typeof meta> = {
   args: {
     level: 2,
     children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearance: 'level-2',
   },
   name: 'Heading 2',
 };
@@ -66,6 +63,7 @@ export const Heading3: StoryObj<typeof meta> = {
   args: {
     level: 3,
     children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearance: 'level-3',
   },
   name: 'Heading 3',
 };
@@ -74,6 +72,7 @@ export const Heading4: StoryObj<typeof meta> = {
   args: {
     level: 4,
     children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearance: 'level-4',
   },
   name: 'Heading 4',
 };
@@ -82,6 +81,7 @@ export const Heading5: StoryObj<typeof meta> = {
   args: {
     level: 5,
     children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearance: 'level-5',
   },
   name: 'Heading 5',
 };
