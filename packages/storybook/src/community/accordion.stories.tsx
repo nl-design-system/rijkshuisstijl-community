e99fc70ddea3dc0f69d0c4a63be88ed35c569469
Ruben Smit <rsmit@quintor.local>
2025-01-30 12:55:53 +0100

    test chromatic build

--- packages/storybook/src/community/accordion.stories.tsx
+++ packages/storybook/src/community/accordion.stories.tsx
@@ -103,7 +103,7 @@ const accordionDefaultDataAR = {
 };
 const accordionData = [
   {
-    label: 'Lorem ipsum 1!!!!!!!!!!!!!!!!!!!!!!!!',
+    label: 'Lorem ipsum 1!!!!!!!!!!!!!!!!!!!!!!!!!',
     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
@@ -112,7 +112,7 @@ const accordionData = [
     expanded: true,
   },
   {
-    label: 'Lorem ipsum 2!!!!!!!!!!!!!!!!!!!!!!!!',
+    label: 'Lorem ipsum 2!!!!!!!!!!!!!!!!!!!!!!!!!',
     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
