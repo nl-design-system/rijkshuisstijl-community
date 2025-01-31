0f734c7d135cd90631d44b7f1866383037f62763
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2025-01-31 08:22:44 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@39c8aebb65d9400f4d0337955febee4862061f2a 🚀

---
+++ assets/font-Cz4ncVu5.js
@@ -0,0 +1,46 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as o}from"./index-CV3Y8eP2.js";import"./chunk-NUUEMKO5-CGMz5B6z.js";import{ak as s,al as r}from"./index-BteQbYPT.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./iframe-3XUk-PBj.js";import"../sb-preview/runtime.js";import"./client-BK4qmh2z.js";import"./index-COgOdkeX.js";import"./index-C19XUJDJ.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";const a=`<!-- @license CC0-1.0 -->
+
+<!-- markdownlint-disable MD033 -->
+
+# Rijkshuisstijl Community font
+
+> <span lang="en">This is not the Rijkshuisstijl font, this is just a tribute.</span>
+>
+> — Tenacious B
+
+Dit npm pakketje met fonts kun je gebruiken als alternatief op de officiële Rijkshuisstijl fonts, voor situaties waar je geen toestemming hebt om de officiële fonts te gebruiken.
+
+- Alternatief voor **Rijkshuisstijl Web Sans**: wij gebruiken Fira Sans als een alternatief voor Rijkshuisstijl Web Sans.
+- Alternatief voor **Rijkshuisstijl Web Serif**: er is nog geen goed alternatief gevonden voor Rijkshuisstijl Web Serif, daarom gebruiken we het sans-serif font Fira Sans ook als alternatief voor het serif font.
+
+## Downloaden
+
+Designers kunnen de font [Fira Sans downloaden via Fontsource](https://fontsource.org/fonts/fira-sans).
+
+## Installatie
+
+Installeer de font in Node.js projecten door het volgende commando in de terminal uit te voeren:
+
+\`\`\`shell
+npm install -D @rijkshuisstijl-community/font
+\`\`\`
+
+Gebruik in JavaScript frameworks zoals React en Angular de volgende code:
+
+\`\`\`mjs
+import '@rijkshuisstijl-community/font/src/index.mjs';
+\`\`\`
+
+## Op zoek naar de officiële lettertypes?
+
+De officiële lettertypes kun je downloaden via [rijkshuisstijl.nl](https://www.rijkshuisstijl.nl/publiek/modules/product/DigitalStyleGuide/default/index.aspx?ItemId=6745), als je voldoende rechten hebt. Bijvoorbeeld als je bij de Rijksoverheid werkt en je ingelogd bent via Citrix.
+
+### Fonts downloaden via de network tab van developer tools
+
+Dat is niet de bedoeling. De officiële manier is om aan de font files te komen is via rijkshuisstijl.nl, via de eerdergenoemde link.
+
+### Fonts opslaan GitHub
+
+Dat is niet de bedoeling. Als je de officiële fonts opslaat in een publieke GitHub repository, dan kun je e-mails verwachten met een vriendelijk doch dwingend verzoek om ze er weer te verwijderen.
+`;function t(n){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Rijkshuisstijl Community/Font"}),`
+`,e.jsx(r,{children:a})]})}function w(n={}){const{wrapper:i}={...o(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(t,{...n})}):t()}export{w as default};
