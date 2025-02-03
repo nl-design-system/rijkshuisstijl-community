3eef3f2a2578e4686cfe9efb62c7b5748ce73faa
sjimbonator <43266491+sjimbonator@users.noreply.github.com>
2025-02-03 14:38:18 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@7ebec02907e210e3ff26bf9718daca62d492150b 🚀

---
+++ assets/column-layout.stories-DqMzT7RC.js
@@ -0,0 +1,27 @@
+import{j as i}from"./jsx-runtime-D_zvdyIk.js";import{m}from"./merge-markdown-BtgmYwh_.js";import{C as n,A as s}from"./index-C4jjA_3H.js";import{H as u}from"./heading-BJGfgtDt.js";import"./_commonjsHelpers-C932wzq6.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-C5EOLjWw.js";import"./index-DKyXVfeP.js";import"./clsx-B-dksMZM.js";const l=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community column layout component
+
+De column layout verdeeld content over meerdere kolommen
+
+## Usage
+
+\`\`\`tsx
+import { ColumnLayout } from '@rijkshuisstijl-community/components-react';
+
+<ColumnLayout>
+  <Heading level={2}>Column layout example</Heading>,
+  <Article>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus nobis
+    sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?
+  </Article>
+</ColumnLayout>;
+\`\`\`
+`,j={title:"Rijkshuisstijl/ColumnLayout",id:"rhc-columnLayout",component:n,parameters:{status:{type:"UNSTABLE"},docs:{description:{component:m([l])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht."},args:{children:""},render:r=>i.jsx(n,{...r})},e={args:{children:[i.jsx(u,{level:2,children:"Column layout"}),i.jsx(s,{children:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus nobis sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?"})]}};var a,o,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: [<Heading level={2}>Column layout</Heading>, <Article>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus
+        nobis sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?
+      </Article>]
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const L=["Default"];export{e as Default,L as __namedExportsOrder,j as default};
---
+++ assets/navbar.stories-DApHEUdj.js
@@ -0,0 +1,19 @@
+import{N as g}from"./index.esm-B0tRZT37.js";/* empty css              */import"./index-C4jjA_3H.js";import"./objectWithoutProperties-BJfzkg-c.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DKyXVfeP.js";import"./clsx-B-dksMZM.js";import"./heading-BJGfgtDt.js";import"./index-CgpWq_u8.js";const S={title:"Rijkshuisstijl/NavBar",id:"rhc-nav-bar",component:g,parameters:{status:{type:"UNSTABLE"},componentOrigin:"Dit component is volledig ontwikkeld door de Rijkshuisstijl Community."}},t=[{id:"first-link",label:"Link",href:"/"},{id:"second-link",label:"Link",href:"/"},{id:"third-link",label:"Link",href:"/"}],h=[{id:"end-first-link",label:"Link",href:"/"},{id:"end-second-link",label:"Link",href:"/"}],e={args:{items:t}},r={args:{items:t,headingItem:{id:"heading",label:"Heading",href:"/"}}},i={args:{items:t,endItems:h}};var n,s,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    items
+  }
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var o,m,d;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items,
+    headingItem: {
+      id: 'heading',
+      label: 'Heading',
+      href: '/'
+    }
+  }
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var c,l,p;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    items,
+    endItems
+  }
+}`,...(p=(l=i.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const W=["Default","WithHeading","WithEndItems"];export{e as Default,i as WithEndItems,r as WithHeading,W as __namedExportsOrder,S as default};
