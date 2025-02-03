929e7ed439669e10dbb4b6e4566a1b541e6bb1ca
Joeri Roijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2025-02-03 09:44:17 +0100

    Feat/wc button (#1176)
    https://github.com/nl-design-system/rijkshuisstijl-community/issues/1173
    
    ---------
    
    Co-authored-by: Ruben Smit <rsmit@quintor.nl>
    Co-authored-by: Ruben Smit <rsmit@quintor.local>

---
+++ packages/web-components/src/components/Button.tsx
@@ -0,0 +1,34 @@
+import stylesheet from '@rijkshuisstijl-community/components-css/dist/index.css?inline';
+import { Button, ButtonProps } from '@rijkshuisstijl-community/components-react';
+import { BaseWebComponent, Slot } from './BaseComponent';
+
+export type ButtonWebComponentAttributes = ButtonProps;
+
+export class ButtonWebComponent extends BaseWebComponent {
+  static override readonly tagName: string = 'rhc-button';
+  static override readonly observedAttributes: string[] = ['appearance', 'disabled'];
+
+  constructor() {
+    super(stylesheet);
+  }
+
+  override setupRestProps(): void {
+    for (const attributeName of this.getAttributeNames()) {
+      if (ButtonWebComponent.observedAttributes.includes(attributeName)) continue;
+      this.restProps[attributeName] = this.getAttribute(attributeName);
+    }
+  }
+
+  render(): void {
+    this.root.render(
+      <Button
+        suppressHydrationWarning
+        appearance={(this.getAttribute('appearance') as ButtonProps['appearance']) ?? undefined}
+        disabled={Boolean(this.getAttribute('disabled')) as ButtonProps['disabled']}
+        {...this.restProps}
+      >
+        <Slot>{this.innerHTML}</Slot>
+      </Button>,
+    );
+  }
+}
