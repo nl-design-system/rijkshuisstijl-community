6728324c13e6bd1fdec17cc899518a617735406a
Ruben Smit <rsmit@quintor.local>
2025-02-04 11:52:49 +0100

    test pr

--- packages/storybook/src/community/accordion.stories.tsx
+++ packages/storybook/src/community/accordion.stories.tsx
@@ -103,7 +103,7 @@ const accordionDefaultDataAR = {
 };
 const accordionData = [
   {
-    label: 'Lorem ipsum 1',
+    label: 'Lorem ipsum 1!',
     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
