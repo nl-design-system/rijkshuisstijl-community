585b03c250b0b28f47ce410c893778b3cda53eb4
AdhamAH <development@adhamabohasson.com>
2025-02-05 14:14:06 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@e61451b1d578da7f4995e629f4a52a31027cca91 🚀

---
+++ assets/action-group.stories-DzyrFHi5.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-CLpGMVip.js";import{a as u}from"./index-DbvYvHaq.js";import{m}from"./merge-markdown-BtgmYwh_.js";import{r as d}from"./action-group-CPnBsn6C.js";import{B as a}from"./index-Be-aV6DI.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-DAIO8Scy.js";u.define();const j={title:"Web Components/Action Group",id:"rhc-action-group-web",component:"rhc-action-group",argTypes:{direction:{description:"Layout of the action group",control:"select",options:["column","row"],table:{defaultValue:{summary:"row"}}}},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:m([d])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht (daar heeft het de naam ButtonGroup), met styling van de Rijkshuisstijl Community."}},t={args:{children:[n.jsx(a,{appearance:"primary-action-button",children:"Save and continue"},"primary-action-button"),n.jsx(a,{appearance:"secondary-action-button",children:"Back"},"secondary-action-button")],direction:"row"}},o={args:{direction:"column",children:[n.jsx(a,{appearance:"primary-action-button",children:"Save and continue"},"primary-action-button"),n.jsx(a,{appearance:"secondary-action-button",children:"Back"},"secondary-action-button")]},name:"Column"};var e,r,c;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: [<Button appearance="primary-action-button" key="primary-action-button">
+        Save and continue
+      </Button>, <Button appearance="secondary-action-button" key="secondary-action-button">
+        Back
+      </Button>],
+    direction: 'row'
+  }
+} as StoryObj<typeof meta>`,...(c=(r=t.parameters)==null?void 0:r.docs)==null?void 0:c.source}}};var i,s,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    direction: 'column',
+    children: [<Button appearance="primary-action-button" key="primary-action-button">
+        Save and continue
+      </Button>, <Button appearance="secondary-action-button" key="secondary-action-button">
+        Back
+      </Button>]
+  },
+  name: 'Column'
+} as StoryObj<typeof meta>`,...(p=(s=o.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const k=["Default","Column"];export{o as Column,t as Default,k as __namedExportsOrder,j as default};
---
+++ assets/separator.stories-Cmtbpt36.js
@@ -0,0 +1,16 @@
+import{v as m}from"./index.esm-aHhU-aAC.js";import{m as d}from"./merge-markdown-BtgmYwh_.js";/* empty css              */import"./index-BLZjEvYy.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-CLpGMVip.js";import"./index-Be-aV6DI.js";import"./clsx-B-dksMZM.js";import"./index-D5-rbeG3.js";const p=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community separator component
+
+Scheidingslijnen worden gebruikt om de scanbaarheid van een lange contentpagina te vergroten.
+
+## Terminologie
+
+- **separator**, van de ARIA \`role="separator"\`. "hr" van \`<hr>\` uit HTML is te onduidelijk buiten de context van code, "horizontal rule" te specifieke beschrijving van de originele rendering in HTML. De "separator" zou er ook heel anders uit kunnen zien dan een "rule" (lijn), en bovendien kan er voor andere tekstrichtingen wel eens een verticale variant komen.
+
+"Divider" is ook een vaak gehoorde term hiervoor, misschien moet deze voor vindbaarheid ook genoemd worden in de documentatie.
+`,y={title:"Rijkshuisstijl/Separator",id:"rhc-separator",component:m,parameters:{status:{type:"STABLE"},docs:{description:{component:d([p])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen en styling van de Rijkshuisstijl Community."},argTypes:{invisible:{control:{type:"boolean"}}}},e={},n={args:{invisible:!0}};var r,o,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var t,a,s;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    invisible: true
+  }
+}`,...(s=(a=n.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const S=["Default","Invisible"];export{e as Default,n as Invisible,S as __namedExportsOrder,y as default};
