3217804cd81038e314b3346a003eff9c15595080
AdhamAH <development@adhamabohasson.com>
2025-02-07 08:56:10 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@63067661c8ffeeaf64c746b2ed20a8c802e987bb 🚀

---
+++ assets/button.stories-Cr-G3NFy.js
@@ -0,0 +1,20 @@
+import{F as g}from"./index-C3n554yS.js";import{a as f,v as S,h,w as A}from"./wcag.nl-ouvEKCqw.js";import{m as D}from"./merge-markdown-BtgmYwh_.js";import{r as j}from"./hero-C6yK1SJZ.js";import"./_commonjsHelpers-C932wzq6.js";g.define();const x={title:"Web Components/Button",id:"rhc-button-web",component:"rhc-button",argTypes:{appearance:{description:"Button appearance",control:{type:"select"},options:["","primary-action-button","secondary-action-button","subtle-button"],table:{category:"Attributes"},defaultValue:""},disabled:{table:{category:"Attributes"},defaultValue:!1},children:{description:"Button text - Custom Elements",type:{name:"string",required:!0},table:{category:"Attributes"},defaultValue:""}},args:{},tags:["autodocs"],parameters:{status:{type:"UNSTABLE"},docs:{description:{component:D([j,f,S,h,A])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen (voor de IconButton) en styling van de Rijkshuisstijl Community."}},e={args:{children:"Default"}},t={args:{children:"PrimaryAction",appearance:"primary-action-button"}},a={args:{children:"SecondaryAction",appearance:"secondary-action-button"}},r={args:{children:"SecondaryAction",appearance:"subtle-button"}};var o,n,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Default'
+  }
+} as StoryObj<typeof meta>`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var c,i,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'PrimaryAction',
+    appearance: 'primary-action-button'
+  }
+} as StoryObj<typeof meta>`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var m,u,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'SecondaryAction',
+    appearance: 'secondary-action-button'
+  }
+} as StoryObj<typeof meta>`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var l,y,b;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'SecondaryAction',
+    appearance: 'subtle-button'
+  }
+} as StoryObj<typeof meta>`,...(b=(y=r.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};const C=["Default","PrimaryAction","SecondaryAction","Subtle"];export{e as Default,t as PrimaryAction,a as SecondaryAction,r as Subtle,C as __namedExportsOrder,x as default};
