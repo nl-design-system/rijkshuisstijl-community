1b21633cb6426a0220d6d0c3b9828df070b6c2fa
AdhamAH <development@adhamabohasson.com>
2025-02-10 14:57:10 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@b10385cbe6e209bd96a995d826e2bc0f1a64b5b9 🚀

---
+++ assets/flag-icon-set.stories-D165gTaU.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-CLpGMVip.js";import{H as d,I as l,R as u}from"./index.esm-BjcEwrCA.js";import{i}from"./icon-set-H98LQYBj.js";import{P as g}from"./index-D05RDfxL.js";/* empty css              */import{m as f}from"./merge-markdown-BtgmYwh_.js";/* empty css              */import"./index-DqXW3Q7L.js";import"./objectWithoutProperties-CbCxjZzm.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-Be-aV6DI.js";import"./clsx-B-dksMZM.js";import"./index-D5-rbeG3.js";u(i);function c(){const a=new Intl.DisplayNames(["nl"],{type:"region"}),m=Object.keys(i).sort((e,p)=>e.localeCompare(p));return o.jsx(g,{children:o.jsx("div",{className:"rhc-icon-set",children:m.map(e=>o.jsxs("div",{className:"rhc-icon-set-item",children:[o.jsx(d,{appearanceLevel:5,level:1,children:a.of(e.substring(0,2).toUpperCase())}),o.jsx(l,{icon:e}),o.jsx("span",{children:e})]},e))})})}c.__docgenInfo={description:"",methods:[],displayName:"IconSet"};const h=`# Rijkshuisstijl Community Flag Icon Set
+
+Onderstaande iconen komen uit een losse icon set van de Rijkshuisstijl Commmunity. Voor gebruik moet deze set eerst geregistreerd worden. Zie ook de storybook pagina over de [Icon component](?path=/docs/rhc-icon--docs) en de [Standaard Icon set](?path=/docs/rhc-templates-default-icon-set--docs)
+
+## Usage
+
+\`\`\`tsx
+import { Icon, registerIconSet } from '@rijkshuisstijl-community/components-react';
+import { iconSet } from '@rijkshuisstijl-community/components-react/src/icon-sets/flag-icons/icon-set';
+
+registerIconSet(iconSet);
+
+<Icon icon="nl-flag" />;
+\`\`\`
+`,O={title:"Icon Sets/Flag Icon Set",id:"rhc-templates-flag-icon-set",component:c,parameters:{docs:{description:{component:f([h])}},status:{type:"UNSTABLE"},isPage:!0}},t={};var s,n,r;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(r=(n=t.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const P=["Default"];export{t as Default,P as __namedExportsOrder,O as default};
---
+++ assets/navigation-list-item.stories-BBX5c0Wn.js
@@ -0,0 +1,30 @@
+import{j as i}from"./jsx-runtime-CLpGMVip.js";import{q as r,r as H,s as j}from"./index.esm-BjcEwrCA.js";import{m as w}from"./merge-markdown-BtgmYwh_.js";/* empty css              */import"./index-DqXW3Q7L.js";import"./objectWithoutProperties-CbCxjZzm.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-Be-aV6DI.js";import"./clsx-B-dksMZM.js";import"./index-D5-rbeG3.js";const f=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community Navigation List Item component
+
+## Usage
+
+\`\`\`tsx
+import { NavigationListItem } from '@rijkshuisstijl-community/components-react';
+
+<NavigationListItem description="Uw gegevens, familie en identiteitsbewijs" href="#" icon="user" label="Identiteit" />;
+\`\`\`
+`,S={title:"Rijkshuisstijl/NavigationList/NavigationListItem",component:r,parameters:{status:{type:"STABLE"},docs:{description:{component:w([f])}},figma:"https://www.figma.com/design/Nv5EsCW9ioWBUSi9m9JqOa/Local---Rijkshuisstijl---Bibliotheek?node-id=4074-1580&node-type=canvas&t=HuDzyBW9wHdB2QVh-0",github:"https://github.com/nl-design-system/rijkshuisstijl-community/issues/557",componentOrigin:"Dit component is volledig ontwikkeld door de Rijkshuisstijl Community."},argTypes:{description:{control:{type:"text"}},href:{control:{type:"text"}},icon:{control:{type:"select"},options:Object.keys(j())},label:{control:{type:"text"}}},args:{description:"Rijkshuisstijl Community Navigation List Item",label:"Label",href:"#",icon:"activiteit"},render:v=>i.jsx(H,{children:i.jsx(r,{...v})})},e={},t={args:{icon:i.jsx("svg",{fill:"none",height:"20",viewBox:"0 0 18 20",width:"18",xmlns:"http://www.w3.org/2000/svg",children:i.jsx("path",{clipRule:"evenodd",d:"M5 0C5.55228 0 6 0.447715 6 1V2H12V1C12 0.447715 12.4477 0 13 0C13.5523 0 14 0.447715 14 1V2H15C16.6569 2 18 3.34315 18 5V17C18 18.6569 16.6569 20 15 20H3C1.34315 20 0 18.6569 0 17V5C0 3.34315 1.34315 2 3 2H4V1C4 0.447715 4.44772 0 5 0ZM4 4H3C2.44772 4 2 4.44772 2 5V8H16V5C16 4.44772 15.5523 4 15 4H14V5C14 5.55228 13.5523 6 13 6C12.4477 6 12 5.55228 12 5V4H6V5C6 5.55228 5.55228 6 5 6C4.44772 6 4 5.55228 4 5V4ZM16 10H2V17C2 17.5523 2.44772 18 3 18H15C15.5523 18 16 17.5523 16 17V10ZM4 13C4 12.4477 4.44772 12 5 12H7C7.55228 12 8 12.4477 8 13V15C8 15.5523 7.55228 16 7 16H5C4.44772 16 4 15.5523 4 15V13Z",fill:"white",fillRule:"evenodd"})})}},s={parameters:{pseudo:{hover:!0}}},o={parameters:{pseudo:{focus:!0}}};var n,a,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(a=e.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};var c,p,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    icon: <svg fill="none" height="20" viewBox="0 0 18 20" width="18" xmlns="http://www.w3.org/2000/svg">
+        <path clipRule="evenodd" d="M5 0C5.55228 0 6 0.447715 6 1V2H12V1C12 0.447715 12.4477 0 13 0C13.5523 0 14 0.447715 14 1V2H15C16.6569 2 18 3.34315 18 5V17C18 18.6569 16.6569 20 15 20H3C1.34315 20 0 18.6569 0 17V5C0 3.34315 1.34315 2 3 2H4V1C4 0.447715 4.44772 0 5 0ZM4 4H3C2.44772 4 2 4.44772 2 5V8H16V5C16 4.44772 15.5523 4 15 4H14V5C14 5.55228 13.5523 6 13 6C12.4477 6 12 5.55228 12 5V4H6V5C6 5.55228 5.55228 6 5 6C4.44772 6 4 5.55228 4 5V4ZM16 10H2V17C2 17.5523 2.44772 18 3 18H15C15.5523 18 16 17.5523 16 17V10ZM4 13C4 12.4477 4.44772 12 5 12H7C7.55228 12 8 12.4477 8 13V15C8 15.5523 7.55228 16 7 16H5C4.44772 16 4 15.5523 4 15V13Z" fill="white" fillRule="evenodd" />
+      </svg>
+  }
+}`,...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var d,u,C;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(C=(u=s.parameters)==null?void 0:u.docs)==null?void 0:C.source}}};var g,h,V;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  }
+}`,...(V=(h=o.parameters)==null?void 0:h.docs)==null?void 0:V.source}}};const W=["Default","WithCustomIcon","Hover","Focus"];export{e as Default,o as Focus,s as Hover,t as WithCustomIcon,W as __namedExportsOrder,S as default};
