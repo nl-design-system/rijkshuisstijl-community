ce4d46b9bd50a66f0b4f5246b7f5718769a484e0
Rerbun <ruben.smit4@gmail.com>
2025-02-11 09:48:50 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@390fe944d9fe1c94e31178823eb82a188a22032d 🚀

---
+++ assets/column-layout.stories-BMcYQyPf.js
@@ -0,0 +1,27 @@
+import{j as i}from"./jsx-runtime-CLpGMVip.js";import{H as m}from"./index.esm-CyUqGOTz.js";import{m as s}from"./merge-markdown-BtgmYwh_.js";import{C as n,A as u}from"./index-DqXW3Q7L.js";/* empty css              */import"./clsx-B-dksMZM.js";import"./objectWithoutProperties-CbCxjZzm.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-Be-aV6DI.js";import"./index-D5-rbeG3.js";const l=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community column layout component
+
+De column layout verdeeld content over meerdere kolommen
+
+## Usage
+
+\`\`\`tsx
+import { ColumnLayout } from '@rijkshuisstijl-community/components-react';
+
+<ColumnLayout>
+  <Heading level={2}>Column layout example</Heading>,
+  <Article>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus nobis
+    sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?
+  </Article>
+</ColumnLayout>;
+\`\`\`
+`,f={title:"Rijkshuisstijl/ColumnLayout",id:"rhc-columnLayout",component:n,parameters:{status:{type:"UNSTABLE"},docs:{description:{component:s([l])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht."},args:{children:""},render:r=>i.jsx(n,{...r})},e={args:{children:[i.jsx(m,{level:2,children:"Column layout"}),i.jsx(u,{children:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus nobis sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?"})]}};var o,a,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: [<Heading level={2}>Column layout</Heading>, <Article>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam magni expedita modi sit quasi, natus
+        nobis sint hic, eum illum quis laboriosam pariatur quo alias eaque qui quae recusandae?
+      </Article>]
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,f as default};
