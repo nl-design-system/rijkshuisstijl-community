3d97042f827c6d9ab0214452bdb2da2dc411312f
aakminasi <arefcoding@gmail.com>
2025-02-13 13:26:19 +0100

    updated details template

--- packages/storybook/src/templates/mijn-omgeving/index.tsx
+++ packages/storybook/src/templates/mijn-omgeving/index.tsx
@@ -269,14 +269,6 @@ export default function MijnOmgeving() {
             ),
           },
         ]}
-        subFooter={
-          <LinkList className="rhc-subfooter-details">
-            <LinkListLink href="#">Privacy</LinkListLink>
-            <LinkListLink href="#">Cookies</LinkListLink>
-            <LinkListLink href="#">Copyright</LinkListLink>
-            <LinkListLink href="#">Toegankelijkheid</LinkListLink>
-          </LinkList>
-        }
       />
     </>
   );
