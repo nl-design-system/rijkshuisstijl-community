308d5e80ec142188514ceb6ceeb6dfbf925e8bfd
AdhamAH <development@adhamabohasson.com>
2025-02-13 15:04:02 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@e2345f77a0c8331c780566080c13d156dbab77f4 🚀

---
+++ assets/link-list-link.stories-DNFZSCt2.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-CLpGMVip.js";import{I as b}from"./index.esm-Ttg4HFc-.js";import{L as a,b as y}from"./index-D8tk2uj3.js";/* empty css              */import"./clsx-B-dksMZM.js";import"./objectWithoutProperties-CbCxjZzm.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./paragraph-Bm2wF22U.js";import"./index-D5-rbeG3.js";const _={title:"Rijkshuisstijl/Link List/Link List Link",id:"rijkshuisstijl-link-list-link",component:a,args:{children:"Label",href:"#",icon:n.jsx(b,{icon:"chevron-right"})},argTypes:{href:{type:{name:"string",required:!0},table:{category:"HTML attribute"}}},parameters:{status:{type:"STABLE"},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, de design tokens kunnen indien nodig nog overschreven worden."},render:S=>n.jsx(y,{children:n.jsx(a,{...S})})},e={args:{children:"Label",href:"#",icon:n.jsx(b,{icon:"chevron-right"})}},r={parameters:{pseudo:{hover:!0}}},s={parameters:{pseudo:{active:!0}}},t={parameters:{pseudo:{focus:!0}},args:{}},o={parameters:{pseudo:{visited:!0}}};var i,c,p;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    children: 'Label',
+    href: '#',
+    icon: <Icon icon={'chevron-right'} />
+  }
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,u,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(d=(u=r.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var l,g,h;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      active: true
+    }
+  }
+}`,...(h=(g=s.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var v,L,f;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  },
+  args: {}
+}`,...(f=(L=t.parameters)==null?void 0:L.docs)==null?void 0:f.source}}};var k,j,x;o.parameters={...o.parameters,docs:{...(k=o.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      visited: true
+    }
+  }
+}`,...(x=(j=o.parameters)==null?void 0:j.docs)==null?void 0:x.source}}};const q=["Default","Hover","Active","Focus","Visited"];export{s as Active,e as Default,t as Focus,r as Hover,o as Visited,q as __namedExportsOrder,_ as default};
---
+++ assets/separator.stories-S_SeZvOO.js
@@ -0,0 +1,16 @@
+import{u as m}from"./index.esm-Ttg4HFc-.js";import{m as p}from"./merge-markdown-BtgmYwh_.js";/* empty css              */import"./index-D8tk2uj3.js";import"./objectWithoutProperties-CbCxjZzm.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-CLpGMVip.js";import"./clsx-B-dksMZM.js";import"./paragraph-Bm2wF22U.js";import"./index-D5-rbeG3.js";const d=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community separator component
+
+Scheidingslijnen worden gebruikt om de scanbaarheid van een lange contentpagina te vergroten.
+
+## Terminologie
+
+- **separator**, van de ARIA \`role="separator"\`. "hr" van \`<hr>\` uit HTML is te onduidelijk buiten de context van code, "horizontal rule" te specifieke beschrijving van de originele rendering in HTML. De "separator" zou er ook heel anders uit kunnen zien dan een "rule" (lijn), en bovendien kan er voor andere tekstrichtingen wel eens een verticale variant komen.
+
+"Divider" is ook een vaak gehoorde term hiervoor, misschien moet deze voor vindbaarheid ook genoemd worden in de documentatie.
+`,S={title:"Rijkshuisstijl/Separator",id:"rhc-separator",component:m,parameters:{status:{type:"STABLE"},docs:{description:{component:p([d])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen en styling van de Rijkshuisstijl Community."},argTypes:{invisible:{control:{type:"boolean"}}}},e={},n={args:{invisible:!0}};var r,o,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var t,a,s;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    invisible: true
+  }
+}`,...(s=(a=n.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const T=["Default","Invisible"];export{e as Default,n as Invisible,T as __namedExportsOrder,S as default};
