a48968d35cfa463d2bc3ce24ef34743584d60368
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2025-02-17 10:25:54 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@0d78c33620a55d781a26bd45efc0151a53f87c7f 🚀

---
+++ assets/side-nav.stories-BTbbfAQX.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-CLpGMVip.js";import{a1 as c,a2 as r,a3 as i,a0 as n,$ as t,X as v}from"./index.esm-D8WgDWwC.js";import{m as l}from"./merge-markdown-BtgmYwh_.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";const m=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community SideNav component
+`,u={title:"Rijkshuisstijl/SideNav",id:"rhc-sideNav",component:c,tags:["autodocs"],argTypes:{},parameters:{status:{type:"UNSTABLE"},docs:{description:{component:l([m])}},componentOrigin:"Dit component is volledig ontwikkeld door de Rijkshuisstijl Community."}},a={args:{children:[e.jsx(r,{children:e.jsx(i,{children:e.jsx(n,{current:!0,href:"/#",icon:"tegelweergave",children:"Overzicht"})})},"ul-1"),e.jsx(t,{invisible:!0},"separator-1"),e.jsxs(r,{children:[e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"activiteit",children:"Mijn taken"})}),e.jsx(i,{children:e.jsxs(n,{href:"/#",icon:"mail",children:["Mijn berichten",e.jsx(v,{children:"2"})]})}),e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"bewerken",children:"Mijn lopende zaken"})})]},"ul-2"),e.jsx(t,{invisible:!0},"separator-2"),e.jsx(r,{children:e.jsx(i,{children:e.jsx(n,{href:"/#",icon:"user",children:"Mijn gegevens"})})},"ul-3")]}};var s,d,o;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: [<SideNavList key="ul-1">
+        <SideNavItem>
+          <SideNavLink current href="/#" icon="tegelweergave">
+            Overzicht
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>, <Separator invisible key="separator-1" />, <SideNavList key="ul-2">
+        <SideNavItem>
+          <SideNavLink href="/#" icon="activiteit">
+            Mijn taken
+          </SideNavLink>
+        </SideNavItem>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="mail">
+            Mijn berichten
+            <NumberBadge>2</NumberBadge>
+          </SideNavLink>
+        </SideNavItem>
+        <SideNavItem>
+          <SideNavLink href="/#" icon="bewerken">
+            Mijn lopende zaken
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>, <Separator invisible key="separator-2" />, <SideNavList key="ul-3">
+        <SideNavItem>
+          <SideNavLink href="/#" icon="user">
+            Mijn gegevens
+          </SideNavLink>
+        </SideNavItem>
+      </SideNavList>]
+  }
+}`,...(o=(d=a.parameters)==null?void 0:d.docs)==null?void 0:o.source}}};const k=["Default"];export{a as Default,k as __namedExportsOrder,u as default};
