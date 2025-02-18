3821d36ef2b6da11bfda18c9253dabf6174d46b3
Rerbun <ruben.smit4@gmail.com>
2025-02-18 08:44:59 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@71a071e85a64afe41d2c60213709eff3eacb3248 🚀

---
+++ assets/navbar.stories-CVGR1Bru.js
@@ -0,0 +1,19 @@
+import{N as g}from"./index.esm-_2RVA9UP.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-CLpGMVip.js";const I={title:"Rijkshuisstijl/NavBar",id:"rhc-nav-bar",component:g,parameters:{status:{type:"UNSTABLE"},componentOrigin:"Dit component is volledig ontwikkeld door de Rijkshuisstijl Community."}},s=[{id:"first-link",label:"Link",href:"/"},{id:"second-link",label:"Link",href:"/"},{id:"third-link",label:"Link",href:"/"}],h=[{id:"end-first-link",label:"Link",href:"/"},{id:"end-second-link",label:"Link",href:"/"}],e={args:{items:s}},r={args:{items:s,headingItem:{id:"heading",label:"Heading",href:"/"}}},n={args:{items:s,endItems:h}};var a,i,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    items
+  }
+}`,...(t=(i=e.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};var o,d,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items,
+    headingItem: {
+      id: 'heading',
+      label: 'Heading',
+      href: '/'
+    }
+  }
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var c,l,p;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    items,
+    endItems
+  }
+}`,...(p=(l=n.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const L=["Default","WithHeading","WithEndItems"];export{e as Default,n as WithEndItems,r as WithHeading,L as __namedExportsOrder,I as default};
