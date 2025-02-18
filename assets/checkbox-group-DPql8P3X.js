a7c123a71123f81b7b0d53db4a1339a925eff71e
JoeriRoijenga <16878956+JoeriRoijenga@users.noreply.github.com>
2025-02-18 10:16:57 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@72af7767b710c40ac5861aeebbd483e334811df7 🚀

---
+++ assets/checkbox-group-DPql8P3X.js
@@ -0,0 +1,15 @@
+const o=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community CheckboxGroup component
+
+## Usage
+
+\`\`\`tsx
+import { CheckboxGroup, Checkbox } from '@rijkshuisstijl-community/components-react';
+
+<CheckboxGroup>
+  <Checkbox>Checkbox 1</Checkbox>
+  <Checkbox>Checkbox 2</Checkbox>
+</CheckboxGroup>;
+\`\`\`
+`;export{o as r};
---
+++ assets/form-field-checkbox-option-DZ8-W3dn.js
@@ -0,0 +1,12 @@
+const n=`<!-- @license CC0-1.0 -->
+
+# Rijkshuisstijl Community FormFieldCheckbox component
+
+## Usage
+
+\`\`\`tsx
+import { FormFieldCheckboxOption } from '@rijkshuisstijl-community/components-react';
+
+<FormFieldCheckboxOption label="Checkbox" />;
+\`\`\`
+`;export{n as r};
