a9c8ab77d07a0c457391d206c71ae270fb056ecc
AdhamAH <development@adhamabohasson.com>
2025-02-20 08:57:32 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@417898cdc4b71909178333efdddf86e8e8c8bae5 🚀

---
+++ assets/heading.stories-Bqhc39-P.js
@@ -0,0 +1,38 @@
+import{h as o,H as x}from"./index.esm-BTUbJIBa.js";import{r as A}from"./heading-BhZlH--z.js";import{m as O}from"./merge-markdown-BtgmYwh_.js";import"./index-DAIO8Scy.js";import"./_commonjsHelpers-C932wzq6.js";import"./jsx-runtime-CLpGMVip.js";/* @license CC0-1.0 */const N={title:"Rijkshuisstijl/Heading",id:"rhc-heading",component:x,argTypes:{level:{description:"Heading level",control:{type:"select"},options:o},appearanceLevel:{description:"Appearance level",control:{type:"select"},options:o}},parameters:{status:{type:"STABLE"},docs:{description:{component:O([A])}},figma:"https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=153-1039&t=9KlJWkn6GEOm2sN1-11",github:"https://github.com/nl-design-system/rijkshuisstijl-community/issues/469",nldesignsystem:"https://nldesignsystem.nl/heading/",componentOrigin:"Dit component is overgenomen van de NL Design System Heading Candidate met extra functionaliteit, en met overgeschreven design tokens van de Rijkshuisstijl Community."}},e={args:{level:1,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Heading 1"},s={args:{level:2,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Heading 2"},i={args:{level:3,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Heading 3"},n={args:{level:4,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Heading 4"},a={args:{level:5,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod"},name:"Heading 5"},r={args:{level:1,children:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",appearanceLevel:5},name:"Heading 5 (in appearance)"};var t,d,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    level: 1,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Heading 1'
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var m,l,p;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    level: 2,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Heading 2'
+}`,...(p=(l=s.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var g,u,H;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    level: 3,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Heading 3'
+}`,...(H=(u=i.parameters)==null?void 0:u.docs)==null?void 0:H.source}}};var h,v,L;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    level: 4,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Heading 4'
+}`,...(L=(v=n.parameters)==null?void 0:v.docs)==null?void 0:L.source}}};var y,S,f;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    level: 5,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod'
+  },
+  name: 'Heading 5'
+}`,...(f=(S=a.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var j,k,w;r.parameters={...r.parameters,docs:{...(j=r.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    level: 1,
+    children: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    appearanceLevel: 5
+  },
+  name: 'Heading 5 (in appearance)'
+}`,...(w=(k=r.parameters)==null?void 0:k.docs)==null?void 0:w.source}}};const T=["Heading1","Heading2","Heading3","Heading4","Heading5","Heading5Appearance"];export{e as Heading1,s as Heading2,i as Heading3,n as Heading4,a as Heading5,r as Heading5Appearance,T as __namedExportsOrder,N as default};
