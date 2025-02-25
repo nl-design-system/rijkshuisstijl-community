b6c57db0b59f63fee7108a6ab7a290b8d9a2f695
eslook <42849307+eslook@users.noreply.github.com>
2025-02-25 17:40:53 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@66667f8b4154f135dfe1d79aa165c972490b8170 🚀

---
+++ assets/logo.stories-Ct7W7J1l.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{R as C,I as f}from"./index.esm-BPdtCe4z.js";import"./index-C5EOLjWw.js";import"./_commonjsHelpers-C932wzq6.js";const g="#fff",m="#154273",d="#154273",t=({imageBackgroundColor:u,textColor:p,imageIconColor:b,...h})=>o.jsx(o.Fragment,{children:o.jsx(C,{...h,style:{"--rhc-logo-color":p||d,"--rhc-logo-image-background-color":u||g,"--rhc-logo-image-color":b||m},children:o.jsx(f,{icon:"nederland-map"})})}),L={title:"Rijkshuisstijl/Logo",id:"rhc-logo",component:t,args:{organisation:"",subtitle:"",imageBackgroundColor:g,imageIconColor:m,textColor:d},argTypes:{organisation:{name:"organisation",type:{name:"string",required:!0}},subtitle:{name:"subtitle",type:{name:"string",required:!1}},imageBackgroundColor:{control:"color",description:"Changes background color of the image",table:{category:"Demo"}},imageIconColor:{control:"color",description:"Changes color of the image icon",table:{category:"Demo"}},textColor:{control:"color",description:"Changes text color",table:{category:"Demo"}}},parameters:{status:{type:"STABLE"},componentOrigin:"Dit component is volledig ontwikkeld door de Rijkshuisstijl Community."},render:t},e={args:{organisation:"Voorbeeld organisatie"}},r={args:{organisation:"Voorbeeld organisatie",subtitle:"Voorbeeld sub-title"}};var a,s,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    organisation: 'Voorbeeld organisatie'
+  }
+}`,...(i=(s=e.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var n,c,l;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    organisation: 'Voorbeeld organisatie',
+    subtitle: 'Voorbeeld sub-title'
+  }
+}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const D=["Default","WithSubtitle"];export{e as Default,r as WithSubtitle,D as __namedExportsOrder,L as default};
