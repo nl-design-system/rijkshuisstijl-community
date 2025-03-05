2284abf359a4805064db7a53bffe010ecf7ad3c4
Rerbun <ruben.smit4@gmail.com>
2025-03-05 09:27:47 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@1dc03fb8df4dc55ac55ab8789600f0c8110356dd 🚀

---
+++ assets/index-pv8SIZMw.js
@@ -0,0 +1,1 @@
+import"./preview-CsTvBXl3.js";import"./DocsRenderer-CFRXHY34-LiQ04Rdy.js";import"./index-BR_pYMs-.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
---
+++ assets/link.stories-B1cJaLb4.js
@@ -0,0 +1,62 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{t as O,u as W,I as m}from"./index.esm-B9c0Ny6h.js";import{r as _,i as B,u as z,a as J,v as K,c as Q,b as X,t as Y,p as Z,f as $,d as ee,e as re,g as ae,h as se,j as te,k as ne,w as oe}from"./wcag.nl-DO89JAHl.js";import{m as ce}from"./merge-markdown-BBBI8PmH.js";import{I as ie,a as le}from"./IconCalendarEvent-Bd5ICa3p.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";import"./createReactComponent-h1bB38t0.js";const p=({href:P,children:T,iconLeft:U,iconRight:q,external:l,className:G,...M})=>e.jsxs(O,{external:l,href:P,className:W({"utrecht-link--external":l},G),...M,children:[U&&e.jsx(m,{children:e.jsx(ie,{})}),T,q&&e.jsx(m,{children:e.jsx(le,{})})]}),Le={title:"Rijkshuisstijl/Link",id:"rhc-link",component:p,argTypes:{href:{description:"Url to link to",type:{name:"string",required:!0},table:{category:"Property"}},external:{description:"Whether the link is external",type:{name:"boolean"},table:{category:"Property"}},externalLabel:{description:"SR only label for external link icon",type:{name:"string"},table:{category:"Property"}},children:{description:"Link text - default webcomponent slot",type:{name:"string",required:!0},table:{category:"Webcomponent Slot"},defaultValue:""},iconLeft:{description:"Icon Left",type:{name:"boolean"},table:{category:"Demo"},defaultValue:!1},iconRight:{description:"Icon Right",type:{name:"boolean"},table:{category:"Demo"},defaultValue:!1}},args:{children:""},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:ce([_,B,z,J,K,Q,X,Y,Z,$,ee,re,ae,se,te,ne,oe])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen en styling van de Rijkshuisstijl Community."},render:p},r={args:{href:"#",children:"Label"}},a={args:{href:"#",children:"Label",iconLeft:!0}},s={args:{href:"#",children:"Label",iconRight:!0}},t={args:{href:"#",children:"Label",external:!0,externalLabel:"example external label"}},n={parameters:{pseudo:{active:!0}},args:{href:"#",children:"Label"}},o={parameters:{pseudo:{focus:!0}},args:{href:"#",children:"Label"}},c={parameters:{},args:{href:"#",children:"Label",className:"utrecht-link--focus-visible"}},i={parameters:{pseudo:{visited:!0}},args:{href:"https://example.com/",children:"Label"}};var u,d,h;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    href: '#',
+    children: 'Label'
+  }
+}`,...(h=(d=r.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var g,f,b;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    href: '#',
+    children: 'Label',
+    iconLeft: true
+  }
+}`,...(b=(f=a.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var L,x,y;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    href: '#',
+    children: 'Label',
+    iconRight: true
+  }
+}`,...(y=(x=s.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};var D,k,v;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    href: '#',
+    children: 'Label',
+    external: true,
+    externalLabel: 'example external label'
+  }
+}`,...(v=(k=t.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};var j,S,I;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      active: true
+    }
+  },
+  args: {
+    href: '#',
+    children: 'Label'
+  }
+}`,...(I=(S=n.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};var R,N,V;o.parameters={...o.parameters,docs:{...(R=o.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  },
+  args: {
+    href: '#',
+    children: 'Label'
+  }
+}`,...(V=(N=o.parameters)==null?void 0:N.docs)==null?void 0:V.source}}};var E,w,A;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  parameters: {},
+  args: {
+    href: '#',
+    children: 'Label',
+    className: 'utrecht-link--focus-visible'
+  }
+}`,...(A=(w=c.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var C,F,H;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      visited: true
+    }
+  },
+  args: {
+    href: 'https://example.com/',
+    children: 'Label'
+  }
+}`,...(H=(F=i.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};const xe=["Default","IconLeft","IconRight","External","Active","Focus","FocusVisible","Visited"];export{n as Active,r as Default,t as External,o as Focus,c as FocusVisible,a as IconLeft,s as IconRight,i as Visited,xe as __namedExportsOrder,Le as default};
