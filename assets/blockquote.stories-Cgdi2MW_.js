7e15b3d8d5f6ce08721977339f394a807e5abfc8
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2025-03-18 08:23:20 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@76554a86a17364cf3c07c20fa692b9df6111a19c 🚀

---
+++ assets/blockquote.stories-Cgdi2MW_.js
@@ -0,0 +1,31 @@
+import{B as I}from"./index.esm-X5UnSiKs.js";import{r as P}from"./blockquote-DQqZQ447.js";import{m as j}from"./merge-markdown-BBBI8PmH.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";import"./jsx-runtime-D_zvdyIk.js";/* @license CC0-1.0 */const O={title:"Rijkshuisstijl/Blockquote",id:"rhc-blockquote",component:I,argTypes:{attribution:{name:"attribution",type:{name:"string",required:!1}},variation:{name:"variation",type:{name:"string",required:!1},control:{type:"select"},options:["","blue-corner-border","pink-background","pink-left-border","pink-corner-border"],defaultValue:""}},args:{children:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"},tags:["autodocs"],parameters:{status:{type:"UNSTABLE"},docs:{description:{component:j([P])}},figma:"https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1349-6340&t=9KlJWkn6GEOm2sN1-11",github:"https://github.com/nl-design-system/rijkshuisstijl-community/issues/461",nldesignsystem:"https://www.nldesignsystem.nl/blockquote",componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen en styling van de Rijkshuisstijl Community."}},e={},r={name:"Attribution",args:{children:'"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',attribution:"— Pippi Langkous"}},t={args:{children:'"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',attribution:"— Pippi Langkous",variation:"blue-corner-border"}},o={args:{children:'"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',attribution:"— Pippi Langkous",variation:"pink-background"}},i={args:{children:'"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',attribution:"— Pippi Langkous",variation:"pink-left-border"}},a={args:{children:'"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',attribution:"— Pippi Langkous",variation:"pink-corner-border"}};var n,s,d;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(d=(s=e.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};var k,u,c;r.parameters={...r.parameters,docs:{...(k=r.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  name: 'Attribution',
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous'
+  }
+}`,...(c=(u=r.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var p,g,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    variation: 'blue-corner-border'
+  }
+}`,...(m=(g=t.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var l,h,b;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    variation: 'pink-background'
+  }
+}`,...(b=(h=o.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var f,w,L;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    variation: 'pink-left-border'
+  }
+}`,...(L=(w=i.parameters)==null?void 0:w.docs)==null?void 0:L.source}}};var v,B,y;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: '"Ik heb het nog nooit gedaan dus ik denk dat ik het wel kan"',
+    attribution: '— Pippi Langkous',
+    variation: 'pink-corner-border'
+  }
+}`,...(y=(B=a.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};const R=["Default","Attribution","blueCornerBorder","pinkBackground","pinkLeftBorder","pinkCornerBorder"];export{r as Attribution,e as Default,R as __namedExportsOrder,t as blueCornerBorder,O as default,o as pinkBackground,a as pinkCornerBorder,i as pinkLeftBorder};
--- assets/index-B8xLjyk5.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CJnrNf3-.js";import"./DocsRenderer-CFRXHY34-Tihel5l9.js";import"./index-B4J_u52n.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
