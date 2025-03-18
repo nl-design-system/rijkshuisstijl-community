84c5e7e54be6ee300ba9299413d5519edca9b7cc
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2025-03-18 15:17:24 +0100

    added heading css

---
+++ packages/components-angular/src/heading/heading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '@nl-design-system-candidate/heading-css/heading.css';
+
+@Component({
+  selector: 'lib-heading',
+  imports: [],
+  templateUrl: './heading.component.html',
+  styleUrl: './heading.component.css',
+})
+export class HeadingComponent {}
