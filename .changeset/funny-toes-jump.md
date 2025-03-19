e4b0c156c1cca44943209c815395cb4b77b3bdcf
Ruben Smit <rsmit@quintor.nl>
2025-03-19 14:29:10 +0100

    fix(card): fixed image no longer being optional in default card variant (#1416)
    https://codefornl.slack.com/archives/C01DAT4TRPF/p1742381899534329
    
    ---------
    
    Co-authored-by: Ruben Smit <rsmit@quintor.local>

---
+++ .changeset/funny-toes-jump.md
@@ -0,0 +1,5 @@
+---
+'@rijkshuisstijl-community/components-react': patch
+---
+
+Fixed image no longer being optional in default card variant and fixed wrong heading and metadata colors
--- packages/components-react/src/Card.test.tsx
+++ packages/components-react/src/Card.test.tsx
@@ -21,12 +21,61 @@ describe('Card', () => {
       expect(navBar).toBeInTheDocument();
       expect(navBar).toBeVisible();
     });
+
     it('renders a link with the correct href and title', () => {
       linkTest(
         <Card heading="Heading" href="/example" linkLabel={'Example Label'} title="Example Title" />,
         '/example',
         'Example Title',
       );
+    });
+
+    describe('optional properties', () => {
+      describe('imageSrc', () => {
+        it('renders an image if imageSrc is provided', () => {
+          const { getByTestId } = render(<Card heading="Heading" href="#" imageAlt="Just for testing" imageSrc="#" />);
+          const imageContainer = getByTestId('rhc-card__image-container');
+          expect(imageContainer).toBeInTheDocument();
+        });
+
+        it('does not render an image if imageSrc is not provided', () => {
+          const { queryByTestId } = render(<Card heading="Heading" href="#" />);
+
+          expect(queryByTestId('rhc-card__image-container')).not.toBeInTheDocument();
+        });
+      });
+
+      describe('linkLabel', () => {
+        it('renders a footer with a link and link label with the correct label', () => {
+          const { queryByTestId } = render(<Card heading="Heading" href="#" linkLabel="Example Label" />);
+          const link = queryByTestId('rhc-card__link');
+
+          expect(link).toBeInTheDocument();
+        });
+
+        it('does not render a footer with a link and link label if linkLabel is not provided', () => {
+          const { queryByTestId } = render(<Card heading="Heading" href="#" />);
+
+          expect(queryByTestId('rhc-card__link')).not.toBeInTheDocument();
+        });
+      });
+
+      describe('button', () => {
+        it('renders a footer with a button if button is provided', () => {
+          const { queryByTestId } = render(
+            <Card button={<button>Example Button</button>} heading="Heading" href="#" />,
+          );
+          const button = queryByTestId('rhc-card__button');
+
+          expect(button).toBeInTheDocument();
+        });
+
+        it('does not render a footer with a button if button is not provided', () => {
+          const { queryByTestId } = render(<Card heading="Heading" href="#" />);
+
+          expect(queryByTestId('rhc-card__button')).not.toBeInTheDocument();
+        });
+      });
     });
   });
 
@@ -37,6 +86,7 @@ describe('Card', () => {
       expect(navBar).toBeInTheDocument();
       expect(navBar).toBeVisible();
     });
+
     it('renders a link with the correct href and title', () => {
       linkTest(
         <FullBleedCard heading="Heading" href="/example" imageSrc="#" title="Example Title" />,
@@ -53,6 +103,7 @@ describe('Card', () => {
       expect(navBar).toBeInTheDocument();
       expect(navBar).toBeVisible();
     });
+
     it('renders a link with the correct href and title', () => {
       linkTest(
         <HorizontalImageCard heading="Heading" href="/example" imageSrc="#" title="Example Title" />,
