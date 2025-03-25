64161bc93ed7b9888c8e3fb487407781b1aef477
Aref-Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2025-03-25 08:04:39 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@cc11bef9f18982d818cd9638a17548e3512df3db 🚀

---
+++ assets/image.stories-aXqiLDl6.js
@@ -0,0 +1,14 @@
+import{K as a}from"./index.esm-BGw14531.js";import{h as n,r as i,w as s}from"./wcag.nl-CbE_ak3H.js";import{m as c}from"./merge-markdown-566X0PcZ.js";import"./index-4CZSx3OK.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./jsx-runtime-D_zvdyIk.js";const m=`<!-- @license CC0-1.0 -->
+
+# Image
+
+Toont een afbeelding.
+`,y={title:"Rijkshuisstijl/Image",id:"rhc-image",component:a,argTypes:{src:{description:"Image source",control:{type:"text"},table:{category:"Property"}},alt:{description:"Image alt text",control:{type:"text"},table:{category:"Property"}},width:{description:"Image width",control:{type:"number"},table:{category:"Property"}},height:{description:"Image height",control:{type:"number"},table:{category:"Property"}}},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:c([m,n,i,s])}},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, maar de photo property altijd geset, daardoor is hij altijd max-height en max-width 100%."}},e={args:{alt:"Multicolored tulip field",height:763,width:640,src:"./placeholder.jpg"},name:"Image"};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    alt: 'Multicolored tulip field',
+    height: 763,
+    width: 640,
+    src: './placeholder.jpg'
+  },
+  name: 'Image'
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,y as default};
---
+++ assets/index-B_fMVYpY.js
@@ -0,0 +1,1 @@
+import"./preview-DS5yw5ma.js";import"./DocsRenderer-CFRXHY34-8G5gz9C3.js";import"./index-CUBdwdNt.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
