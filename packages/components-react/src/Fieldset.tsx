41369e12aded782fa0f0d2dccf2718bc305dcb45
aakminasi <arefcoding@gmail.com>
2025-03-25 13:02:11 +0100

    refactor: fieldset

--- packages/components-react/src/Fieldset.tsx
+++ packages/components-react/src/Fieldset.tsx
@@ -1,6 +1,47 @@
-export {
-  Fieldset,
+export { FieldsetLegend, type FieldsetLegendProps } from '@utrecht/component-library-react';
+import {
   FieldsetLegend,
-  type FieldsetProps,
-  type FieldsetLegendProps,
+  Fieldset as UtrechtFieldSet,
+  FieldsetProps as UtrechtFieldsetProps,
 } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+import { PropsWithChildren, ReactNode } from 'react';
+
+export interface FieldsetProps extends UtrechtFieldsetProps {
+  element?: string | 'div' | 'fieldset';
+  legend?: ReactNode;
+  legendId?: string;
+  section?: boolean;
+}
+
+export const Fieldset = ({
+  children,
+  legend,
+  legendId,
+  section,
+  disabled,
+  invalid,
+  ...restProps
+}: PropsWithChildren<FieldsetProps>) => {
+  return (
+    <UtrechtFieldSet
+      className={clsx({
+        'utrecht-form-fieldset--section': section,
+      })}
+      {...restProps}
+    >
+      {legend && (
+        <FieldsetLegend
+          id={legendId}
+          className={clsx({
+            'utrecht-form-fieldset__legend--disabled': disabled,
+            'utrecht-form-fieldset__legend--invalid': invalid,
+          })}
+        >
+          {legend}
+        </FieldsetLegend>
+      )}
+      {children}
+    </UtrechtFieldSet>
+  );
+};
