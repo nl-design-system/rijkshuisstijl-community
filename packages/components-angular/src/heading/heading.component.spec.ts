88f4347a71d2f2af18f3e9121600b263d0913d78
Sjimmie <sbleeker@quintor.local>
2025-04-01 13:06:43 +0200

    Jest tests

--- packages/components-angular/src/heading/heading.component.spec.ts
+++ packages/components-angular/src/heading/heading.component.spec.ts
@@ -1,18 +1,6 @@
-import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { HeadingComponent, HeadingLevels } from './heading.component';
-
-// This is a test component used to test how the HeadingComponent handles child elements
-@Component({
-  template: `<community-heading [headingLevel]="headingLevel">{{ text }}</community-heading>`,
-  imports: [HeadingComponent],
-})
-class TestHostComponent {
-  @Input() headingLevel!: HeadingLevels;
-  @Input() appearanceLevel?: HeadingLevels;
-  @Input() text?: string;
-}
+import { HeadingComponent, HeadingLevel } from './heading.component';
 
 describe('HeadingComponent', () => {
   let component: HeadingComponent;
@@ -20,7 +8,7 @@ describe('HeadingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeadingComponent, TestHostComponent],
+      imports: [HeadingComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeadingComponent);
@@ -33,7 +21,7 @@ describe('HeadingComponent', () => {
   });
 
   it('should use the correct heading level', () => {
-    for (let i of [1, 2, 3, 4, 5, 6] as HeadingLevels[]) {
+    for (let i of [1, 2, 3, 4, 5] as HeadingLevel[]) {
       component.headingLevel = i;
       fixture.detectChanges();
       const element = fixture.nativeElement.querySelector(`h${i}`);
@@ -42,7 +30,7 @@ describe('HeadingComponent', () => {
   });
 
   it('should apply the css class according to appearance level', () => {
-    component.headingLevel = 6;
+    component.headingLevel = 5;
     component.appearanceLevel = 2;
     fixture.detectChanges();
     const element = fixture.debugElement.query(By.css('.nl-heading--level-2'));
@@ -56,20 +44,11 @@ describe('HeadingComponent', () => {
     expect(element).toBeTruthy();
   });
 
-  it('should render child text', () => {
-    // Setup host component
-    const hostFixture = TestBed.createComponent(TestHostComponent);
-    const hostComponent = hostFixture.componentInstance;
-    hostFixture.detectChanges();
-    expect(hostComponent).toBeTruthy();
-
-    const text = 'Hello World';
-    const headingLevel = 5;
-    hostComponent.headingLevel = headingLevel;
-    hostComponent.text = text;
-    hostFixture.detectChanges();
-
-    const element = hostFixture.nativeElement.querySelector(`h${headingLevel}`);
-    expect(element.textContent).toBe(text);
+  it('should render rich text content', () => {
+    component.headingLevel = 1;
+    fixture.detectChanges();
+    const headingDebugElement = fixture.debugElement.query(By.css('.nl-heading--level-1'));
+    headingDebugElement.nativeElement.innerHTML = '<strong>Breaking</strong> news';
+    expect(fixture.nativeElement).toContainHTML('strong');
   });
 });
