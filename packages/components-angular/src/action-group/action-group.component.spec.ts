a507be567192d015872809493bd8effbaaa77a9f
aakminasi <arefcoding@gmail.com>
2025-04-16 14:14:31 +0200

    added action group component

---
+++ packages/components-angular/src/action-group/action-group.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ActionGroupComponent } from './action-group.component';
+
+describe('ActionGroupComponent', () => {
+  let component: ActionGroupComponent;
+  let fixture: ComponentFixture<ActionGroupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ActionGroupComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionGroupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have "utrecht-button-group--row" class by default', () => {
+    const divEl = fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(divEl.classList).toContain('utrecht-button-group--row');
+    expect(divEl.classList).not.toContain('utrecht-button-group--column');
+  });
+
+  it('should apply "column" direction class when input is column', () => {
+    component.direction = 'column';
+    fixture.detectChanges();
+
+    const divEl = fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(divEl.classList).toContain('utrecht-button-group--column');
+    expect(divEl.classList).not.toContain('utrecht-button-group--row');
+  });
+
+  it('should apply "row" direction class when input is row', () => {
+    component.direction = 'row';
+    fixture.detectChanges();
+
+    const divEl = fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(divEl.classList).toContain('utrecht-button-group--row');
+    expect(divEl.classList).not.toContain('utrecht-button-group--column');
+  });
+});
---
+++ packages/storybook-angular/src/stories/action-group.stories.ts
@@ -0,0 +1,51 @@
+import { ActionGroupComponent } from '@rijkshuisstijl-community/components-angular';
+import { ButtonComponent } from '@rijkshuisstijl-community/components-angular';
+import { type Meta, type StoryObj } from '@storybook/angular';
+
+const meta: Meta<ActionGroupComponent> = {
+  title: 'Rijkshuisstijl-angular/Action Group',
+  id: 'rhc-angular-action-group',
+  component: ActionGroupComponent,
+  argTypes: {
+    direction: {
+      description: 'Layout of the action group',
+      control: 'select',
+      options: ['column', 'row'],
+    },
+  },
+  args: {
+    direction: 'column',
+  },
+  render: ({ direction }) => ({
+    template: `<rhc-action-group direction="${direction}"> 
+        <rhc-button appearance='primary-action'>
+            Save and continue
+        </rhc-button>
+        <rhc-button appearance='secondary-action'>
+            Back
+        </rhc-button>
+    </rhc-action-group>`,
+    props: {
+      direction,
+    },
+    moduleMetadata: {
+      imports: [ButtonComponent],
+    },
+  }),
+};
+
+export default meta;
+
+export const Default: StoryObj<ActionGroupComponent> = {
+  args: {
+    direction: 'row',
+  },
+  name: 'Default',
+};
+
+export const Column: StoryObj<ActionGroupComponent> = {
+  args: {
+    direction: 'column',
+  },
+  name: 'Column',
+};
