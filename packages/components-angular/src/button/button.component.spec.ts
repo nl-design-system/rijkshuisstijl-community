480ad7c8acfc2a17d215cea24687cc2ce31aa166
Jan Abe de Waal <janabedewaal@protonmail.com>
2025-04-28 14:50:52 +0200

    Refactor button so it can use <button> native attributes

--- packages/components-angular/src/button/button.component.spec.ts
+++ packages/components-angular/src/button/button.component.spec.ts
@@ -1,16 +1,27 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './button.component';
 
+@Component({
+  template: ` <button rhc-button [appearance]="appearance" [disabled]="disabled">Label</button> `,
+  imports: [ButtonComponent],
+})
+class StandAloneButtonComponent {
+  @Input() appearance?: '' | 'primary-action' | 'secondary-action' | 'subtle' = '';
+  @Input() disabled?: boolean = false;
+}
+
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+  let component: StandAloneButtonComponent;
+  let fixture: ComponentFixture<StandAloneButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent],
+      imports: [StandAloneButtonComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(StandAloneButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -37,5 +48,94 @@ describe('ButtonComponent', () => {
     fixture.detectChanges();
     const button = fixture.nativeElement.querySelector('button');
     expect(button.disabled).toBe(true);
+  });
+});
+
+@Component({
+  template: `
+    <form (ngSubmit)="submitFunction()">
+      <button rhc-button [disabled]="disabled">Submit</button>
+    </form>
+  `,
+  imports: [ButtonComponent, FormsModule],
+})
+class SubmitButtonComponentInForm {
+  @Input() disabled?: boolean = false;
+  submitFunction = jest.fn();
+}
+
+describe('SubmitButtonComponentInForm', () => {
+  let component: SubmitButtonComponentInForm;
+  let fixture: ComponentFixture<SubmitButtonComponentInForm>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SubmitButtonComponentInForm],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SubmitButtonComponentInForm);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should submit the form', () => {
+    const submitButton = fixture.nativeElement.querySelector('form > button');
+    expect(submitButton).toHaveTextContent('Submit');
+    submitButton.click();
+    expect(component.submitFunction).toHaveBeenCalled();
+  });
+
+  it('should not submit the form if the button is disabled', () => {
+    component.disabled = true;
+    fixture.detectChanges();
+    const submitButton = fixture.nativeElement.querySelector('form > button');
+
+    expect(submitButton).toBeDisabled();
+    expect(submitButton).toHaveTextContent('Submit');
+
+    submitButton.click();
+    expect(component.submitFunction).not.toHaveBeenCalled();
+  });
+});
+
+@Component({
+  template: `
+    <form (ngSubmit)="submitFunction()">
+      <button rhc-button type="button">Don't submit</button>
+    </form>
+  `,
+  imports: [ButtonComponent, FormsModule],
+})
+class RegularButtonComponentInForm {
+  submitFunction = jest.fn();
+}
+
+describe('RegularButtonComponentInForm', () => {
+  let component: RegularButtonComponentInForm;
+  let fixture: ComponentFixture<RegularButtonComponentInForm>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegularButtonComponentInForm],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RegularButtonComponentInForm);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should not submit the form', () => {
+    const button = fixture.nativeElement.querySelector('form > button');
+    expect(button).toHaveTextContent("Don't submit");
+    button.click();
+    expect(component.submitFunction).not.toHaveBeenCalled();
   });
 });
