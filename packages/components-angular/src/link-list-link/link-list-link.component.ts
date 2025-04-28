63e78e2eae52cc7603ffaa92548ec382cf859e19
Jan Abe de Waal <janabedewaal@protonmail.com>
2025-04-28 10:55:36 +0200

    Refactor code

--- packages/components-angular/src/link-list-link/link-list-link.component.ts
+++ packages/components-angular/src/link-list-link/link-list-link.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 import { LinkComponent } from '../link/link.component';
 
 @Component({
-  selector: 'rhc-link-list-link',
+  selector: 'li[rhc-link-list-link]',
   imports: [LinkComponent],
   templateUrl: './link-list-link.component.html',
   providers: [],
+  host: {
+    '[class.utrecht-link-list__item]': 'true',
+  },
 })
 export class LinkListLinkComponent {
   @Input({ required: true }) href!: string;
