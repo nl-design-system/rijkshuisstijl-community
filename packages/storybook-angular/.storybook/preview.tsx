5116399f16b9aa52b9ccb14b8dc8a59dd66b4383
Sjimmie <sbleeker@quintor.local>
2025-05-02 13:57:32 +0200

    Preview.tsx

--- packages/storybook-angular/.storybook/preview.tsx
+++ packages/storybook-angular/.storybook/preview.tsx
@@ -1,11 +1,18 @@
 import { componentWrapperDecorator, type Preview } from '@storybook/angular';
+import { Controls, Description, Primary } from '@storybook/blocks';
 
 const preview: Preview = {
   parameters: {
     docs: {
-      page: () => (
-       
-      ),
+      page: () => {
+        return (
+          <>
+            <Description />
+            <Primary />
+            <Controls />
+          </>
+        );
+      },
       source: {
         excludeDecorators: true,
       },
@@ -21,4 +28,3 @@ const preview: Preview = {
 };
 
 export default preview;
-
