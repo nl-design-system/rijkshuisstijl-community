2937d46db1d4e44ceef583df4e13a258e44e1a1a
Jan Abe de Waal <janabedewaal@protonmail.com>
2025-05-09 09:23:01 +0200

    Add FormFieldTextInput-component in Angular

---
+++ packages/storybook-angular/src/stories/text-input.stories.ts
@@ -0,0 +1,102 @@
+import { TextInputComponent } from '@rijkshuisstijl-community/components-angular';
+import { type Meta, type StoryObj } from '@storybook/angular';
+
+type StoryType = TextInputComponent & {
+  type: string;
+};
+
+const meta: Meta<StoryType> = {
+  title: 'Rijkshuisstijl-angular/Text input',
+  id: 'rhc-text-input',
+  component: TextInputComponent,
+  argTypes: {
+    invalid: {
+      control: 'boolean',
+    },
+    readonly: {
+      control: 'boolean',
+    },
+    required: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    type: {
+      control: 'select',
+      options: ['email', 'number', 'password', 'search', 'tel', 'text', 'url', 'date'],
+    },
+  },
+  args: {
+    invalid: false,
+    required: false,
+    disabled: false,
+    readonly: false,
+    type: 'text',
+  },
+  parameters: {},
+  render: ({ disabled, required, readonly, invalid, type }) => ({
+    template: `
+    <input rhc-text-input
+      [invalid]="invalid"
+      [disabled]="disabled"
+      [readonly]="readonly"
+      [required]="required"
+      [type]="type"
+    />
+    `,
+    props: {
+      invalid,
+      readonly,
+      required,
+      disabled,
+      type,
+    },
+  }),
+};
+
+export default meta;
+
+export const Default: StoryObj<StoryType> = {
+  args: {
+    disabled: false,
+  },
+  name: 'Default',
+};
+
+export const Required: StoryObj<StoryType> = {
+  args: {
+    required: true,
+  },
+};
+
+export const Disabled: StoryObj<StoryType> = {
+  args: {
+    disabled: true,
+  },
+  render: ({ disabled, required, readonly, invalid, type }) => ({
+    template: `
+    <input rhc-text-input
+      value="Hello, World!"
+      [invalid]="invalid"
+      [disabled]="disabled"
+      [readonly]="readonly"
+      [required]="required"
+      [type]="type"
+    />
+    `,
+    props: {
+      invalid,
+      readonly,
+      required,
+      disabled,
+      type,
+    },
+  }),
+};
+
+export const Invalid: StoryObj<StoryType> = {
+  args: {
+    invalid: true,
+  },
+};
