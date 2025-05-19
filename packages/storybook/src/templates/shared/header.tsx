e12a770faf30ef1c2dee4ee082d7feb1fa0f21d0
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-05-19 17:23:07 +0200

    docs: use correct content in templates shared header

--- packages/storybook/src/templates/shared/header.tsx
+++ packages/storybook/src/templates/shared/header.tsx
@@ -1,4 +1,5 @@
-import { Icon, Logo, NavBar, PageHeader, SkipLink } from '@rijkshuisstijl-community/components-react';
+import { Icon, Link, Logo, NavBar, PageHeader, SkipLink } from '@rijkshuisstijl-community/components-react';
+import './index.css';
 
 export default function SharedHeader() {
   return (
@@ -7,18 +8,29 @@ export default function SharedHeader() {
         Ga naar hoofdinhoud
       </SkipLink>
       <PageHeader>
-        <Logo organisation="Rijkshuisstijl Community">
-          <Icon className={'dutch-map'} icon={'nederland-map'} />
+        <Logo organisation="Rijkshuisstijl Community" subtitle="Onofficieel Rijkshuisstijl Design System">
+          <Link className="rhc-logo__link" href="/">
+            <Icon className={'dutch-map'} icon={'nederland-map'} />
+          </Link>
         </Logo>
         <NavBar
+          endItems={[
+            {
+              id: 'end1',
+              href: 'https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?m=auto',
+              label: 'Figma',
+            },
+            { id: 'end2', href: 'https://github.com/nl-design-system/rijkshuisstijl-community', label: 'GitHub' },
+            { id: 'end3', href: 'https://rijkshuisstijl-community.vercel.app/', label: 'Storybook' },
+          ]}
           items={[
-            { id: '', href: '/', label: 'Homepage' },
-            { id: '1', href: '/rich-text', label: 'Rich Text' },
-            { id: '2', href: '/form', label: 'Form' },
-            { id: '3', href: '/details', label: 'Details' },
-            { id: '4', href: '/collage', label: 'Collage' },
-            { id: '5', href: '/mijn-omgeving', label: 'Mijn Omgeving' },
-            { id: '6', href: '/', label: 'Componenten' },
+            { id: 'start1', href: '/', label: 'Homepage' },
+            { id: 'start2', href: '/rich-text', label: 'Rich Text' },
+            { id: 'start3', href: '/form', label: 'Form' },
+            { id: 'start4', href: '/details', label: 'Details' },
+            { id: 'start5', href: '/collage', label: 'Collage' },
+            { id: 'start6', href: '/mijn-omgeving', label: 'Mijn Omgeving' },
+            { id: 'start7', href: '/', label: 'Componenten' },
           ]}
         ></NavBar>
       </PageHeader>
