1c23275202fc06c77c3d0e354bd4175b846d5525
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-05-21 21:39:42 +0200

    feat: use more descriptive title and description for metadata on component overview

--- apps/rhc-templates/src/app/componenten/page.tsx
+++ apps/rhc-templates/src/app/componenten/page.tsx
@@ -2,8 +2,9 @@ import PageContent from '@rijkshuisstijl-community/storybook/src/templates/compo
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Componenten',
-  description: 'Componenten template voor de Rijkshuisstijl Community',
+  title: 'Componenten overzicht',
+  description:
+    'Filter op zoekterm en framework om componenten uit de Rijkshuisstijl Community te vinden. Bekijk direct de component in Storybook door de link in het zoekresultaat te volgen.',
 };
 
 export default function Page() {
