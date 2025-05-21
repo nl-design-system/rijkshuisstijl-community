7ba6a2937b16f24f43d84f06d740f9c05ec73dad
Marwa <112861555+Marwaxhello@users.noreply.github.com>
2025-05-21 23:12:48 +0200

    Feat/componenten template zoek en filter functies (#1615)
    ### Componenten Template
    
    Deze template bevat een simpele zoekfunctie waarmee je op elk woord kunt
    zoeken om de bijbehorende card te vinden. De zoekfunctie filtert alleen
    kaarten waarin het opgegeven woord voorkomt (10 cards tot nu). Daarnaast
    is er aan de linkerkant een filterfunctie met checkboxes, waarmee je
    kaarten kunt filteren op basis van frameworks door een selectievakje aan
    te vinken.
    
    [Preview component
    template](https://rijkshuisstijl-community-templates-4siyah5ji-nl-design-system.vercel.app/componenten)
    
    [Issue #1595
    ](https://github.com/nl-design-system/rijkshuisstijl-community/issues/1595)
    
    ---------
    
    Co-authored-by: Marwa <marwahello@frameless.io>
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

---
+++ packages/storybook/src/templates/componenten.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Page from './componenten';
+import './globals.css';
+
+const meta: Meta = {
+  title: 'Templates/Componenten',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+    status: {
+      type: 'UNSTABLE',
+    },
+    isPage: true,
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Page>;
+
+export const Default: Story = {};
