836faec3976b347f531e2432f0e7086f3924601f
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-05-22 00:38:25 +0200

    fix: oplossen wcag probleem 5: focus probleem bij terug naar boven

--- packages/storybook/src/templates/shared/main-page-content.tsx
+++ packages/storybook/src/templates/shared/main-page-content.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 
 export default function SharedMainPageContent({ children }: PropsWithChildren<{}>) {
   return (
-    <main className="rhc-templates-main-content" id="main">
+    <main className="rhc-templates-main-content" id="main" tabIndex={-1}>
       <PageContent className="rhc-templates-page-content">{children}</PageContent>
     </main>
   );
