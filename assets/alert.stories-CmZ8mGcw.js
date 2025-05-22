a241c5fcaed15b60dc0011a4455f667c91565aab
Robbert <mail@robbertbroersma.nl>
2025-05-22 12:33:42 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@e6d20a425fb722637db47bc7630b572213ab09da 🚀

---
+++ assets/alert.stories-CmZ8mGcw.js
@@ -0,0 +1,25 @@
+import{A as h}from"./index.esm-u2QJjVQj.js";import{r as f}from"./alert-CWufKqrZ.js";import{m as x}from"./merge-markdown-566X0PcZ.js";import"./index-DeTAkU8q.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./jsx-runtime-D_zvdyIk.js";/* @license CC0-1.0 */const A={title:"Rijkshuisstijl/Alert",id:"rhc-alert",component:h,argTypes:{type:{description:"Alert type",control:{type:"select"},options:["info","error","warning","ok"],table:{category:"Property"}},heading:{description:"Alert heading - used in default webcomponent slot",type:{name:"string"},table:{category:"Demo"}},headingLevel:{description:"Alert heading level",control:{type:"select"},options:[1,2,3,4,5],table:{category:"Demo"}},textContent:{description:"Alert content - used in default webcomponent slot",type:{name:"string"},table:{category:"Demo"}}},args:{type:"info",heading:"Heading",headingLevel:3,textContent:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"},tags:["autodocs"],parameters:{status:{type:"STABLE"},docs:{description:{component:x([f])}},figma:"https://www.figma.com/design/txFX5MGRf4O904dtIFcGTF/NLDS---Rijkshuisstijl---Bibliotheek?node-id=1195-4201&t=n1djYpmvDCKmAEUi-0",github:"https://github.com/nl-design-system/rijkshuisstijl-community/blob/main/packages/components-react/src/Alert.tsx",nldesignsystem:"https://www.nldesignsystem.nl/alert/",componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met HTML aanpassingen en styling van de Rijkshuisstijl Community."}},e={args:{type:"info",heading:"Heading",textContent:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"}},t={args:{type:"error",heading:"Heading",textContent:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"}},o={args:{type:"ok",heading:"Heading",textContent:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"}},i={args:{type:"warning",heading:"Heading",textContent:"Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *"}};var n,s,r;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    type: 'info',
+    heading: 'Heading',
+    textContent: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *'
+  }
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};var a,d,m;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    type: 'error',
+    heading: 'Heading',
+    textContent: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *'
+  }
+}`,...(m=(d=t.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var c,g,p;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    type: 'ok',
+    heading: 'Heading',
+    textContent: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *'
+  }
+}`,...(p=(g=o.parameters)==null?void 0:g.docs)==null?void 0:p.source}}};var l,u,y;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    type: 'warning',
+    heading: 'Heading',
+    textContent: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *'
+  }
+}`,...(y=(u=i.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};const H=["Informative","Negative","Positive","Warning"];export{e as Informative,t as Negative,o as Positive,i as Warning,H as __namedExportsOrder,A as default};
--- assets/index-BoFq5TE9.js
+++
@@ -1,1 +0,0 @@
-import"./preview-C7EL4hQs.js";import"./DocsRenderer-CFRXHY34-NRncq3eS.js";import"./index-BndYlYD1.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
