15f63e2061e36b351284a80e7349a70dbdacad0e
sjimbonator <sjimmiebleeker2000@gmail.com>
2025-06-02 09:43:49 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@14334a4ec3e2002bffd2f0297bdf94c8f7ea29f5 🚀

--- assets/index-DccfMDni.js
+++
@@ -1,1 +0,0 @@
-import"./preview-D1FOQs2J.js";import"./DocsRenderer-CFRXHY34-B3xX2cCM.js";import"./index-DpfV5vmY.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
---
+++ assets/notice-DJwJ6LuX.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as o}from"./index-BysLjLkY.js";import"./index-CIrkAX0p.js";import{M as i,e as a}from"./index-DO2rL2E6.js";import"./index-DeTAkU8q.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./preview-7TgNL2bP.js";import"./iframe-C4VqQB5J.js";import"./DocsRenderer-CFRXHY34-CtgPs2M8.js";import"./react-18-CnEToxBx.js";import"./index-CTPIcns_.js";import"./index-B-2rlRvl.js";import"./index-CXQShRbs.js";const s=`# Auteursrecht {auteur}
+
+Copyright (c) {jaar} {auteur}
+
+## Logo en huisstijl
+
+Op het huisstijl en logo zijn auteursrechten van toepassing. Het gebruik van logo en huisstijl is alleen toegestaan voor gebruik door {auteur}.
+
+Wanneer je een bewerking van de software wilt gebruiken voor andere doeleinden, mag je niet het logo van {auteur} gebruiken en je ontwerpt een eigen huisstijl.
+
+## Lettertype
+
+Lettertypes die worden gebruikt voor de huisstijl zijn niet allemaal gratis en open source. Let op dat bij gebruik van die bijgeleverde lettertypes je een (betaalde) licentie regelt. Pas anders de configuratie aan om minder of andere lettertypes te gebruiken.
+
+## Toestemming
+
+Wanneer je het logo of de huisstijl wilt gebruiken kun je voor toestemming contact opnemen met {contactpersoon}.
+`;function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Example/Toestemming voor gebruik"}),`
+`,e.jsx(a,{children:s})]})}function x(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r()}export{x as default};
