92e33034810e5ffe8cf5a0bb3c56635d7c0f50a1
Sjimmie <sbleeker@quintor.local>
2025-06-03 15:59:50 +0200

    Added border and flex tokens

--- packages/components-angular/src/data-summary/data-summary.component.ts
+++ packages/components-angular/src/data-summary/data-summary.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   selector: 'rhc-data-summary',
   imports: [],
   templateUrl: './data-summary.component.html',
+  styleUrl: './data-summary.component.css',
 })
 export class DataSummaryComponent {}
