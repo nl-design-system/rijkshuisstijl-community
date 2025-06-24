7b5cf085c1a8775528b88c3988b3445e650a0665
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-06-24 13:59:51 +0200

    feat: add DataBadgeButton to Storybook + available as a component

---
+++ packages/components-react/src/DataBadgeButton.tsx
@@ -0,0 +1,51 @@
+import { DataBadge, DataBadgeProps } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+import { PropsWithChildren, ReactNode, Ref } from 'react';
+
+export interface DataBadgeButtonProps extends DataBadgeProps {
+  pressed?: boolean;
+  helperText?: ReactNode;
+  showHelperText?: boolean;
+  helperId?: string;
+  ref?: Ref<HTMLElement>;
+}
+
+export const DataBadgeButton = ({
+  ref,
+  children,
+  className,
+  pressed = false,
+  helperText,
+  showHelperText = false,
+  helperId,
+  ...restProps
+}: PropsWithChildren<DataBadgeButtonProps>) => {
+  const actualHelperId = showHelperText && helperId ? helperId : undefined;
+
+  return (
+    <DataBadge
+      aria-describedby={actualHelperId}
+      aria-pressed={pressed}
+      ref={ref}
+      role="button"
+      tabIndex={0}
+      className={clsx(
+        'rhc-databadge-button',
+        {
+          'rhc-databadge-button--pressed': pressed,
+        },
+        className,
+      )}
+      {...restProps}
+    >
+      {children}
+      {showHelperText && helperText && actualHelperId && (
+        <span className="rhc-databadge-button__sr-only" id={actualHelperId}>
+          {helperText}
+        </span>
+      )}
+    </DataBadge>
+  );
+};
+
+DataBadgeButton.displayName = 'DataBadgeButton';
