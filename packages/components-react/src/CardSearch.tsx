bf153265030f5c92030c44a14f9d308f7ba7a47b
Marwa <marwahello@frameless.io>
2025-06-24 06:42:06 +0200

    refactor: rebuild card improve accessibly

--- packages/components-react/src/CardSearch.tsx
+++ packages/components-react/src/CardSearch.tsx
@@ -1,69 +1,126 @@
+import { Link as DefaultLink } from '@utrecht/link-react';
 import clsx from 'clsx';
-import { PropsWithChildren, ReactNode, Ref } from 'react';
-import { Link } from './Link';
+import React from 'react';
+import {
+  ElementType,
+  forwardRef,
+  type HTMLAttributes,
+  JSX,
+  type KeyboardEvent,
+  type ReactNode,
+  useId,
+  useRef,
+} from 'react';
 
-interface CardSearchProps {
-  href?: string;
-  title?: string;
-  target?: string;
-  className?: string;
+export interface CardSearchProps extends HTMLAttributes<HTMLDivElement> {
   heading: ReactNode;
+  headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
   subheading?: ReactNode;
   description?: ReactNode;
   metadata?: string;
-  ref?: Ref<HTMLDivElement>;
+  image?: ReactNode;
+  href?: string;
+  target?: string;
   linkLabel?: string;
   button?: ReactNode;
+  LinkComponent?: ElementType;
 }
 
-export const CardSearch = ({
-  ref,
-  heading,
-  subheading,
-  className,
-  description,
-  target,
-  button,
-  href,
-  title,
-  metadata,
-  linkLabel,
+const HTMLHeading = ({
+  level = 2,
   children,
-  ...restProps
-}: PropsWithChildren<CardSearchProps>) => {
-  return (
-    <div className={clsx('rhc-card-search', 'rhc-card-search--default', className)} ref={ref} {...restProps}>
-      <div className="rhc-card-search__content">
-        <div className="rhc-card-search__link">
-          <Link aria-label={title} href={href} title={title}>
-            <div className="rhc-card-search__heading">{heading}</div>
-          </Link>
-          <Link aria-label={title} href={href} title={title}>
-            <div className="rhc-card-search__subheading">{subheading}</div>
-          </Link>
-        </div>
-        <div className="rhc-card-search__description">{description}</div>
-        <div className="rhc-card-search__metadata">{metadata}</div>
-      </div>
-      {children}
-      {linkLabel && (
-        <div className="rhc-card-search__footer">
-          {linkLabel && href && (
-            <div className="rhc-card-search__link" data-testid="rhc-card-search__link">
-              <Link href={href} target={target} title={title}>
-                {linkLabel}
-              </Link>
-            </div>
-          )}
-          {button && (
-            <div className="rhc-card-search__button" data-testid="rhc-card-search__button">
-              {button}
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
+  ...rest
+}: { level: number; children: ReactNode } & HTMLAttributes<HTMLHeadingElement>) => {
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  return React.createElement(Tag, rest, children);
 };
 
-CardSearch.displayName = 'DefaultCardSearch';
+export const CardSearch = forwardRef<HTMLDivElement, CardSearchProps>(
+  (
+    {
+      heading,
+      headingLevel = 2,
+      subheading,
+      description,
+      metadata,
+      image,
+      href,
+      target,
+      linkLabel,
+      button,
+      className,
+      LinkComponent = DefaultLink,
+      ...rest
+    },
+    ref,
+  ) => {
+    const cardLinkRef = useRef<HTMLAnchorElement>(null);
+    const headingId = useId();
+
+    const onClick = () => {
+      if (href && cardLinkRef.current) {
+        cardLinkRef.current.click();
+      }
+    };
+
+    const onKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+      if ((event.key === 'Enter' || event.key === ' ') && href && cardLinkRef.current) {
+        event.preventDefault();
+        cardLinkRef.current.click();
+      }
+    };
+
+    return (
+      <div
+        aria-labelledby={href ? headingId : undefined}
+        ref={ref}
+        role={href ? 'link' : undefined}
+        tabIndex={href ? 0 : undefined}
+        className={clsx('rhc-card-search', 'rhc-card-search--default', className, {
+          'rhc-card-search--clickable': href,
+        })}
+        onClick={href ? onClick : undefined}
+        onKeyDown={href ? onKeyDown : undefined}
+        {...rest}
+      >
+        {image && <div className="rhc-card-search__image">{image}</div>}
+
+        <div className="rhc-card-search__content">
+          <HTMLHeading className="rhc-card-search__heading" id={headingId} level={headingLevel}>
+            {href ? (
+              <LinkComponent className="rhc-card-search__link" href={href} ref={cardLinkRef} target={target}>
+                {heading}
+              </LinkComponent>
+            ) : (
+              heading
+            )}
+          </HTMLHeading>
+
+          {subheading && <div className="rhc-card-search__subheading">{subheading}</div>}
+          {description && <div className="rhc-card-search__description">{description}</div>}
+          {metadata && <div className="rhc-card-search__metadata">{metadata}</div>}
+        </div>
+
+        {(linkLabel || button) && (
+          <div className="rhc-card-search__footer">
+            {linkLabel && href && (
+              <div className="rhc-card-search__link" data-testid="rhc-card-search__link">
+                <LinkComponent href={href} target={target}>
+                  {linkLabel}
+                </LinkComponent>
+              </div>
+            )}
+            {button && (
+              <div className="rhc-card-search__button" data-testid="rhc-card-search__button">
+                {button}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+
+CardSearch.displayName = 'CardSearch';
