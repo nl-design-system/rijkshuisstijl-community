f315041029e1dd903aac14868a4e090179973fee
Marwa <marwahello@frameless.io>
2025-06-25 13:15:08 +0200

    refactor: remove card clickable refactor to not clickable

--- packages/components-react/src/CardSearch.tsx
+++ packages/components-react/src/CardSearch.tsx
@@ -1,29 +1,19 @@
-import { Link as DefaultLink } from '@utrecht/link-react';
 import clsx from 'clsx';
 import React from 'react';
-import {
-  ElementType,
-  forwardRef,
-  type HTMLAttributes,
-  JSX,
-  type KeyboardEvent,
-  type ReactNode,
-  useId,
-  useRef,
-} from 'react';
+import { forwardRef, type HTMLAttributes, JSX, type ReactNode, useId } from 'react';
+import { Link } from './Link';
 
 export interface CardSearchProps extends HTMLAttributes<HTMLDivElement> {
   heading: ReactNode;
   headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
   subheading?: ReactNode;
   description?: ReactNode;
+  children?: ReactNode;
   metadata?: string;
   image?: ReactNode;
   href?: string;
   target?: string;
   linkLabel?: string;
-  button?: ReactNode;
-  LinkComponent?: ElementType;
 }
 
 const HTMLHeading = ({
@@ -48,50 +38,24 @@ export const CardSearch = forwardRef<HTMLDivElement, CardSearchProps>(
       href,
       target,
       linkLabel,
-      button,
       className,
-      LinkComponent = DefaultLink,
+      children,
       ...rest
     },
     ref,
   ) => {
-    const cardLinkRef = useRef<HTMLAnchorElement>(null);
     const headingId = useId();
 
-    const onClick = () => {
-      if (href && cardLinkRef.current) {
-        cardLinkRef.current.click();
-      }
-    };
-
-    const onKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-      if ((event.key === 'Enter' || event.key === ' ') && href && cardLinkRef.current) {
-        event.preventDefault();
-        cardLinkRef.current.click();
-      }
-    };
-
     return (
-      <div
-        aria-labelledby={href ? headingId : undefined}
-        ref={ref}
-        role={href ? 'link' : undefined}
-        tabIndex={href ? 0 : undefined}
-        className={clsx('rhc-card-search', 'rhc-card-search--default', className, {
-          'rhc-card-search--clickable': href,
-        })}
-        onClick={href ? onClick : undefined}
-        onKeyDown={href ? onKeyDown : undefined}
-        {...rest}
-      >
+      <div className={clsx('rhc-card-search', 'rhc-card-search--default', className)} ref={ref} {...rest}>
         {image && <div className="rhc-card-search__image">{image}</div>}
 
         <div className="rhc-card-search__content">
           <HTMLHeading className="rhc-card-search__heading" id={headingId} level={headingLevel}>
             {href ? (
-              <LinkComponent className="rhc-card-search__link" href={href} ref={cardLinkRef} target={target}>
+              <Link className="rhc-card-search__link" href={href} target={target}>
                 {heading}
-              </LinkComponent>
+              </Link>
             ) : (
               heading
             )}
@@ -100,22 +64,16 @@ export const CardSearch = forwardRef<HTMLDivElement, CardSearchProps>(
           {subheading && <div className="rhc-card-search__subheading">{subheading}</div>}
           {description && <div className="rhc-card-search__description">{description}</div>}
           {metadata && <div className="rhc-card-search__metadata">{metadata}</div>}
+          {children}
         </div>
 
-        {(linkLabel || button) && (
+        {linkLabel && (
           <div className="rhc-card-search__footer">
-            {linkLabel && href && (
-              <div className="rhc-card-search__link" data-testid="rhc-card-search__link">
-                <LinkComponent href={href} target={target}>
-                  {linkLabel}
-                </LinkComponent>
-              </div>
-            )}
-            {button && (
-              <div className="rhc-card-search__button" data-testid="rhc-card-search__button">
-                {button}
-              </div>
-            )}
+            <div className="rhc-card-search__link" data-testid="rhc-card-search__link">
+              <Link href={href} target={target}>
+                {linkLabel}
+              </Link>
+            </div>
           </div>
         )}
       </div>
