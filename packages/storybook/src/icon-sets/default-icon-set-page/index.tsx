5996b0efb678f6bfc6a21df550ca69e55b376fb5
Casper Ravenhorst <casper@sinedub.io>
2025-08-11 16:57:28 +0200

    feat: surround every story with utrecht-document class (#1863)
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

--- packages/storybook/src/icon-sets/default-icon-set-page/index.tsx
+++ packages/storybook/src/icon-sets/default-icon-set-page/index.tsx
@@ -1,20 +1,17 @@
 import { Icon, defaultIconSet as iconSet } from '@rijkshuisstijl-community/components-react';
-import { PageBody } from '@utrecht/page-body-react';
 import '../index.css';
 
 export default function IconSet() {
   const iconIDs = Object.keys(iconSet).sort((a, b) => a.localeCompare(b));
 
   return (
-    <PageBody>
-      <div className="rhc-icon-set">
-        {iconIDs.map((iconID) => (
-          <div className="rhc-icon-set-item" key={iconID}>
-            <Icon icon={iconID}></Icon>
-            <span>{iconID}</span>
-          </div>
-        ))}
-      </div>
-    </PageBody>
+    <div className="rhc-icon-set">
+      {iconIDs.map((iconID) => (
+        <div className="rhc-icon-set-item" key={iconID}>
+          <Icon icon={iconID}></Icon>
+          <span>{iconID}</span>
+        </div>
+      ))}
+    </div>
   );
 }
