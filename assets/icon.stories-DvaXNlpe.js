d6a4796babb63a25953e9d3193517a922136dfb5
eslook <42849307+eslook@users.noreply.github.com>
2025-08-18 15:42:28 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@022af85194c498c02913276ff89776656c689924 🚀

---
+++ assets/icon.stories-DvaXNlpe.js
@@ -0,0 +1,13 @@
+import{j as g}from"./jsx-runtime-D_zvdyIk.js";import{o as f,r as u,p as l,q as S}from"./index.esm-q1PdjJ_h.js";import{S as h}from"./index-CxvHwAyN.js";import{m as y}from"./merge-markdown-DQEtknEV.js";import{r as b}from"./icon-Ck1AHgvD.js";import{I}from"./IconBrandX-BY56ZTl2.js";import"./index-BJzegUt9.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./createReactComponent-DpnDSqqq.js";u(l);u(S);h.define();const E={title:"Web Components/Icon",id:"rhc-icon-web",component:"rhc-icon",argTypes:{icon:{name:"Icon",type:{name:"string"},control:"select",options:Object.keys(f())}},parameters:{docs:{description:{component:y([b])}},status:{type:"STABLE"},componentOrigin:"Dit component is overgenomen van de Gemeente Utrecht, met extra functionaliteit voor het gebruiken van de iconenset van de Rijkshuisstijl Community.",github:"https://github.com/nl-design-system/rijkshuisstijl-community/blob/main/packages/web-components/src/components/Icon.tsx"}},e={args:{icon:"home"}},o={args:{children:g.jsx(I,{})}},r={args:{icon:"nl-flag"}};var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    icon: 'home'
+  }
+} as StoryObj<typeof meta>`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var a,c,m;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: <IconBrandX />
+  }
+} as StoryObj<typeof meta>`,...(m=(c=o.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,p,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    icon: 'nl-flag'
+  }
+} as StoryObj<typeof meta>`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const F=["Default","CustomIcon","FromDifferentIconSet"];export{o as CustomIcon,e as Default,r as FromDifferentIconSet,F as __namedExportsOrder,E as default};
--- assets/index-CYZ_qlOq.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CySipOFQ.js";import"./DocsRenderer-CFRXHY34-DjgpuwFr.js";import"./index-sqECh0d7.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
