1a7e00e53292173454e0c07cbce7b7ef7ca62fd9
Casper Ravenhorst <casper@sinedub.io>
2025-08-20 11:38:03 +0200

    fix: remove visibleOnFocus prop and class for SkipLink storybook
    (visibleOnFocus should not be a prop, it should be the default)

--- packages/storybook/src/community/skip-link.stories.tsx
+++ packages/storybook/src/community/skip-link.stories.tsx
@@ -1,28 +1,18 @@
 import { SkipLink, type SkipLinkProps } from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react';
-import clsx from 'clsx';
 import readme from './skip-link.md?raw';
 
-interface SkipLinkStoryProps extends SkipLinkProps {
-  visibility?: string | 'hidden' | 'visible';
-  visibleOnFocus?: boolean;
-}
-
-const SkipLinkStory = ({ visibility, visibleOnFocus, ...restProps }: SkipLinkStoryProps) => (
-  <SkipLink
-    className={clsx({
-      'rhc-skip-link--visible-on-focus': visibleOnFocus,
-      'rhc-skip-link--hidden': visibility === 'hidden',
-      'rhc-skip-link--visible': visibility === 'visible',
-    })}
-    {...restProps}
-  />
+// set fixed height and width so that the automatic hiding works
+const StoryWrapper = (props: SkipLinkProps) => (
+  <div style={{ width: '400px', height: '100px' }}>
+    <SkipLink {...props} />
+  </div>
 );
 
 const meta = {
   title: 'Rijkshuisstijl/Skip link',
   id: 'rijkshuisstijl-skip-link',
-  component: SkipLinkStory,
+  component: SkipLink,
   argTypes: {
     children: {
       description: 'Link text',
@@ -38,20 +28,10 @@ const meta = {
         required: true,
       },
     },
-    visibility: {
-      description: 'Visibilty',
-      options: ['', 'hidden', 'visible'],
-    },
-    visibleOnFocus: {
-      description: 'Visible on focus',
-      control: 'boolean',
-    },
   },
   args: {
     children: '',
     href: '',
-    visibility: '',
-    visibleOnFocus: false,
   },
   parameters: {
     status: {
@@ -64,8 +44,8 @@ const meta = {
     },
     // TODO: add Figma, GitHub and NL DesignSystem links
   },
-  render: SkipLinkStory,
-} satisfies Meta<typeof SkipLinkStory>;
+  render: StoryWrapper,
+} satisfies Meta<typeof SkipLink>;
 
 export default meta;
 
@@ -75,7 +55,6 @@ export const Default: Story = {
   args: {
     href: '#main',
     children: 'Skip to main content',
-    visibility: 'visible',
   },
   parameters: {
     docs: {
@@ -90,29 +69,10 @@ export const Default: Story = {
   },
 };
 
-export const VisibleOnFocus: Story = {
-  args: {
-    href: '#main',
-    children: 'Skip to main content',
-    visibleOnFocus: true,
-  },
-  name: 'Visible on focus',
-  parameters: {
-    chromatic: { disableSnapshot: true },
-    docs: {
-      description: {
-        story:
-          'Styling met de `.rhc-skip-link--visible-on-focus` class naam. Standaard niet zichtbaar, wordt zichtbaar wanneer je met het toetsenbord er naar toe tabt.',
-      },
-    },
-  },
-};
-
 export const RightToLeft: Story = {
   args: {
     href: '#main',
     children: 'تخطي إلى المحتوى الرئيسي',
-    visibility: 'visible',
   },
   name: 'Right-to-left',
   decorators: [
