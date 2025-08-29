6e67718493b40d4c3d4348a21db35e4fb6ae90e0
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-08-29 13:02:06 +0200

    docs: add sort order to angular storybook

--- packages/storybook-angular/.storybook/preview.ts
+++ packages/storybook-angular/.storybook/preview.ts
@@ -16,6 +16,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    options: {
+      storySort: {
+        order: ['Angular Components', ['README', 'CHANGELOG'], 'Angular Templates'],
+      },
+    },
   },
   decorators: [
     withThemeByClassName({
