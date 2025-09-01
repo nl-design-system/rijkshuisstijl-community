708862a5d3bac24899eb0a5079f79badd1312459
Sjimmie <sbleeker@quintor.local>
2025-09-01 11:00:36 +0200

    Reverted to previous api

--- packages/storybook-angular/src/stories/link-list-link.stories.ts
+++ packages/storybook-angular/src/stories/link-list-link.stories.ts
@@ -46,12 +46,10 @@ const meta: Meta<StoryType> = {
     <rhc-link-list>
       <li rhc-link-list-item>
         <a rhc-link-list-link href="${href}">
-          <link-icon>
-            <rhc-icon>
-              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
-            </rhc-icon>
-          </link-icon>
-          <link-text>${label}</link-text>
+          <rhc-icon icon>
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+          </rhc-icon>
+          ${label}
         </a>
       </li>
     </rhc-link-list>
@@ -72,7 +70,7 @@ export const NoIcon: StoryObj<StoryType> = {
     <rhc-link-list>
       <li rhc-link-list-item>
         <a rhc-link-list-link href="${href}">
-          <link-text>${label}</link-text>
+          ${label}
         </a>
       </li>
     </rhc-link-list>
