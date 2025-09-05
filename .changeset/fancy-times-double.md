9dede3b16d9e12590a2dc5c4e26af3cab80583fd
Sjimmie <sjimmiebleeker2000@gmail.com>
2025-09-05 09:53:42 +0200

    Misc storybook fixes (#1911)
    Footer now has relative import for hot reload and added missing icons in
    Link list stories. The chevron icons inside the footer now get placed in
    the proper place in the DOM.
    
    ---------
    
    Co-authored-by: Sjimmie <sbleeker@quintor.local>
    Co-authored-by: Charlotte <42849307+eslook@users.noreply.github.com>

---
+++ .changeset/fancy-times-double.md
@@ -0,0 +1,5 @@
+---
+'@rijkshuisstijl-community/components-angular': patch
+---
+
+The chevron icons inside the footer now get placed in the proper place in the DOM.
--- packages/storybook-angular/src/stories/link-list.stories.ts
+++ packages/storybook-angular/src/stories/link-list.stories.ts
@@ -42,19 +42,19 @@ const meta: Meta<StoryType> = {
       <rhc-link-list>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            ${hasIcons ? "<rhc-icon icon='chevron-right' />" : ''}
+            ${hasIcons ? `<rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>` : ''}
             Learn about <i lang="fr">joi de vivre</i>, an essential foreign phrase!
           </a>
         </li>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            ${hasIcons ? "<rhc-icon icon='chevron-right' />" : ''}
+            ${hasIcons ? `<rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>` : ''}
             Link 2
           </a>
         </li>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            ${hasIcons ? "<rhc-icon icon='chevron-right' />" : ''}
+            ${hasIcons ? `<rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>` : ''}
             Link 3
           </a>
         </li>
@@ -77,19 +77,19 @@ export const ColumnsLayout: StoryObj<LinkListComponent> = {
       <rhc-link-list>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            <rhc-icon icon='chevron-right' />
+            <rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>
             Learn about <i lang="fr">joi de vivre</i>, an essential foreign phrase!
           </a>
         </li>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            <rhc-icon icon='chevron-right' />
+            <rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>
             Link 2
           </a>
         </li>
         <li rhc-link-list-item>
           <a rhc-link-list-link href="#">
-            <rhc-icon icon='chevron-right' />
+            <rhc-icon icon><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="tabler-icon tabler-icon-chevron-right "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></rhc-icon>
             Link 3
           </a>
         </li>
