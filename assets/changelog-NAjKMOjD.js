0b522bafeb10f3d496738bfe5591ce07706b309d
dineshduggal <1058337+dineshduggal@users.noreply.github.com>
2025-09-08 06:41:22 +0000

    Deploying to gh-pages from @ nl-design-system/rijkshuisstijl-community@884d9903bdf64b1b7409e22647799511fbbf7817 🚀

---
+++ assets/changelog-NAjKMOjD.js
@@ -0,0 +1,177 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-CgIrIXX2.js";import"./index-VzEIsP63.js";import{M as i,e as a}from"./index-3zfmylod.js";import"./index-BJzegUt9.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./preview-CE5S-tHI.js";import"./iframe-km_YVzGc.js";import"./DocsRenderer-CFRXHY34-Bt9ZeXQf.js";import"./react-18-WSCnJXIz.js";import"./index-jD2FfvU1.js";import"./index-Bw_1hv9w.js";import"./index-CXQShRbs.js";const c=`# @rijkshuisstijl-community/components-twig
+
+## 2.0.0
+
+### Major Changes
+
+- 8e508f9: Removed the following tokens:
+  Used but unnecessary:
+  --utrecht-alert-heading-line-height
+  --utrecht-alert-message-line-height
+
+  Unused:
+  --utrecht-alert-heading-font-family
+  --utrecht-alert-heading-font-size
+  --utrecht-alert-heading-font-weight
+
+  Reworked the Alert react component to allow users to add content like heading and paragraph in the children instead of duplicating heading props in the alert directly.
+  Added icon overwrite support in case you want a different icon on a specific alert type.
+
+### Patch Changes
+
+- d2c235c: Uitloggen icoon toegevoegd
+- Updated dependencies [8e508f9]
+- Updated dependencies [29bfdd5]
+- Updated dependencies [965c2a4]
+  - @rijkshuisstijl-community/components-css@6.0.0
+
+## 1.3.9
+
+### Patch Changes
+
+- Updated dependencies [35dcda9]
+  - @rijkshuisstijl-community/components-css@5.1.0
+
+## 1.3.8
+
+### Patch Changes
+
+- 3d9999c: Update patch & minor dependencies
+- Updated dependencies [cd3758e]
+- Updated dependencies [bf73b50]
+- Updated dependencies [6b3a5aa]
+- Updated dependencies [39e082e]
+- Updated dependencies [93b9259]
+- Updated dependencies [3d9999c]
+- Updated dependencies [022af85]
+- Updated dependencies [0057047]
+- Updated dependencies [f89c576]
+- Updated dependencies [acca7a7]
+- Updated dependencies [f24efc1]
+  - @rijkshuisstijl-community/components-css@5.0.0
+
+## 1.3.7
+
+### Patch Changes
+
+- Updated dependencies [08d4354]
+- Updated dependencies [923d857]
+  - @rijkshuisstijl-community/components-css@4.0.0
+
+## 1.3.6
+
+### Patch Changes
+
+- 9e83410: Fixed Twig peer dependency on React to run Chromatic snapshots locally.
+- Updated dependencies [9e83410]
+- Updated dependencies [a1502a5]
+  - @rijkshuisstijl-community/components-css@3.1.0
+
+## 1.3.5
+
+### Patch Changes
+
+- d20a098: Voeg \`react\` als peer dependency toe aan \`components-twig\` om Storybook builds succesvol te laten verlopen in Chromatic.
+- Updated dependencies [f4a37da]
+- Updated dependencies [f8f4bab]
+- Updated dependencies [9916255]
+- Updated dependencies [790ead0]
+  - @rijkshuisstijl-community/components-css@3.0.0
+
+## 1.3.4
+
+### Patch Changes
+
+- Updated dependencies [280d76c]
+- Updated dependencies [c7034fd]
+  - @rijkshuisstijl-community/components-css@2.0.0
+
+## 1.3.3
+
+### Patch Changes
+
+- 99dd213: Package Updates
+- Updated dependencies [99dd213]
+- Updated dependencies [093b285]
+- Updated dependencies [893b3e3]
+- Updated dependencies [8395a93]
+  - @rijkshuisstijl-community/components-css@1.2.0
+
+## 1.3.2
+
+### Patch Changes
+
+- b505c9b: Update README to fix broken relative links to files in the repository.
+- b505c9b: Update README on installing and importing styling when applying theme.
+- 9eeb7b7: Dependency updates (week 12)
+- Updated dependencies [292ceb3]
+- Updated dependencies [b505c9b]
+- Updated dependencies [9eeb7b7]
+- Updated dependencies [5cf3cd0]
+  - @rijkshuisstijl-community/components-css@1.1.0
+
+## 1.3.1
+
+### Patch Changes
+
+- Updated dependencies [90f6b1d]
+- Updated dependencies [145d80d]
+  - @rijkshuisstijl-community/components-css@1.0.3
+
+## 1.3.0
+
+### Minor Changes
+
+- 9c4303a: Create image component in Twig
+
+## 1.2.0
+
+### Minor Changes
+
+- f08e050: Added "Alert" component in Twig.
+
+## 1.1.2
+
+### Patch Changes
+
+- 1dc03fb: Wekelijkse package updates voor Rijkshuisstijl-componenten en design tokens (week 10)
+- b9a4945: Added implementation documentation to package README.
+- f398137: - Add Paragraph Twig template
+- Updated dependencies [1dc03fb]
+- Updated dependencies [b9a4945]
+  - @rijkshuisstijl-community/components-css@1.0.2
+
+## 1.1.1
+
+### Patch Changes
+
+- bf359ed: Added nl-heading and nl-heading--level-\\* classes to heading
+- bf359ed: Split attribute logic for svg attributes on icon between component and parent
+  Use drupal attribute to set classes and attributes in icon
+
+## 1.1.0
+
+### Minor Changes
+
+- 253c3a9: Add support for Twig namespaces.
+
+### Patch Changes
+
+- Updated dependencies [723c927]
+  - @rijkshuisstijl-community/components-css@1.0.1
+
+## 1.0.0
+
+### Major Changes
+
+- c91761d: Provide components-twig package which builds twig files and typescript
+
+## 0.0.2
+
+### Patch Changes
+
+- ed83ac9: First full release
+- Updated dependencies [ed83ac9]
+  - @rijkshuisstijl-community/components-css@1.0.0
+`;function t(e){return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"CHANGELOG"}),`
+`,n.jsx(a,{children:c})]})}function y(e={}){const{wrapper:d}={...s(),...e.components};return d?n.jsx(d,{...e,children:n.jsx(t,{...e})}):t()}export{y as default};
--- assets/index-D8Vkj_yf.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CHcz7OmU.js";import"./DocsRenderer-CFRXHY34-CEWIMsdL.js";import"./index-C6L4sK_Y.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
