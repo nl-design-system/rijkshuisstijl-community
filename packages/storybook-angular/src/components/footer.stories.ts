b160e7cd5d49d9e6ba4433acc587a104e7eb6e5d
Sjimmie <sbleeker@quintor.local>
2025-09-25 10:53:57 +0200

    Footer in page layout

--- packages/storybook-angular/src/components/footer.stories.ts
+++ packages/storybook-angular/src/components/footer.stories.ts
@@ -1,6 +1,6 @@
 import { type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
 import { provideTablerIcons, TablerIconComponent } from 'angular-tabler-icons';
-import { IconChevronRight } from 'angular-tabler-icons/icons';
+import { IconArrowNarrowUp, IconChevronRight } from 'angular-tabler-icons/icons';
 import readme from './footer.md';
 import {
   BackToTopComponent,
@@ -30,6 +30,7 @@ const meta: Meta<FooterComponent> = {
       providers: [
         provideTablerIcons({
           IconChevronRight,
+          IconArrowNarrowUp,
         }),
       ],
     }),
@@ -206,25 +207,7 @@ const meta: Meta<FooterComponent> = {
         <rhc-link-list subFooter></rhc-link-list>
         <rhc-back-to-top subFooter>
             Terug naar boven
-            <rhc-icon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-up"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 5l0 14" />
-                <path d="M16 9l-4 -4" />
-                <path d="M8 9l4 -4" />
-              </svg>
-            </rhc-icon>
+            <rhc-icon icon><i-tabler name="arrow-narrow-up"/></rhc-icon>
         </rhc-back-to-top>
       </footer>
     `,
