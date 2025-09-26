5c302e24beca39dc6d708882cb0eaca8715fde6e
Sjimmie <sjimmiebleeker2000@gmail.com>
2025-09-26 14:38:03 +0200

    custom form field error message icon (#1918)
    Closes #1903 
    Closes #1837
    
    ---------
    
    Co-authored-by: Sjimmie <sbleeker@quintor.local>

---
+++ .changeset/clean-cows-visit.md
@@ -0,0 +1,5 @@
+---
+'@rijkshuisstijl-community/components-angular': major
+---
+
+You now have to provide your own error icon to Form field error message
--- packages/storybook-angular/src/components/form-field-text-input.stories.ts
+++ packages/storybook-angular/src/components/form-field-text-input.stories.ts
@@ -416,7 +416,27 @@ export const ErrorMessage: StoryObj<FormFieldTextInputComponent> = {
         [label]="label"
         [invalid]="invalid"
         [errorMessage]="errorMessage"
-      />
+      >
+        <rhc-icon class="utrecht-form-field-error-message--icon-container-icon" error-icon>
+          <svg
+            xmlns="<http://www.w3.org/2000/svg>"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-exclamation-circle"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path d="M12 9v4" />
+            <path d="M12 16v.01" />
+          </svg>
+        </rhc-icon>  
+      </rhc-form-field-text-input>
     </form>
     `,
     props: {
