6cc1589d7c4d6109239fb6f98862adf902020ded
Fionn Mac an Bhaird <fionn.mac@rijksoverheid.nl>
2025-10-13 12:03:11 +0200

    chore: add testing an clean up old code

--- packages/internal-tooling/src/markdownUtils/index.ts
+++ packages/internal-tooling/src/markdownUtils/index.ts
@@ -19,12 +19,13 @@ export const mergeMarkdown = (markdown: string[]) =>
  * The target is replaced only when it appears as a whole word, ensuring it's not part of another word.
  *
  * The function uses a regular expression with word boundaries to match the target only when it is surrounded by spaces,
- * punctuation (including hyphens and underscores), or at the start/end of the string. The replacement is case-insensitive
- * by default and global, meaning it will replace all instances of the target in the text.
+ * punctuation (including hyphens, underscores, and backticks), or at the start/end of the string. The replacement is
+ * case-insensitive by default and global, meaning it will replace all instances of the target in the text.
  *
  * Use cases:
  * - Replaces specific words or phrases in markdown text without affecting words that contain the target string.
  * - Handles hyphenated and underscored words (e.g., 'test' in 'test-case', 'test_case', or 'some-test').
+ * - Handles words in backticks (e.g., 'test' in '`test`').
  * - Ensures case-insensitive (by default) and global replacements across the entire markdown text.
  */
 
@@ -34,6 +35,7 @@ export const replaceMarkdown = (
   replacement: string,
   caseSensitive: boolean = false,
 ): string => {
-  const regex = new RegExp(`(?<=^|[\\s\\p{P}_-])${target}(?=[\\s\\p{P}_-]|$)`, `g${caseSensitive ? '' : 'i'}u`);
+  // Include backticks explicitly since they need to be escaped in template literals
+  const regex = new RegExp(`(?<=^|[\\s\\p{P}_\\-\`])${target}(?=[\\s\\p{P}_\\-\`]|$)`, `g${caseSensitive ? '' : 'i'}u`);
   return markdown.replace(regex, replacement);
 };
