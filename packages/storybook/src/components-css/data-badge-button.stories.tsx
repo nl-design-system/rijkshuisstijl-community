fff7d0c86a70ed3384ff9769889747f19d0b6dd5
Fionn Mac an Bhaird <fionn.mac@rijksoverheid.nl>
2025-10-15 14:42:17 +0200

    fix: update css export to fix story name casing

--- packages/storybook/src/components-css/data-badge-button.stories.tsx
+++ packages/storybook/src/components-css/data-badge-button.stories.tsx
@@ -1,4 +1,5 @@
 import reactMeta from '../components-react/data-badge-button.stories';
+import * as ReactStories from '../components-react/data-badge-button.stories';
 import { mergeCssMeta } from '../helpers/mergeCssMeta';
 
 export default {
@@ -7,12 +8,10 @@ export default {
   id: 'css-data-badge-button',
 };
 
-export {
-  Default,
-  WithIconBefore,
-  WithIconAfter,
-  WithHelperText,
-  FocusVisible,
-  Hover,
-  MultipleButtons,
-} from '../components-react/data-badge-button.stories';
+export const Default = ReactStories.Default;
+export const MultipleButtons = ReactStories.MultipleButtons;
+export const WithIconAfter = ReactStories.WithIconAfter;
+export const WithIconBefore = ReactStories.WithIconBefore;
+export const WithHelperText = ReactStories.WithHelperText;
+export const FocusVisible = ReactStories.FocusVisible;
+export const Hover = ReactStories.Hover;
