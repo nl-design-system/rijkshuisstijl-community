06d855d097a02bd54efcf24e76c9490fa8085902
Fionn Mac an Bhaird <fionn.mac@rijksoverheid.nl>
2025-10-17 11:10:20 +0200

    fix: add type helpers for drupal & twig components

--- packages/components-twig/types/twig.d.ts
+++ packages/components-twig/types/twig.d.ts
@@ -1,4 +1,6 @@
 declare module '*.twig' {
-  const content: string;
-  export default content;
+  // Twig templates are compiled to functions that accept props and return strings
+  // We type them as 'any' to work with both string rendering and Storybook's component expectations
+  const TwigTemplate: any;
+  export default TwigTemplate;
 }
