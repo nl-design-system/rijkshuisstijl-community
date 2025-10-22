af526a53401e2255833943c36666f6fa04b58b43
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-10-22 12:57:02 +0200

    chore: update import

--- packages/storybook/.storybook/StoryRootDecorator.tsx
+++ packages/storybook/.storybook/StoryRootDecorator.tsx
@@ -1,4 +1,4 @@
-import type { Decorator } from '@storybook/react';
+import type { Decorator } from '@storybook/react-vite';
 import { type CSSProperties, useEffect } from 'react';
 
 export const StoryRootDecorator: Decorator = (Story, context) => {
