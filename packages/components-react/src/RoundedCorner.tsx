d251cb294088840078916a97e6f469ae245732ba
FinnWard <33222605+FinnWard@users.noreply.github.com>
2025-10-27 14:35:52 +0100

    feat: add react rounded corner based on css component

---
+++ packages/components-react/src/RoundedCorner.tsx
@@ -0,0 +1,38 @@
+import clsx from 'clsx';
+import { ComponentPropsWithoutRef, CSSProperties, ElementType } from 'react';
+
+type Size = 'sm' | 'md' | 'lg';
+type Corner = 'start-start' | 'start-end' | 'end-start' | 'end-end';
+
+type SuggestedTokens = {
+  '--rhc-rounded-corner-radius': CSSProperties['borderRadius'];
+  '--rhc-rounded-corner-overflow': CSSProperties['overflow'];
+};
+
+type OverwriteTokens = Partial<SuggestedTokens & Record<string, string>>;
+
+export type RoundedCornerProps<T extends ElementType> = {
+  size?: Size; // We make size optional to allow for custom styling via CSS overwriteTokens
+  corner: Corner;
+  as?: T;
+  overwriteTokens?: OverwriteTokens;
+} & ComponentPropsWithoutRef<T>;
+
+export const RoundedCorner = <T extends ElementType = 'div'>({
+  size,
+  corner,
+  as,
+  overwriteTokens,
+  ...props
+}: RoundedCornerProps<T>) => {
+  const className = clsx(
+    'rhc-rounded-corner',
+    {
+      [`rhc-rounded-corner--size-${size}`]: size,
+      [`rhc-rounded-corner--corner-${corner}`]: corner,
+    },
+    props['className'],
+  );
+  const Component = as || 'div';
+  return <Component {...props} className={className} style={{ ...overwriteTokens, ...props['style'] }} />;
+};
