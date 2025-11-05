093f3af6c98b68b400f33a3ca03d81ce595313cf
Casper Ravenhorst <casper@sinedub.io>
2025-11-05 14:07:55 +0100

    feat: update token format (#2061)
    fixes
    https://github.com/nl-design-system/rijkshuisstijl-community/issues/1768
    
    ---------
    
    Co-authored-by: AlineNap <59806622+AlineNap@users.noreply.github.com>
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

--- proprietary/design-tokens/token-transformer.mjs
+++ proprietary/design-tokens/token-transformer.mjs
@@ -1,31 +1,6 @@
 import { existsSync, mkdirSync } from 'fs';
 import { readFile, writeFile } from 'node:fs/promises';
 
-import { transformTokens } from 'token-transformer';
-
-// the commented options and their values are default values from the token-transformer package
-const transformerOptions = {
-  // expandTypography: true,
-  // expandShadow: true,
-  // expandComposition: true,
-  // expandBorder: true,
-  // preserveRawValue: false,
-  // throwErrorWhenNotResolved: true,
-  // Tried to set this to false, it worked in principle but there were breaking in the styles especially with spacing. I think it's better to keep it true
-  // resolveReferences: true,
-};
-
-const excludes = [
-  'components/avatar',
-  'components/form-field-option-label',
-  'components/modal-dialog',
-  'components/pagination/todo',
-  'components/status-badge',
-  'components/summary-list',
-  'components/task-list',
-  'components/toolbar-button',
-];
-
 // Split tokens into separate files
 async function transformAndSplitTokens() {
   // Read the raw JSON file directly
@@ -36,14 +11,10 @@ async function transformAndSplitTokens() {
   // Not sure if other designers might not be able to modify the themes created by paid plugin so getting the base tokens is a good fallback
   const baseTokens = {};
   Object.entries(tokens).forEach(([key, value]) => {
-    if (!key.startsWith('$') && !key.startsWith('overwrites/')) {
+    if (!key.startsWith('$themes') && !key.startsWith('overwrites/')) {
       baseTokens[key] = value;
     }
   });
-
-  // Transform base tokens
-  let resolved = transformTokens(baseTokens, [], excludes, transformerOptions);
-  delete resolved['tokenSetOrder'];
 
   // Process themes to add the enabled components
   const themes = tokens.$themes || [];
@@ -60,12 +31,9 @@ async function transformAndSplitTokens() {
       }
     });
 
-    // Transform the theme tokens
-    const transformedThemeTokens = transformTokens(themeTokens, [], excludes, transformerOptions);
-
     processedThemes[theme.name] = {
       id: theme.id,
-      tokens: transformedThemeTokens,
+      tokens: themeTokens,
       group: theme.group,
     };
   });
@@ -76,7 +44,7 @@ async function transformAndSplitTokens() {
   }
 
   // Write files
-  await writeFile('./src/generated/base.tokens.json', JSON.stringify(resolved, null, 2));
+  await writeFile('./src/generated/base.tokens.json', JSON.stringify(baseTokens, null, 2));
   await writeFile('./src/generated/themes.json', JSON.stringify(processedThemes, null, 2));
 }
 
