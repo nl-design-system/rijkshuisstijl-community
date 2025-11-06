8263f2086240946483db2b1c79e29cd6bd237e20
FinnWard <33222605+FinnWard@users.noreply.github.com>
2025-11-06 15:42:10 +0100

    fix: force addon docs to be loaded into the build

--- packages/storybook/vite.config.mjs
+++ packages/storybook/vite.config.mjs
@@ -12,6 +12,16 @@ export default defineConfig({
       framework: 'react',
     }),
   ],
+  // Force inclusion of addon-docs to avoid issues with it being excluded in the build
+  viteFinal: (config) => {
+    config.optimizeDeps = config.optimizeDeps || {};
+    config.optimizeDeps.include = [
+      ...(config.optimizeDeps.include || []),
+      '@storybook/addon-docs',
+      '@storybook/addon-docs/blocks',
+    ];
+    return config;
+  },
   resolve: {
     // we set the rhc:source so that the react package resolves directly to the source files for development in this monorepo
     conditions: ['rhc:source', 'import', 'require', 'default'],
