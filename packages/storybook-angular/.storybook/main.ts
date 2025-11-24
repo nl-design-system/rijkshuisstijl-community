8d409420ea04171a3b471cd65edfc952221291d8
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-11-24 08:50:38 +0100

    fix: gebruik variant welke werkt met cjs format

--- packages/storybook-angular/.storybook/main.ts
+++ packages/storybook-angular/.storybook/main.ts
@@ -1,11 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
+import { dirname, join } from 'path';
 // Utility to resolve the absolute path of a package needed in projects that use Yarn PnP or are set up within a monorepo
 // https://storybook.js.org/docs/faq#how-do-i-fix-module-resolution-in-special-environments
-const getAbsolutePath = (packageName: string) =>
-  dirname(fileURLToPath(import.meta.resolve(`${packageName}/package.json`)));
+export const getAbsolutePath = (value: string): string => dirname(require.resolve(join(value, 'package.json')));
 
 const config: StorybookConfig = {
   stories: [
