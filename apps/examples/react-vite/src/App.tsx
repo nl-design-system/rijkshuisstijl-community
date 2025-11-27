38521c0d73cf7bd8895397364d46b5ce3a9bd520
NeslihanUTUK <nesli.utuk@gmail.com>
2025-11-27 14:44:19 +0100

    docs: voorbeel applicatie react-vite - image toegevoegd

--- apps/examples/react-vite/src/App.tsx
+++ apps/examples/react-vite/src/App.tsx
@@ -7,59 +7,24 @@ import { InDePraktijk } from './InDePraktijk';
 import '@rijkshuisstijl-community/design-tokens/dist/index.css'; // design tokens importeren
 import '@rijkshuisstijl-community/components-css/dist/index.css';
 
-import { Card, PageContent } from '@rijkshuisstijl-community/components-react';
+import { Card, PageContent, Image } from '@rijkshuisstijl-community/components-react';
 
 function App() {
   return (
     <>
-      <Header/>
-      <PageContent className='page-content'>
+      <Header />
+      <PageContent className="page-content">
         {/* geef alle thema tokens mee aan child components voor styling */}
-        {/* <RoundedCorner
-          position="start-start"
-          size="lg"
-          style={{
-            backgroundImage: 'url(.src/placeholder.jpg)',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            height: 'full',
-            width: 'full',
-          }}
-        >
-          <RoundedCorner
-            position="start-end"
-            size="md"
-            style={{
-              margin: '13rem 3rem 0rem 0rem',
-            }}
-          >
-            <div
-              style={{
-                backgroundColor: 'var(--rhc-color-primary-500)',
-                padding: '3rem 1rem',
-              }}
-            >
-              <span
-                style={{
-                  color: 'white',
-                }}
-              >
-                Rounded Corner Content
-              </span>
-            </div>
-          </RoundedCorner>
-        </RoundedCorner> */}
+        <Image alt="Multicolored tulip field" height={425} src="src/placeholder.jpg" width={1000} />
 
         <div className="card-doelen">
           <Card
             description="Een uitgebreide bibliotheek van herbruikbare componenten. Platform-onafhankelijk met de nieuwe Rijkshuisstijl."
             heading="Efficient"
-          
           />
           <Card
             description=" De community deelt regelmatig kennis, ervaring, updates en verbeteringen."
             heading="Breed gedragen"
-
           />
           <Card
             description="Consistente gebruikservaring met herbruikbare voorbeeldpagina's"
