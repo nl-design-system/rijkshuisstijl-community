f7d62f754d8cb29bf470be83617d03a3d98814cc
NeslihanUTUK <nesli.utuk@gmail.com>
2025-12-01 14:13:06 +0100

    docs: voorbeeld applicatie react-vite - css regelingen Co-authored-by: Fionn Mac an Bhaird <FinnWard@users.noreply.github.com>

--- apps/examples/react-vite/src/App.tsx
+++ apps/examples/react-vite/src/App.tsx
@@ -1,42 +1,47 @@
+
+import '@rijkshuisstijl-community/design-tokens/dist/index.css'; // design tokens importeren
+import '@rijkshuisstijl-community/components-css/dist/index.css';
+import { Card, PageContent, Image } from '@rijkshuisstijl-community/components-react';
+
 import './App.css';
+
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { LeesEnLeer } from './LeesEnLeer';
 import { KomInContact } from './KomInContact/KomInContact';
 import { InDePraktijk } from './InDePraktijk';
-import '@rijkshuisstijl-community/design-tokens/dist/index.css'; // design tokens importeren
-import '@rijkshuisstijl-community/components-css/dist/index.css';
 
-import { Card, PageContent, Image } from '@rijkshuisstijl-community/components-react';
 
 function App() {
   return (
     <>
       <Header />
-      <PageContent className="page-content">
-        {/* geef alle thema tokens mee aan child components voor styling */}
-        <Image alt="Multicolored tulip field" height={425} src="src/placeholder.jpg" width={1000} />
+      <div className='page-content-container'>
+        <PageContent className="page-content">
+          {/* geef alle thema tokens mee aan child components voor styling */}
+          <Image alt="Multicolored tulip field" height={425} src="src/placeholder.jpg" width={1280} />
 
-        <div className="card-doelen">
-          <Card
-            description="Een uitgebreide bibliotheek van herbruikbare componenten. Platform-onafhankelijk met de nieuwe Rijkshuisstijl."
-            heading="Efficient"
-          />
-          <Card
-            description=" De community deelt regelmatig kennis, ervaring, updates en verbeteringen."
-            heading="Breed gedragen"
-          />
-          <Card
-            description="Consistente gebruikservaring met herbruikbare voorbeeldpagina's"
-            heading="Gebruiksvriendelijk"
-          />
-        </div>
+          <div className="card-doelen">
+            <Card
+              description="Een uitgebreide bibliotheek van herbruikbare componenten. Platform-onafhankelijk met de nieuwe Rijkshuisstijl."
+              heading="Efficient"
+            />
+            <Card
+              description=" De community deelt regelmatig kennis, ervaring, updates en verbeteringen."
+              heading="Breed gedragen"
+            />
+            <Card
+              description="Consistente gebruikservaring met herbruikbare voorbeeldpagina's"
+              heading="Gebruiksvriendelijk"
+            />
+          </div>
 
-        <InDePraktijk />
-        <LeesEnLeer />
-        <KomInContact />
-        <Footer />
-      </PageContent>
+          <InDePraktijk />
+          <LeesEnLeer />
+          <KomInContact />
+          <Footer />
+        </PageContent>
+      </div>
     </>
   );
 }
---
+++ apps/examples/react-vite2/vite-voorbeeld/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import '@rijkshuisstijl-community/design-tokens/dist/index.css'; // design tokens importeren
+import '@rijkshuisstijl-community/components-css/dist/index.css';
+
+import { } from '@rijkshuisstijl-community/components-react';
+
+
+function App() {
+  return(
+    <>
+    <h1> Merhaba</h1>
+  </>
+  );
+}
+
+export default App;
