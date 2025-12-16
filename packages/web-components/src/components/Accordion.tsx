f984c4c2f53708efcc546afc662f6e68386c6634
FinnWard <33222605+FinnWard@users.noreply.github.com>
2025-12-16 11:45:54 +0100

    feat: add licenses to accordion

--- packages/web-components/src/components/Accordion.tsx
+++ packages/web-components/src/components/Accordion.tsx
@@ -1,3 +1,8 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2025 Community for NL Design System
+ */
+
 import stylesheet from '@rijkshuisstijl-community/components-css/dist/index.css?inline';
 import { AccordionProvider, AccordionProviderProps, Icon, RHCIconID } from '@rijkshuisstijl-community/components-react';
 import { render } from 'preact';
