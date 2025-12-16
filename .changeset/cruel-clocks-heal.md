bd3dd48d5dee37a36df6724d2109243da9e2e73e
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2025-12-16 12:02:03 +0100

    Estafette/accordion (#2164)

---
+++ .changeset/cruel-clocks-heal.md
@@ -0,0 +1,7 @@
+---
+'@rijkshuisstijl-community/components-react': major
+'@rijkshuisstijl-community/web-components': major
+'@rijkshuisstijl-community/components-twig': major
+---
+
+Added EUPL license to Accordion and Alert source code
--- packages/web-components/src/components/Alert.tsx
+++ packages/web-components/src/components/Alert.tsx
@@ -1,3 +1,7 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2025 Community for NL Design System
+ */
 import stylesheet from '@rijkshuisstijl-community/components-css/dist/index.css?inline';
 import { Alert, AlertProps } from '@rijkshuisstijl-community/components-react';
 import { render } from 'preact';
