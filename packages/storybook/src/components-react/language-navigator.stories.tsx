386a3a51b28c820a60af40c8d5136d4706cd7b0c
FinnWard <33222605+FinnWard@users.noreply.github.com>
2025-12-16 09:14:38 +0100

    chore: add language navigation to storybook

--- packages/storybook/src/components-react/language-navigator.stories.tsx
+++ packages/storybook/src/components-react/language-navigator.stories.tsx
@@ -1,58 +1,25 @@
-import { Icon, Link, LinkButton, Listbox, ListboxOption } from '@rijkshuisstijl-community/components-react';
+import { LanguageNavigation } from '@rijkshuisstijl-community/components-react';
 import { Meta, StoryObj } from '@storybook/react-vite';
-import { useState } from 'react';
 
-const Languages = [
-  { languageName: 'English', localLanguageName: 'Engels', lang: 'en' },
-  { languageName: 'Español', localLanguageName: 'Spaans', lang: 'es' },
-  { languageName: 'Frysk', localLanguageName: 'Fries', lang: 'fy' },
-  { languageName: 'Nederlands', localLanguageName: 'Nederlands', lang: 'nl' },
-  { languageName: 'Papiamento', localLanguageName: 'Arubaans', lang: 'pap' },
-  { languageName: 'Papiamentu', localLanguageName: 'Curaçaos', lang: 'pap' },
-];
-
-const LanguageNavigator = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState('Nederlands');
+const LanguageNav = () => {
   return (
-    <>
-      <LinkButton onClick={() => setIsOpen(!isOpen)}>
-        {selectedLanguage} <Icon icon={isOpen ? 'chevron-up' : 'chevron-down'} />
-      </LinkButton>
-      {isOpen && (
-        <Listbox className="rhc-language-navigation--listbox" role="list">
-          {Languages.map(({ languageName, localLanguageName, lang }) => (
-            <ListboxOption
-              aria-selected={undefined}
-              className="rhc-language-navigation--list-item"
-              key={lang}
-              role={undefined}
-              selected={languageName === selectedLanguage}
-              onClick={() => {
-                setSelectedLanguage(languageName);
-                setIsOpen(false);
-              }}
-            >
-              <Link className="rhc-language-navigation--link" href="#">
-                <span lang={lang}>{languageName}</span>
-                {languageName !== selectedLanguage && (
-                  <span className="rhc-language-navigation--local-language"> ({localLanguageName})</span>
-                )}
-              </Link>
-            </ListboxOption>
-          ))}
-        </Listbox>
-      )}
-    </>
+    <LanguageNavigation.Root defaultSelectedLanguage="Nederlands">
+      <LanguageNavigation.Trigger />
+      <LanguageNavigation.Content>
+        <LanguageNavigation.Option lang="nl" languageName="Nederlands" />
+        <LanguageNavigation.Option lang="en" languageName="English" localLanguageName="Engels" />
+        <LanguageNavigation.Option lang="de" languageName="Deutsch" localLanguageName="Duits" />
+      </LanguageNavigation.Content>
+    </LanguageNavigation.Root>
   );
 };
 
 const meta = {
   title: 'Language Navigator',
   id: 'rhc-language-navigator',
-  component: LanguageNavigator,
+  component: LanguageNav,
   tags: ['autodocs'],
-} as Meta<typeof LanguageNavigator>;
+} as Meta<typeof LanguageNav>;
 
 type Story = StoryObj<typeof meta>;
 export default meta;
