8241e160610e16563b61f9559f164c17efcb954c
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-12 17:49:38 +0100

    fix: bezig met blockquote components tsx code

--- packages/components-react/src/Blockquote.tsx
+++ packages/components-react/src/Blockquote.tsx
@@ -3,5 +3,27 @@
  * Copyright (c) 2025 Community for NL Design System
  */
 
-export { Blockquote } from '@utrecht/component-library-react';
-export type { BlockquoteProps } from '@utrecht/component-library-react';
+import {
+     Blockquote as Blockquote,
+     BlockquoteProps as BlockquoteProps,
+type BlockquoteType as BlockquoteType,
+} from '@utrecht/component-library-react';
+
+import clsx from 'clsx';
+
+import { PropsWithChildren } from 'react';
+import { Blockquote, RHCBlockquoteID } from './Blockquote';
+export interface BlockquoteProps extends UtrechtBlockquoteProps{ };
+
+
+export const Blockquote = ({ type, blockquote, className, ...restProps }: PropsWithChildren<BlockquoteProps>) => {
+  const rhc-blockquote = blockquote ? <Blockquote blockquote={blockquote} /> : type ? <Blockquote blockquote={blockquoteMap[type]} /> : undefined;
+  return <UtrechtBlockquote className={clsx('rhc-blockquote', className)} {...restProps} blockquote={rhcBlockquote} type={type} />;
+};
+
+Blockquote.displayName = 'Blockquote'
+
+};
+
+// export { Blockquote } from '@utrecht/component-library-react';
+// export type { BlockquoteProps } from '@utrecht/component-library-react';
