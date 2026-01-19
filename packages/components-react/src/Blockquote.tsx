1f306bca368a4dba403c9f99002c64adf508c4c4
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-19 14:24:03 +0100

    fix: blockqutoe  cross contamination

--- packages/components-react/src/Blockquote.tsx
+++ packages/components-react/src/Blockquote.tsx
@@ -3,27 +3,13 @@
  * Copyright (c) 2025 Community for NL Design System
  */
 
-import {
-     Blockquote as Blockquote,
-     BlockquoteProps as BlockquoteProps,
-type BlockquoteType as BlockquoteType,
-} from '@utrecht/component-library-react';
-
+import { BlockquoteProps, Blockquote as UtrechtBlockquote } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 
-import { PropsWithChildren } from 'react';
-import { Blockquote, RHCBlockquoteID } from './Blockquote';
-export interface BlockquoteProps extends UtrechtBlockquoteProps{ };
+export type { BlockquoteProps } from '@utrecht/component-library-react';
 
-
-export const Blockquote = ({ type, blockquote, className, ...restProps }: PropsWithChildren<BlockquoteProps>) => {
-  const rhc-blockquote = blockquote ? <Blockquote blockquote={blockquote} /> : type ? <Blockquote blockquote={blockquoteMap[type]} /> : undefined;
-  return <UtrechtBlockquote className={clsx('rhc-blockquote', className)} {...restProps} blockquote={rhcBlockquote} type={type} />;
+export const Blockquote = ({ className, ...restProps }: BlockquoteProps) => {
+  return <UtrechtBlockquote className={clsx('rhc-blockquote', className)} {...restProps} />;
 };
 
-Blockquote.displayName = 'Blockquote'
-
-};
-
-// export { Blockquote } from '@utrecht/component-library-react';
-// export type { BlockquoteProps } from '@utrecht/component-library-react';
+Blockquote.displayName = 'Blockquote';
