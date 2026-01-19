4b40d675949586adc9ed177927ac83a459b191f6
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-19 17:24:18 +0100

    fix: cross contamination is resolved for checkbox component

--- packages/components-react/src/Checkbox.tsx
+++ packages/components-react/src/Checkbox.tsx
@@ -1,1 +1,12 @@
-export { Checkbox, type CheckboxProps } from '@utrecht/component-library-react';
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2025 Community for NL Design System
+ */
+import {type CheckboxProps, Checkbox as UtrechtCheckbox } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+
+export const Checkbox = ({ className, ...restProps }: CheckboxProps) => {
+  return (
+    <UtrechtCheckbox className={clsx('rhc-checkbox', className)} {...restProps}/>
+  );
+};
