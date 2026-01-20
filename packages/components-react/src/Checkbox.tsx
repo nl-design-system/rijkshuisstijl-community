9c4960ad1c6346247b1a656e522a5fc53e13d2a0
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-20 17:16:28 +0100

    fix : css selector name to rhc-theme changed

--- packages/components-react/src/Checkbox.tsx
+++ packages/components-react/src/Checkbox.tsx
@@ -2,12 +2,5 @@
  * @license EUPL-1.2
  * Copyright (c) 2025 Community for NL Design System
  */
-import { type CheckboxProps, Checkbox as UtrechtCheckbox } from '@utrecht/component-library-react';
-import clsx from 'clsx';
 
-export type { CheckboxProps } from '@utrecht/component-library-react';
-export const Checkbox = ({ className, ...restProps }: CheckboxProps) => {
-  return <UtrechtCheckbox className={clsx('rhc-checkbox', className)} {...restProps} />;
-};
-
-Checkbox.displayName = 'Checkbox';
+export { Checkbox, type CheckboxProps } from '@utrecht/component-library-react';
