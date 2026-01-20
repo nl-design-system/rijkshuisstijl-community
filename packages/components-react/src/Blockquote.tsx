a676a68251a0b3ad9483a9c401f9fe9f0c6d70ca
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-20 15:23:44 +0100

    fix: css selector veranderd

--- packages/components-react/src/Blockquote.tsx
+++ packages/components-react/src/Blockquote.tsx
@@ -2,14 +2,13 @@
  * @license EUPL-1.2
  * Copyright (c) 2025 Community for NL Design System
  */
-
 import { BlockquoteProps, Blockquote as UtrechtBlockquote } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 
 export type { BlockquoteProps } from '@utrecht/component-library-react';
 
 export const Blockquote = ({ className, ...restProps }: BlockquoteProps) => {
-  return <UtrechtBlockquote className={clsx('rhc-blockquote__blockquote', className)} {...restProps} />;
+  return <UtrechtBlockquote className={clsx('rhc-blockquote', className)} {...restProps} />;
 };
 
 Blockquote.displayName = 'Blockquote';
