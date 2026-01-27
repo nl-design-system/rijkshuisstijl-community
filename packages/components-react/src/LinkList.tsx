3f08b19a4677e2267103c5da8fafba4b381e247b
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-27 15:46:34 +0100

    fix: LinkList cross contamination

--- packages/components-react/src/LinkList.tsx
+++ packages/components-react/src/LinkList.tsx
@@ -1,1 +1,16 @@
-export { LinkList, LinkListLink, type LinkListProps, type LinkListLinkProps } from '@utrecht/component-library-react';
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2025 Community for NL Design System
+ */
+import { LinkList, type LinkListProps as UtrechtLinkList } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+
+
+export type { LinkListProps } from '@utrecht/component-library-react';
+
+
+export const LinkList =({className, ...restProps }: LinklistProps) => {
+    return  <UtrechtLinkList className={clsx('rhc-link-list', className)}{...restProps} />;
+    };
+
+LinkList.displayName = 'LinkList';
