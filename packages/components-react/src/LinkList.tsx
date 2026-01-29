ba1de8275f90cd71da5bc8e63dd83e1ceb4731c9
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-29 10:50:09 +0100

    fix: LinkList.tsx page changed for css cross contamination

--- packages/components-react/src/LinkList.tsx
+++ packages/components-react/src/LinkList.tsx
@@ -2,15 +2,14 @@
  * @license EUPL-1.2
  * Copyright (c) 2025 Community for NL Design System
  */
-import { LinkList, type LinkListProps as UtrechtLinkList } from '@utrecht/component-library-react';
+import { LinkList as UtrechtLinkList, type LinkListProps as UtrechtLinkListProps} from '@utrecht/component-library-react';
 import clsx from 'clsx';
 
+export type LinkListProps = UtrechtLinkListProps;
 
-export type { LinkListProps } from '@utrecht/component-library-react';
 
-
-export const LinkList =({className, ...restProps }: LinklistProps) => {
-    return  <UtrechtLinkList className={clsx('rhc-link-list', className)}{...restProps} />;
+export const LinkList =({className, ...restProps }: LinkListProps) => {
+    return  <UtrechtLinkList className={clsx('rhc-link-list', className)}{...restProps}/>;
     };
 
 LinkList.displayName = 'LinkList';
