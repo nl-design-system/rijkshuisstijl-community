606e038f71768ce630ecd7c08147dd733203cef7
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-30 11:10:51 +0100

    fix: typing and sorting error corrected

--- packages/components-react/src/LinkList.tsx
+++ packages/components-react/src/LinkList.tsx
@@ -2,14 +2,14 @@
  * @license EUPL-1.2
  * Copyright (c) 2025 Community for NL Design System
  */
-import { LinkList, LinkListProps as UtrechtLinkList} from '@utrecht/component-library-react';
+import { LinkListProps, LinkList as UtrechtLinkList} from '@utrecht/component-library-react';
 import clsx from 'clsx';
 
 export type { LinkListProps } from '@utrecht/component-library-react';
 
 
-export const LinkList =({className, ...restProps }: LinkListProps) => {
-    return  <UtrechtLinkList className={clsx('rhc-link-list', className)}{...restProps}/>;
-    };
+export const LinkList = ({className, ...restProps } : LinkListProps) => {
+    return <UtrechtLinkList className = {clsx('rhc-link-list', className)}{...restProps}/>;
+};
 
 LinkList.displayName = 'LinkList';
