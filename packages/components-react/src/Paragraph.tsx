aef06e7e2f954e176b9e23cfb1984627736a773b
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-02 13:52:14 +0100

    fix: unnecessary semi colon afgehaald

--- packages/components-react/src/Paragraph.tsx
+++ packages/components-react/src/Paragraph.tsx
@@ -2,17 +2,19 @@
  * @license EUPL-1.2
  * Copyright (c) 2025 Community for NL Design System
  */
-import { Paragraph as NlParagraph, type ParagraphPurpose as NlParagraphPurpose} from '@nl-design-system-candidate/paragraph-react';
+import {
+  Paragraph as NlParagraph,
+  type ParagraphPurpose as NlParagraphPurpose } from '@nl-design-system-candidate/paragraph-react';
 import clsx from 'clsx';
 import { HTMLAttributes } from 'react';
 
 export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
   purpose?: NlParagraphPurpose;
   className?: string;
-};
+}
 
 export const Paragraph = ({ className, ...restProps }: ParagraphProps) => {
-  return <NlParagraph className= {clsx('rhc-paragraph', className)}{...restProps} />;
-  };
+  return <NlParagraph className={clsx('rhc-paragraph', className)} {...restProps} />;
+};
 
 Paragraph.displayName = 'Paragraph';
