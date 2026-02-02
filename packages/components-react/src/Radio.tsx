06f8655e93c18a2c0267555622525dc56b1d37c6
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-02 16:08:37 +0100

    fix: werken voor css cross contamination op te lossen

--- packages/components-react/src/Radio.tsx
+++ packages/components-react/src/Radio.tsx
@@ -1,1 +1,11 @@
-export { RadioButton as Radio, type RadioButtonProps as RadioProps } from '@utrecht/component-library-react';
+import{RadioProps, Radio as RadioButton } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+
+export type { RadioButtonProps } from  '@utrecht/component-library-react';
+
+
+export const Radio = ({ className, ...restProps }: RadioProps) => {
+  return <RadioButton className={clsx('rhc-radio', className)} {...restProps} />;
+};
+
+Radio.displayName = 'Radio';
