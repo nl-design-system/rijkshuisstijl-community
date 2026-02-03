19f1cd66f9ffa09e62f15c2c032230c70d5d2c4a
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-03 12:42:53 +0100

    chore: license added

--- packages/components-react/src/Radio.tsx
+++ packages/components-react/src/Radio.tsx
@@ -1,3 +1,7 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
 import{ RadioButton, type RadioButtonProps } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 
