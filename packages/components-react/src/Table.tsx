dc3a016b7d47b0ddcf96b6ed3910f3fd5563dcf8
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-05 16:12:00 +0100

    fix: eerste stapjes om te oplossen

--- packages/components-react/src/Table.tsx
+++ packages/components-react/src/Table.tsx
@@ -1,12 +1,15 @@
-export {
-  Table,
-  type TableProps,
-  TableHeader,
-  type TableHeaderProps,
-  TableRow,
-  type TableRowProps,
-  TableBody,
-  type TableBodyProps,
-  TableFooter,
-  type TableFooterProps,
-} from '@utrecht/component-library-react';
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import { Table } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+import { ComponentProps } from 'react';
+
+export type { TableProps as RhcTableProps } from  '@utrecht/component-library-react'
+
+export const RhcTable = ( {className, ...restProps } : ComponentProps<typeof TableProps> ) => {
+  return <Table className={clsx('rhc-table',className)} {...restProps} />;
+
+};
+RhcTable.displayName = 'RhcTable';
