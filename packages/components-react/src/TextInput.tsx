b0479f25cded3798725aca74bd2f6cb4774355a6
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-09 15:23:03 +0100

    fix: corrected className

--- packages/components-react/src/TextInput.tsx
+++ packages/components-react/src/TextInput.tsx
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 export type { TextboxProps as TextInputProps } from '@utrecht/component-library-react';
 
 export const TextInput= ({className, ...restProps } : TextInputProps ) => {
-    return <Textbox className={clsx('rhc-textinput', className )} {...restProps} />;
+    return <Textbox className={clsx('rhc-text-input', className )} {...restProps} />;
 };
 
 TextInput.displayName = 'TextInput';
