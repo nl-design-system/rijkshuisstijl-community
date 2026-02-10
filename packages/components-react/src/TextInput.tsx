764499173d7b8badd27b2709171d4be48ac9c459
NeslihanUTUK <nesli.utuk@gmail.com>
2026-02-10 11:35:10 +0100

    fix:  name from Props aangepast

--- packages/components-react/src/TextInput.tsx
+++ packages/components-react/src/TextInput.tsx
@@ -3,13 +3,14 @@
  * Copyright (c) 2021 Community for NL Design System
  */
 
-import { Textbox , type TextboxProps as TextInputProps} from '@utrecht/component-library-react';
+import { Textbox } from '@utrecht/component-library-react';
 import clsx from 'clsx';
+import { ComponentProps } from 'react';
 
 export type { TextboxProps as TextInputProps } from '@utrecht/component-library-react';
 
-export const TextInput= ({className, ...restProps } : TextInputProps ) => {
-    return <Textbox className={clsx('rhc-text-input', className )} {...restProps} />;
+export const TextInput = ({ className, ...restProps }: ComponentProps<typeof Textbox>) => {
+  return <Textbox className={clsx('rhc-text-input', className)} {...restProps} />;
 };
 
 TextInput.displayName = 'TextInput';
