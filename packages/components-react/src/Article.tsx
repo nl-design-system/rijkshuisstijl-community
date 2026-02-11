8481c2fa7596095d652a6f73387bc28faeccf60d
NeslihanUTUK <120681285+NeslihanUTUK@users.noreply.github.com>
2026-02-11 09:34:47 +0100

    2262 article   css cross contamination (#2263)
    Co-authored-by: Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>

--- packages/components-react/src/Article.tsx
+++ packages/components-react/src/Article.tsx
@@ -1,1 +1,15 @@
-export { Article, type ArticleProps } from '@utrecht/component-library-react';
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2026 Community for NL Design System
+ */
+
+import { Article as UtrechtArticle } from '@utrecht/component-library-react'; 
+import type { ArticleProps } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+
+export type { ArticleProps } from '@utrecht/component-library-react'; 
+export const Article = ({ className,  ...restProps } : ArticleProps ) => {
+    return <UtrechtArticle className = {clsx('rhc-article', className )}{...restProps} />;
+};
+
+Article.displayName = 'Article';
