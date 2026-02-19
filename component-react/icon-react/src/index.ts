d29774cdebfe823cd43a861c36cc5b69a16ea21b
Casper Ravenhorst <casper@sinedub.io>
2026-02-19 20:22:08 +0100

    fix: import type notation

--- component-react/icon-react/src/index.ts
+++ component-react/icon-react/src/index.ts
@@ -1,3 +1,4 @@
-export { Icon, type IconProps, type RHCIconID, getIconSet, registerIconSet } from './Icon';
+export { Icon, getIconSet, registerIconSet } from './Icon';
+export type { IconProps, RHCIconID } from './Icon';
 export { iconSet as defaultIconSet } from './icon-sets/default-icons/icon-set';
 export { iconSet as flagIconSet } from './icon-sets/flag-icons/icon-set';
