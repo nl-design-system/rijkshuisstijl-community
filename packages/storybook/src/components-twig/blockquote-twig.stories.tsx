b8639b0e08ca9b12ee5a69995c308d2847aac66b
Job Kuipers <info@photosjob.nl>
2026-02-23 15:01:29 +0100

    Add Blockquote.twig and blockquote-twig.stories.tsx

---
+++ packages/storybook/src/components-twig/blockquote-twig.stories.tsx
@@ -0,0 +1,44 @@
+import TwigBlockquote from '@rijkshuisstijl-community/components-twig/src/Blockquote.twig';
+import { mergeMarkdown } from '@rijkshuisstijl-community/storybook-tooling/markdownUtils';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import readme from '../components-react/blockquote.md?raw';
+
+const meta = {
+  title: 'Blockquote',
+  id: 'rhc-twig-blockquote',
+  component: TwigBlockquote,
+  argTypes: {
+    attribution: {
+      description: 'string',
+      control: { type: 'text' },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: mergeMarkdown([readme]),
+      },
+    },
+    componentOrigin: 'Dit component is volledig ontwikkeld door de Rijkshuisstijl Community.',
+    //Todo: voeg NL-Design system & Figma links toe
+    github:
+      'https://github.com/nl-design-system/rijkshuisstijl-community/blob/main/packages/components-twig/src/Blockquote.twig',
+  },
+} satisfies Meta<typeof TwigBlockquote>;
+
+export default meta;
+
+export const TwigBlockquoteDefault: StoryObj<typeof meta> = {
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *',
+  },
+  name: 'Default',
+};
+
+export const TwigBlockquoteAttribution: StoryObj<typeof meta> = {
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *',
+    attribution: '— Pippi Langkous',
+  },
+  name: 'Attribution',
+};
