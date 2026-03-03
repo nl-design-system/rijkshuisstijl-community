e04bf8828c0c9c46fb4b422218a5caa38092706a
FinnWard <33222605+FinnWard@users.noreply.github.com>
2026-03-03 16:26:22 +0100

    fix: NumberbadgeProps ts error

--- packages/components-react/library-react/src/NumberBadge.tsx
+++ packages/components-react/library-react/src/NumberBadge.tsx
@@ -3,8 +3,11 @@
  * Copyright (c) 2026 Community for NL Design System
  */
 
-import { NumberBadge as NlNumberBadge, NumberBadgeProps } from '@nl-design-system-candidate/number-badge-react';
+import { NumberBadge as NlNumberBadge } from '@nl-design-system-candidate/number-badge-react';
 import clsx from 'clsx';
+import { type ComponentPropsWithoutRef } from 'react';
+
+export type NumberBadgeProps = ComponentPropsWithoutRef<typeof NlNumberBadge>;
 
 export const NumberBadge = ({ className, ...restProps }: NumberBadgeProps) => {
   return <NlNumberBadge className={clsx('rhc-number-badge', className)} {...restProps} />;
