8c7aafb9ac1ec57af3b5f6ca86bdf26837907a7c
Robbert Broersma <git@robbertbroersma.nl>
2026-03-16 17:58:04 +0100

    refactor: use new layout components

--- apps/rhc-templates/src/app/layout.tsx
+++ apps/rhc-templates/src/app/layout.tsx
@@ -1,13 +1,18 @@
+/**
+ * TODO: Replace direct imports from `@utrecht/*-react` with `@rijkshuisstijl-community/components-react`
+ * when `"use client"` issues are solved.
+ */
+
 import { PropsWithChildren } from 'react';
+import { Body } from '@utrecht/body-react';
+import { Root } from '@utrecht/root-react';
+import { PageLayout } from '@utrecht/page-layout-react';
+import type { Metadata } from 'next';
 import '@nl-rvo/assets/fonts/index.css';
 import '@rijkshuisstijl-community/design-tokens/dist/index.css';
 import '@rijkshuisstijl-community/components-css/dist/index.css';
 import '@rijkshuisstijl-community/font/src/index.mjs';
 import '@rijkshuisstijl-community/storybook/src/templates/globals.css';
-import { PageLayout } from '@utrecht/page-layout-react';
-import { Body } from '@utrecht/body-react';
-import { Root } from '@utrecht/root-react';
-import type { Metadata } from 'next';
 import './layout.css';
 
 export const metadata: Metadata = {
