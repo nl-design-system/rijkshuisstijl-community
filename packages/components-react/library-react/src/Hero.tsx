f15163d60279735bb61fd92047b8a27911095fbb
NeslihanUTUK <nesli.utuk@gmail.com>
2026-03-24 10:38:08 +0100

    Fix: imports aangepast from '." to eigen packages/file

--- packages/components-react/library-react/src/Hero.tsx
+++ packages/components-react/library-react/src/Hero.tsx
@@ -3,12 +3,12 @@
  * Copyright (c) 2026 Community for NL Design System
  */
 
+import { Image } from '@rijkshuisstijl-community/image-react';
+import { Paragraph } from '@rijkshuisstijl-community/paragraph-react';
 import clsx from 'clsx';
 import { HTMLAttributes, PropsWithChildren, ReactNode, Ref } from 'react';
-import { Image, Paragraph } from '.';
 import { Heading, HeadingLevel } from './Heading';
 import { HeadingGroup } from './HeadingGroup';
-
 
 export interface HeroBaseProps extends HTMLAttributes<HTMLDivElement> {
   textAlign?: 'start' | 'end';
