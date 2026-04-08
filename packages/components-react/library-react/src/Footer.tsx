ed4088c6bc191249f0da69b5620da1a52bae027e
Robbert Broersma <git@robbertbroersma.nl>
2026-04-08 15:35:00 +0200

    refactor: simplify and improve footer implementation

--- packages/components-react/library-react/src/Footer.tsx
+++ packages/components-react/library-react/src/Footer.tsx
@@ -4,13 +4,13 @@
  */
 
 import { Icon } from '@rijkshuisstijl-community/icon-react';
-import { PageFooterProps, PageFooter as UtrechtPageFooter } from '@utrecht/component-library-react';
 import clsx from 'clsx';
-import { MouseEvent, PropsWithChildren, ReactNode, Ref } from 'react';
+import { HTMLAttributes, MouseEvent, PropsWithChildren, ReactNode, Ref } from 'react';
 import { ColumnLayout } from './ColumnLayout';
 import { Heading, HeadingLevel } from './Heading';
+import { Link } from './Link';
 
-interface FooterProps extends PageFooterProps {
+interface FooterProps extends HTMLAttributes<HTMLDivElement> {
   heading?: ReactNode;
   headingId?: string;
   appearanceLevel?: HeadingLevel;
@@ -56,18 +56,18 @@ export const Footer = ({
   tagline,
   ...restProps
 }: PropsWithChildren<FooterProps>) => (
-  <>
+  <footer aria-labelledby={heading ? headingId : undefined} className="rhc-page-footer-container">
     {preFooter && (
       <div className="rhc-page-prefooter">
         {preFooterMessage && <span className="rhc-page-prefooter__content">{preFooterMessage}</span>}
       </div>
     )}
 
-    <UtrechtPageFooter
-      aria-labelledby={heading ? headingId : undefined}
+    <div
       {...restProps}
       ref={ref}
       className={clsx(
+        'utrecht-page-footer',
         'rhc-page-footer',
         background ? `rhc-page-footer--${background}` : 'rhc-page-footer--primary-filled',
         className,
@@ -100,25 +100,29 @@ export const Footer = ({
           </ColumnLayout>
         </div>
       </div>
-      {(backtotop || subFooter) && (
-        <div
-          className={clsx(
-            'rhc-page-subfooter',
-            background ? `rhc-page-footer--${background}` : 'rhc-page-footer--primary-filled',
-          )}
-        >
-          <div className="rhc-page-subfooter__content rhc-page-footer__wrapper">
+    </div>
+    {(backtotop || subFooter) && (
+      <div
+        className={clsx(
+          'utrecht-page-footer',
+          'rhc-page-footer',
+          'rhc-page-footer--subfooter',
+          background ? `rhc-page-footer--${background}` : 'rhc-page-footer--primary-filled',
+        )}
+      >
+        <div className="utrecht-page-footer__content">
+          <div className="rhc-page-subfooter-layout">
             {subFooter}
             {backtotop && (
-              <a className="rhc-page-subfooter__backtotop" href="#main" onClick={scrollBackToTop}>
+              <Link href="#main" onClick={scrollBackToTop}>
                 Terug naar boven <Icon icon="pijl-omhoog" />
-              </a>
+              </Link>
             )}
           </div>
         </div>
-      )}
-    </UtrechtPageFooter>
-  </>
+      </div>
+    )}
+  </footer>
 );
 
 Footer.displayName = 'Footer';
