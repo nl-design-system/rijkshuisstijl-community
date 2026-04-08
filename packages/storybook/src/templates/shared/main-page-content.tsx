b14dca72f6e4567e7978e211324ce390d548d842
Robbert Broersma <git@robbertbroersma.nl>
2026-04-08 15:38:04 +0200

    refactor: stop using deprecated Utrecht PageContent component

--- packages/storybook/src/templates/shared/main-page-content.tsx
+++ packages/storybook/src/templates/shared/main-page-content.tsx
@@ -1,10 +1,9 @@
-import { PageContent } from '@rijkshuisstijl-community/components-react';
 import { PropsWithChildren } from 'react';
 
 export default function SharedMainPageContent({ children }: PropsWithChildren<{}>) {
   return (
     <div className="rhc-templates-main-content">
-      <PageContent className="rhc-templates-page-content">{children}</PageContent>
+      <div className="rhc-templates-page-content">{children}</div>
     </div>
   );
 }
