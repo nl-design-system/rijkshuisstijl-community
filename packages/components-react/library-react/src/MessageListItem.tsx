7c0864a177d657f15c91ab07ff673a5249ca28fe
NeslihanUTUK <nesli.utuk@gmail.com>
2026-04-09 11:26:55 +0200

    fix: import van button verwijderd

--- packages/components-react/library-react/src/MessageListItem.tsx
+++ packages/components-react/library-react/src/MessageListItem.tsx
@@ -3,13 +3,11 @@
  * Copyright (c) 2026 Community for NL Design System
  */
 
-
 import { DotBadge, DotBadgeProps } from '@rijkshuisstijl-community/dot-badge-react';
 import { Icon } from '@rijkshuisstijl-community/icon-react';
 import { Paragraph } from '@rijkshuisstijl-community/paragraph-react';
 import clsx from 'clsx';
 import { HTMLAttributes, ReactNode, Ref } from 'react';
-
 
 export interface MessageListItemProps extends HTMLAttributes<HTMLLIElement> {
   label: ReactNode;
