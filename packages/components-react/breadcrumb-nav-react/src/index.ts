d7a8f02737205687b1f9a1b449b58d5d45de4377
NeslihanUTUK <120681285+NeslihanUTUK@users.noreply.github.com>
2026-04-20 13:34:01 +0200

    2451 breadcrumbnav cssreact componenten opslitsen (#2455)

---
+++ packages/components-react/breadcrumb-nav-react/src/index.ts
@@ -0,0 +1,10 @@
+import '@rijkshuisstijl-community/breadcrumb-nav-css';
+
+export {
+  BreadcrumbNav,
+  BreadcrumbNavLink,
+  type BreadcrumbNavLinkProps,
+  type BreadcrumbNavProps,
+  BreadcrumbNavSeparator,
+  type BreadcrumbNavSeparatorProps,
+} from './BreadcrumbNav';
