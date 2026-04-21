a6c6d863ea7d93a74dd37e248aeb63046fc34206
NeslihanUTUK <nesli.utuk@gmail.com>
2026-04-21 15:30:37 +0200

    feat: updated exports and dependencies

--- packages/components-react/library-react/src/index.ts
+++ packages/components-react/library-react/src/index.ts
@@ -44,7 +44,7 @@ export {
   type HorizontalImageCardAsLinkProps,
 } from '@rijkshuisstijl-community/card-as-link-react';
 export { Checkbox, type CheckboxProps } from './Checkbox';
-export { CheckboxGroup, type CheckboxGroupProps } from './CheckboxGroup';
+export { CheckboxGroup, type CheckboxGroupProps } from '@rijkshuisstijl-community/checkbox-group-react';
 export { CodeInput } from './CodeInput';
 export { CodeInputGroup } from './CodeInputGroup';
 export { ColumnLayout, type ColumnLayoutProps } from './ColumnLayout';
