ebbb5fc0124e3d35e2101696acdf106a375c7b46
NeslihanUTUK <nesli.utuk@gmail.com>
2026-04-22 14:59:33 +0200

    fix: prettier

--- packages/components-react/checkbox-group-react/src/CheckboxGroup.test.tsx
+++ packages/components-react/checkbox-group-react/src/CheckboxGroup.test.tsx
@@ -1,7 +1,6 @@
 import { CheckboxGroup } from '@rijkshuisstijl-community/checkbox-group-react';
 import '@testing-library/jest-dom/vitest';
 import { PropsWithChildren } from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
 import { afterEach, describe, expect, it } from 'vitest';
 
 const Checkbox = ({ children }: PropsWithChildren) => (
