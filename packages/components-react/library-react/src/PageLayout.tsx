a56cbf0e12f21d090e0cb3fed4e33c77daaf508f
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2026-04-23 12:26:43 +0200

    chore: add * export to component barrel files

--- packages/components-react/library-react/src/PageLayout.tsx
+++ packages/components-react/library-react/src/PageLayout.tsx
@@ -3,4 +3,4 @@
  * Copyright (c) 2026 Community for NL Design System
  */
 
-export { PageLayout, type PageLayoutProps } from '@utrecht/page-layout-react';
+export * from '@utrecht/page-layout-react';
