9273b55bf7d2220de12d49b233a8d28684a91295
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2026-04-28 13:01:14 +0200

    chore: add no side effect to data summary item

--- packages/components-react/library-react/src/noSideEffects.ts
+++ packages/components-react/library-react/src/noSideEffects.ts
@@ -17,7 +17,7 @@ export * from './ColumnLayout';
 export * from './FormField';
 export * from './DataBadgeButton';
 export * from '@rijkshuisstijl-community/data-summary-react/no-side-effects';
-export * from '@rijkshuisstijl-community/data-summary-item-react';
+export * from '@rijkshuisstijl-community/data-summary-item-react/no-side-effects';
 export * from './Root';
 export * from '@rijkshuisstijl-community/dot-badge-react/no-side-effects';
 export * from './Drawer';
