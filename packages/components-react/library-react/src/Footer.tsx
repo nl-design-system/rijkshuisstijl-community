9b81470fe50ea5f0ea99ec6aa6560e663724afba
NeslihanUTUK <nesli.utuk@gmail.com>
2026-05-07 14:11:19 +0200

    fix: update colum layout imports in andere file

--- packages/components-react/library-react/src/Footer.tsx
+++ packages/components-react/library-react/src/Footer.tsx
@@ -3,12 +3,12 @@
  * Copyright (c) 2026 Community for NL Design System
  */
 
+import { ColumnLayout } from '@rijkshuisstijl-community/column-layout-react';
 import { Heading, HeadingLevel } from '@rijkshuisstijl-community/heading-react';
 import { Icon } from '@rijkshuisstijl-community/icon-react';
 import { Link } from '@rijkshuisstijl-community/link-react';
 import clsx from 'clsx';
 import { HTMLAttributes, MouseEvent, PropsWithChildren, ReactNode, Ref } from 'react';
-import { ColumnLayout } from './ColumnLayout';
 
 interface FooterProps extends HTMLAttributes<HTMLDivElement> {
   heading?: ReactNode;
