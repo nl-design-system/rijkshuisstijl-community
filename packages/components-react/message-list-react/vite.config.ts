456aa7d62e6720e34d7cbdda2f124963bc4877a4
NeslihanUTUK <nesli.utuk@gmail.com>
2026-06-04 17:00:37 +0200

    fix: vitest update

--- packages/components-react/message-list-react/vite.config.ts
+++ packages/components-react/message-list-react/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config.js';
 
 export default defineConfig({
   build: {
