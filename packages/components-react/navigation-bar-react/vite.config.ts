f1066742b55ff9c4eefaa98ee084b8f74ae19c1e
NeslihanUTUK <120681285+NeslihanUTUK@users.noreply.github.com>
2026-06-08 14:03:38 +0200

    Update vite.config.ts

--- packages/components-react/navigation-bar-react/vite.config.ts
+++ packages/components-react/navigation-bar-react/vite.config.ts
@@ -1,6 +1,6 @@
+import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
