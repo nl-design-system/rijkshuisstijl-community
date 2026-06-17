3e79c61154baa119405e05fdf5f083becc9e5d72
Casper Ravenhorst <casper@sinedub.io>
2026-06-17 10:41:10 +0200

    feat: basic components list

--- apps/rhc-templates/src/app/componenten/page.tsx
+++ apps/rhc-templates/src/app/componenten/page.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import SharedHeader from '@rijkshuisstijl-community/storybook/src/templates/shared/header';
 import SharedFooter from '@rijkshuisstijl-community/storybook/src/templates/shared/footer';
 import { PageBody } from '@utrecht/page-body-react';
+import { DataBadgeButton, Heading } from '@rijkshuisstijl-community/components-react';
 import type { Metadata } from 'next';
+
+import components from './componenten';
 
 export const metadata: Metadata = {
   title: 'Componenten',
@@ -11,7 +15,17 @@ export const metadata: Metadata = {
 export default () => (
   <>
     <SharedHeader />
-    <PageBody>hai</PageBody>
+    <PageBody>
+      {components.items.map((comp) => (
+        <React.Fragment key={comp.content.title}>
+          <Heading level={2}>{comp.content.title}</Heading>
+          {comp.react == 'Available' && <DataBadgeButton>React</DataBadgeButton>}
+          {comp.angular == 'Available' && <DataBadgeButton>Angular</DataBadgeButton>}
+          {comp.twig == 'Available' && <DataBadgeButton>Twig</DataBadgeButton>}
+          {comp['web component'] == 'Available' && <DataBadgeButton>Web component</DataBadgeButton>}
+        </React.Fragment>
+      ))}
+    </PageBody>
     <SharedFooter />
   </>
 );
