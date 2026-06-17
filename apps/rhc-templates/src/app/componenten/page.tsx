f4d2683929e07e59c0a3b05bb50fd0d01ba93db5
Casper Ravenhorst <casper@sinedub.io>
2026-06-17 11:55:36 +0200

    feat: display figma and discussion links

--- apps/rhc-templates/src/app/componenten/page.tsx
+++ apps/rhc-templates/src/app/componenten/page.tsx
@@ -1,30 +1,55 @@
-import React from 'react';
 import SharedHeader from '@rijkshuisstijl-community/storybook/src/templates/shared/header';
 import SharedFooter from '@rijkshuisstijl-community/storybook/src/templates/shared/footer';
 import { PageBody } from '@utrecht/page-body-react';
-import { DataBadgeButton, Heading } from '@rijkshuisstijl-community/components-react';
+import {
+  CardAsLink,
+  DataBadgeButton,
+  Heading,
+  LinkList,
+  LinkListLink,
+  Paragraph,
+} from '@rijkshuisstijl-community/components-react';
 import type { Metadata } from 'next';
 
 import components from './componenten';
 
 export const metadata: Metadata = {
   title: 'Componenten',
-  description: 'Lijst van alle beschikbare componenten, gepaard met statussen',
+  description: 'Lijst van alle beschikbare componenten, gepaard met statussen en handige links',
 };
 
 export default () => (
   <>
     <SharedHeader />
-    <PageBody>
-      {components.items.map((comp) => (
-        <React.Fragment key={comp.content.title}>
-          <Heading level={2}>{comp.content.title}</Heading>
-          {comp.react == 'Available' && <DataBadgeButton>React</DataBadgeButton>}
-          {comp.angular == 'Available' && <DataBadgeButton>Angular</DataBadgeButton>}
-          {comp.twig == 'Available' && <DataBadgeButton>Twig</DataBadgeButton>}
-          {comp['web component'] == 'Available' && <DataBadgeButton>Web component</DataBadgeButton>}
-        </React.Fragment>
-      ))}
+    <PageBody
+      style={{
+        paddingBlockEnd: 'var(--rhc-nav-bar-link-padding-block-end)',
+        paddingBlockStart: 'var(--rhc-nav-bar-link-padding-block-start)',
+        paddingInlineEnd: 'var(--rhc-nav-bar-link-padding-inline-end)',
+        paddingInlineStart: 'var(--rhc-nav-bar-link-padding-inline-start)',
+      }}
+    >
+      <div style={{ display: 'flex', flexDirection: 'column', rowGap: '12px' }}>
+        {components.items.map((comp) => (
+          <CardAsLink key={comp.content.title}>
+            <Heading level={2}>{comp.content.title}</Heading>
+            <Paragraph>Oorsprong: {comp.oorsprong}</Paragraph>
+            <Paragraph>NL status: {comp['nL Status']}</Paragraph>
+            <div style={{ display: 'flex', columnGap: '4px' }}>
+              {comp.react == 'Available' && <DataBadgeButton>React</DataBadgeButton>}
+              {comp.angular == 'Available' && <DataBadgeButton>Angular</DataBadgeButton>}
+              {comp.twig == 'Available' && <DataBadgeButton>Twig</DataBadgeButton>}
+              {comp['web component'] == 'Available' && <DataBadgeButton>Web component</DataBadgeButton>}
+            </div>
+            <LinkList>
+              {comp['figma URL'] && <LinkListLink href={comp['figma URL']}>Design</LinkListLink>}
+              {comp['rijkshuisstijl interpretatie discussie URL'] && (
+                <LinkListLink href={comp['rijkshuisstijl interpretatie discussie URL']}>Designdiscussie</LinkListLink>
+              )}
+            </LinkList>
+          </CardAsLink>
+        ))}
+      </div>
     </PageBody>
     <SharedFooter />
   </>
