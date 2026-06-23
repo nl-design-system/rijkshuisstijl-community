9185f67a1afc61a8937f358bc437f8e093773596
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2026-06-23 10:29:53 +0200

    design-tokens fix

---
+++ packages/storybook/src/templates/handboek/design-tokens/LineHeightSample/index.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+import type { HTMLAttributes, PropsWithChildren } from 'react';
+import './index.css';
+
+export const LineHeightSample = ({
+  children,
+  className,
+  fontFamily,
+  fontWeight,
+  fontSize,
+  lineHeight,
+}: PropsWithChildren<
+  {
+    fontFamily: string;
+    fontWeight: number;
+    fontSize: string;
+    lineHeight: string;
+  } & HTMLAttributes<HTMLDivElement>
+>) => (
+  <div
+    className={clsx('rhc-line-height-sample', className)}
+    style={{ fontFamily, fontWeight, fontSize, lineHeight }}
+  >
+    {children}
+  </div>
+);
