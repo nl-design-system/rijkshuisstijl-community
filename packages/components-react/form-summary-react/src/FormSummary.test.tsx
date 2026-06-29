76b341e5613dd6110468c1a6c40cae4c93230fd0
NeslihanUTUK <120681285+NeslihanUTUK@users.noreply.github.com>
2026-06-29 17:51:04 +0200

    Update FormSummary.test.tsx

--- packages/components-react/form-summary-react/src/FormSummary.test.tsx
+++ packages/components-react/form-summary-react/src/FormSummary.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup, render } from '@testing-library/react';
 import { afterEach, describe, expect, it } from 'vitest';
-import { FormSummary } from '../../library-react/src';
+import { FormSummary } from '.';
 
 describe('FormSummary', () => {
   it('renders a visible element', () => {
