dd21cefed2be4ab7ffe3997d6a80aeda3e2118e2
Bart Waardenburg <bart@waardenburg.dev>
2026-07-02 20:16:43 +0200

    test(blockquote): unit test voor de Blockquote React-component

---
+++ packages/components-react/blockquote-react/src/Blockquote.test.tsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom/vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, it } from 'vitest';
+import { Blockquote } from '.';
+
+describe('Blockquote', () => {
+  it('renders a visible element', () => {
+    const { container } = render(<Blockquote>Citaat</Blockquote>);
+
+    const blockquote = container.querySelector(':only-child');
+
+    expect(blockquote).toBeInTheDocument();
+    expect(blockquote).toBeVisible();
+  });
+
+  it('renders the attribution', () => {
+    const { getByText } = render(<Blockquote attribution="Pippi Langkous">Citaat</Blockquote>);
+
+    expect(getByText('Pippi Langkous')).toBeVisible();
+  });
+
+  it('merges a custom class name with the rhc class name', () => {
+    const { container } = render(<Blockquote className="rhc-rounded-corner">Citaat</Blockquote>);
+
+    const blockquote = container.querySelector(':only-child');
+
+    expect(blockquote).toHaveClass('rhc-blockquote', 'rhc-rounded-corner');
+  });
+});
+
+afterEach(() => cleanup());
