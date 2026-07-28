858026269e4a687d7cd089f75f4533ec280a248e
Casper Ravenhorst <casper@sinedub.io>
2026-07-29 00:11:32 +0200

    feat: remove everything footer except bare bones, add footer-css and grid-css

--- packages/components-react/footer-react/rollup.config.mjs
+++
@@ -1,6 +0,0 @@
-import { createRollupConfig } from '../library-react/rollup.config.mjs';
-import { readFileSync } from 'node:fs';
-
-const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
-
-export default createRollupConfig(packageJson);
--- packages/storybook/src/components-css/footer.stories.tsx
+++ packages/storybook/src/components-css/footer.stories.tsx
@@ -1,15 +1,21 @@
-import reactMeta from '../components-react/footer.stories';
-import * as ReactStories from '../components-react/footer.stories';
-import { mergeCssMeta } from '../helpers/mergeCssMeta';
+import '@rijkshuisstijl-community/footer-css/dist/index.css';
+import '@rijkshuisstijl-community/grid-css/dist/index.css';
+
+export const Footer = () => (
+  <div className="rhc-footer">
+    <div className="rhc-section">
+      <div className="rhc-grid">
+        <div className="rhc-grid__cell--span3">1</div>
+        <div className="rhc-grid__cell--span3">2</div>
+        <div className="rhc-grid__cell--span3">3</div>
+        <div className="rhc-grid__cell--span3">4</div>
+      </div>
+    </div>
+  </div>
+);
 
 export default {
-  ...mergeCssMeta(reactMeta),
+  component: Footer,
   title: 'Footer',
-  id: 'css-footer',
+  id: 'rhc-footer',
 };
-
-export const DefaultFooter = ReactStories.DefaultFooter;
-export const PrimaryOutlinedFooter = ReactStories.PrimaryOutlinedFooter;
-export const PrimaryOutlinedFooterSubFooter = ReactStories.PrimaryOutlinedFooterSubFooter;
-export const ColumnLayoutFooter = ReactStories.ColumnLayoutFooter;
-export const CustomHeadingFooter = ReactStories.CustomHeadingFooter;
