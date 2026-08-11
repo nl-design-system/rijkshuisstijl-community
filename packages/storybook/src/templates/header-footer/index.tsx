36ddb6cf617e1cfbaf8830d185e933588635f497
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2026-08-11 13:06:56 +0200

    navbar in template

--- packages/storybook/src/templates/header-footer/index.tsx
+++ packages/storybook/src/templates/header-footer/index.tsx
@@ -1,4 +1,4 @@
-import { Footer, Icon, Logo, PageHeader } from '@rijkshuisstijl-community/components-react';
+import { Footer, Icon, Logo, PageHeader, NavBar } from '@rijkshuisstijl-community/components-react';
 import { FooterLinks, FooterFooterLinks } from '../../components-css/footer.stories';
 
 const HeaderFooter = () => {
@@ -11,6 +11,43 @@ const HeaderFooter = () => {
         <Logo organisation="Organisatie" subtitle="Wat wij doen">
           <Icon className={'dutch-map'} icon={'nederland-map'} />
         </Logo> 
+        <NavBar
+          className="rhc-nav-bar"
+          endItems={[
+            {
+              href: '/',
+              id: 'end-first-link',
+              label: 'Figma',
+            },
+            {
+              href: '/',
+              id: 'end-second-link',
+              label: 'Github',
+            },
+            {
+              href: '/',
+              id: 'end-third-link',
+              label: 'Storybook',
+            },
+          ]}
+          items={[
+            {
+              href: '/',
+              id: 'first-link',
+              label: 'Homepage',
+            },
+            {
+              href: '/',
+              id: 'second-link',
+              label: 'Componenten',
+            },
+            {
+              href: '/',
+              id: 'third-link',
+              label: 'Voorbeelden',
+            },
+          ]}
+        />
       </PageHeader>
       <Footer slot1={slot1} slot2={slot2} />
     </>
